f9c697a9298cb5a4412a3b1058c019b8fea9d62d
David Di Biase <david@pilotinterative.ca>
2021-06-08 22:21:30 -0400

    Major clean-up to geolocation prim

--- packages/createGeolocation/src/index.ts
+++ packages/createGeolocation/src/index.ts
@@ -1,97 +1,82 @@
 
-import { createEffect, createSignal } from 'solid-js';
+import { createEffect, createSignal, onCleanup } from 'solid-js';
 
 /**
  * Provides a set of utilities for geolocation tracking in browser.
  * Ported from https://github.com/imbhargav5/rooks/blob/main/src/hooks/useGeolocation.ts
  *
- * @param callback - Function that will be called every `delay` ms
- * @param delay - Number representing the delay in ms
- * @param schedule - Specify the schedule you'd like to use or supply a custom function
- * @return Provides a manual clear/end function.
+ * @param watchPosition - Specify if the location should be updated periodically
+ * @param enableHighAccuracy - Enable if the locator should be very accurate
+ * @param maximumAge - Maximum cached position age
+ * @param timeout - Amount of time before the error callback is envoked, if 0 then never
+ * @return Returns a location signal and one-off async query callback
  * 
  * @example
  * ```ts
- * let [count, setCount] = createGeolocation(0);
+ * let [location] = createGeolocation(true);
+ * let [location, getLocation] = createGeolocation(true, true, 0, 100);
  * ```
  */
+const createGeolocation = (
+  watchPosition: boolean,
+  enableHighAccuracy = false,
+  maximumAge = 0,
+  timeout = Number.POSITIVE_INFINITY
+): [
+  location: () => GeolocationCoordinates | null,
+  getLocation: () => Promise<GeolocationCoordinates>
+] => {
+  let registeredHandlerID: number | null;
+  const [location, setLocation] = createSignal<GeolocationCoordinates | null>(
+    null
+  );
 
- type GeolocationDetail = {
-  lat?: number;
-  lng?: number;
-  isError: boolean;
-  message: string;
-};
+  // Helper to retrieve the current location synchronously
+  const getLocation = (): Promise<GeolocationCoordinates> =>
+    new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (res) => {
+            setLocation(res.coords);
+            resolve(res.coords);
+          },
+          (error) => reject({ isError: true, message: error.message }),
+          {
+            enableHighAccuracy,
+            timeout,
+            maximumAge
+          }
+        );
+      } else {
+        reject({
+          isError: true,
+          message: "Geolocation is not supported for this Browser/OS."
+        });
+      }
+    });
+  // Helper to clear the geolocator
+  const clearGeolocator = () =>
+    navigator.geolocation.clearWatch(registeredHandlerID!);
 
-type IOptions = {
-  enableHighAccuracy?: boolean;
-  timeout?: number;
-  maximumAge?: number;
-  when?: boolean;
-};
-
-// Helper method
-const getGeoLocation = (options: IOptions): Promise<GeolocationDetail> => (
-  new Promise((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (res) => {
-          const { coords } = res;
-          const { latitude, longitude } = coords;
-          resolve({
-            isError: false,
-            lat: latitude,
-            lng: longitude,
-            message: '',
-          });
-        },
-        (error) => {
-          reject({ isError: true, message: error.message });
-        },
-        options
-      );
-    } else {
-      reject({
-        isError: true,
-        message: 'Geolocation is not supported for this Browser/OS.',
-      });
-    }
-  })
-);
-
-const defaultGeoLocationOptions = {
-  enableHighAccuracy: false,
-  maximumAge: 0,
-  timeout: Number.POSITIVE_INFINITY,
-  when: true,
-};
-
-const useGeolocation = (
-  geoLocationOptions: IOptions = defaultGeoLocationOptions
-): GeolocationDetail | null => {
-  const [geoObject, setGeoObject] = createSignal<GeolocationDetail | null>(null);
-  const { when, enableHighAccuracy, timeout, maximumAge } = geoLocationOptions;
-  const getGeoCode = async () => {
-    try {
-      const value = await getGeoLocation({
-        enableHighAccuracy,
-        maximumAge,
-        timeout,
-        when,
-      });
-      setGeoObject(value);
-    } catch (error) {
-      setGeoObject(error);
-    }
-  }
-
+  // Implement as an effect to allow switching locator on/off
   createEffect(() => {
-    if (when) {
-      getGeoCode();
+    if (watchPosition === true) {
+      registeredHandlerID = navigator.geolocation.watchPosition(
+        (res) => setLocation(res.coords),
+        () => setLocation(null), // Maybe we should throw an error as well?
+        {
+          enableHighAccuracy,
+          timeout,
+          maximumAge
+        }
+      );
+    } else if (watchPosition === false && registeredHandlerID === null) {
+      clearGeolocator();
     }
   });
+  onCleanup(() => registeredHandlerID != null && clearGeolocator());
 
-  return geoObject;
-}
+  return [location, getLocation];
+};
 
-export { useGeolocation };
+export default createGeolocation;
