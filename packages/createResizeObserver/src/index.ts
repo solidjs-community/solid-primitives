65e9344409940ba7d634eb50e881f0728bac5144
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2021-07-04 09:10:43 -0400

    add createResizeObserver, bump dependencies

---
+++ packages/createResizeObserver/src/index.ts
@@ -0,0 +1,77 @@
+import { createEffect, createSignal, onCleanup } from "solid-js";
+
+type ObservedSize = {
+  width: number | undefined;
+  height: number | undefined;
+};
+
+type ResizeHandler = (size: ObservedSize, ref: Element) => void;
+
+/**
+ * Create event listenere is a helper primitiive for binding events.
+ *
+ * @param opts.refs - Either an `HTMLElement`, an array of `HTMLElement`s, or a signal returning one of these.
+ * @param opts.onResize - Function handler to trigger on resize
+ * @return A callback that can be used to add refs to observe
+ *
+ */
+function createResizeObserver<T extends HTMLElement>(
+  opts: {
+    onResize: ResizeHandler;
+    refs?: T | T[] | (() => T | T[]);
+  }
+): (arg: T) => void {
+  const [otherRefs, setOtherRefs] = createSignal<T[]>([]);
+  const refCallback = (e: T) => setOtherRefs((l) => l.concat(e));
+
+  const previousMap = new Map<Element, ObservedSize>();
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    if (!Array.isArray(entries)) {
+      return;
+    }
+
+    for (const entry of entries) {
+      const newWidth = Math.round(entry.contentRect.width);
+      const newHeight = Math.round(entry.contentRect.height);
+      const previous = previousMap.get(entry.target);
+      if (!previous || previous.width !== newWidth || previous.height !== newHeight) {
+        const newSize = { width: newWidth, height: newHeight };
+        opts.onResize(newSize, entry.target);
+        previousMap.set(entry.target, { width: newWidth, height: newHeight });
+      }
+    }
+  });
+
+  createEffect((oldRefs?: T[]) => {
+    let refs: T[] = [];
+    if (opts.refs) {
+      const optsRefs = typeof opts.refs === "function" ? opts.refs() : opts.refs;
+      if (Array.isArray(optsRefs))
+        refs = refs.concat(optsRefs);
+      else refs.push(optsRefs)
+    }
+    refs = refs.concat(otherRefs());
+    oldRefs = oldRefs || [];
+    oldRefs.forEach((oldRef) => {
+      if (!(oldRef in refs)) {
+        resizeObserver.unobserve(oldRef);
+        previousMap.delete(oldRef);
+      }
+    });
+    refs.forEach((ref) => {
+      if (!(ref in oldRefs!)) {
+        resizeObserver.observe(ref);
+      }
+    });
+    return refs;
+  });
+
+  onCleanup(() => {
+    resizeObserver.disconnect();
+  })
+
+  return refCallback;
+}
+
+export default createResizeObserver;
---
+++ packages/createResizeObserver/test/index.test.ts
@@ -0,0 +1,7 @@
+import { render } from 'solid-testing-library';
+import createResizeObserver from '../src/index';
+
+describe('createResizeObserver', (): void => {
+
+
+});
