1f719f91dd3cbc5c94482dddbdc68b06a5f5d05e
David Di Biase <david@pilotinterative.ca>
2021-07-12 09:14:39 -0400

    Added a throttle function

---
+++ packages/throttle/README.md
@@ -0,0 +1,22 @@
+# @solid-primitives/throttle
+
+Creates a helpful throttle function.
+
+## How to use it
+
+## Demo
+
+## Changelog
+
+<details>
+<summary><b>Expand Changelog</b></summary>
+
+1.0.0
+
+First ported commit from react-use-localstorage.
+
+</details>
+
+## Contributors
+
+Ported from the amazing work by at https://github.com/dance2die/react-use-localstorage.
--- packages/throttle/src/index.ts
+++ packages/throttle/src/index.ts
@@ -1,18 +1,18 @@
 /**
- * Creates a method that is debounced and cancellable.
+ * Creates a method that is throttled and cancellable.
  *
  * @param callback The callback to debounce
  * @param wait The duration to debounce
- * @returns The debounced callback trigger
+ * @returns The throttled callback trigger
  * 
  * @example
  * ```ts
- * const [trigger, clear] = createDebounce(() => console.log('hi'), 250));
+ * const [trigger, clear] = createThrottle(() => console.log('hi'), 250));
  * trigger('my-new-value');
  * console.log(value());
  * ```
  */
-const createDebounce = <T extends (...args: any[]) => void>(
+const createThrottle = <T extends (...args: any[]) => void>(
   func: T,
   wait?: number
 ): [
@@ -22,12 +22,14 @@ const createDebounce = <T extends (...args: any[]) => void>(
   let timeoutId: number | undefined;
   const clear = () => clearTimeout(timeoutId);
   const trigger = (...args: Parameters<T>) => {
-    if (timeoutId !== undefined) {
-      clear();
+    if (timeoutId === undefined) {
+      timeoutId = setTimeout(() => {
+        func(...args);
+        timeoutId = undefined;
+      }, wait);
     }
-    timeoutId = setTimeout(() => func(...args), wait);
   };
   return [trigger, clear];
 };
 
-export default createDebounce;
+export default createThrottle;
