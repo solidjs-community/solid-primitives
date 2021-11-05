5a9b22ac51ec9d2ce25378ff2d798e0a29cece2f
Damian Tarnawski <gthetarnav@gmail.com>
2021-11-05 20:24:45 +0100

    event-listener: generic event handler types

--- packages/event-listener/src/index.ts
+++ packages/event-listener/src/index.ts
@@ -1,17 +1,30 @@
 import { Accessor, createEffect, onCleanup } from "solid-js";
 import type { JSX } from "solid-js";
 
-export type EventListenerProps<E extends Record<string, Event> = {}> = [
-  name: (keyof WindowEventMap | keyof E) & string,
-  handler: EventListenerOrEventListenerObject | null,
-  options?: AddEventListenerOptions
+// because of a generic eventName, this needs to be used for type casting addEventListener arguments
+type ValidAddEventListenerProps = [
+  type: string,
+  callback: EventListenerOrEventListenerObject | null,
+  options?: boolean | AddEventListenerOptions
 ];
+
+export type EventHandler<
+  E extends WindowEventMap | Record<string, Event> = WindowEventMap,
+  K extends keyof E = keyof E
+> = (event: E[K]) => void;
+
+export type EventListenerProps<
+  E extends WindowEventMap | Record<string, Event> = WindowEventMap,
+  K extends keyof E = keyof E
+> = [name: K, handler: EventHandler<E, K> | null, options?: boolean | AddEventListenerOptions];
 
 declare module "solid-js" {
   namespace JSX {
     interface Directives {
-      createEventListener: (ref: HTMLElement, props: Accessor<EventListenerProps<{}>>) =>
-        [add: (target: EventTarget) => void, remove: (target: EventTarget) => void];
+      createEventListener: (
+        ref: HTMLElement,
+        props: Accessor<EventListenerProps>
+      ) => [add: (target: EventTarget) => void, remove: (target: EventTarget) => void];
     }
   }
 }
@@ -34,45 +47,63 @@ export type E = JSX.Element;
  *   "mouseDown",
  *   () => console.log("Click")
  * );
+ *
  * // or as a directive
  * <MyButton use:createEventListener={() => ['click', () => console.log("Click")]}>Click!</MyButton>
+ *
  * // you can provide your own event map type:
  * createEventListener<{ myCustomEvent: Event }>(window, 'myCustomEvent', () => console.log("yup!"));
  * ```
  */
-export function createEventListener<E extends Record<string, Event> = {}>(
+export function createEventListener<
+  E extends WindowEventMap | Record<string, Event> = WindowEventMap,
+  K extends keyof E = keyof E
+>(
+  target: EventTarget | EventTarget[],
+  eventName: K,
+  handler: EventHandler<E, K> | null,
+  options?: boolean | AddEventListenerOptions
+): readonly [add: (el: EventTarget) => void, remove: (el: EventTarget) => void];
+
+export function createEventListener<
+  E extends WindowEventMap | Record<string, Event> = WindowEventMap,
+  K extends keyof E = keyof E
+>(
   ref: EventTarget | EventTarget[],
-  props: Accessor<EventListenerProps<E>>
+  props: Accessor<EventListenerProps<E, K>>
 ): readonly [add: (el: EventTarget) => void, remove: (el: EventTarget) => void];
-export function createEventListener<E extends Record<string, Event> = {}>(
+
+export function createEventListener<
+  E extends WindowEventMap | Record<string, Event> = WindowEventMap,
+  K extends keyof E = keyof E
+>(
   target: EventTarget | EventTarget[],
-  eventName: keyof E & string,
-  handler: EventListenerOrEventListenerObject | null,
-  options?: EventListenerOptions
-): readonly [add: (el: EventTarget) => void, remove: (el: EventTarget) => void];
-export function createEventListener<E extends Record<string, Event> = {}>(
-  target: EventTarget | EventTarget[],
-  nameOrProps: (keyof E & string) | Accessor<EventListenerProps<E>>,
-  handler?: EventListenerOrEventListenerObject | null,
-  options?: EventListenerOptions
+  nameOrProps: K | Accessor<EventListenerProps<E, K>>,
+  handler?: EventHandler<E, K> | null,
+  options?: boolean | AddEventListenerOptions
 ): readonly [add: (el: EventTarget) => void, remove: (el: EventTarget) => void] {
   const targets = Array.isArray(target) ? target : [target];
-  const props: Accessor<EventListenerProps<E>> =
-    typeof nameOrProps === 'function' ? nameOrProps : () => [nameOrProps ?? '', handler ?? null, options];
+  const props: Accessor<EventListenerProps<E, K>> =
+    typeof nameOrProps === "function"
+      ? nameOrProps
+      : () => [nameOrProps ?? "", handler ?? null, options];
   const add = (target: EventTarget) => {
     targets.includes(target) || targets.push(target);
-    target.addEventListener.apply(target, props());
-  }
+    target.addEventListener.apply(target, props() as ValidAddEventListenerProps);
+  };
   const remove = (target: EventTarget) => {
     targets.forEach((t, index) => t === target && targets.splice(index, 1));
-    target.removeEventListener.apply(target, props());
-  }
+    target.removeEventListener.apply(target, props() as ValidAddEventListenerProps);
+  };
   // we need to directly add the event, otherwise we cannot dispatch it before the next effect runs
   targets.forEach(add);
-  createEffect((previousProps?: EventListenerProps<E>) => {
+  createEffect((previousProps?: EventListenerProps<E, K>) => {
     const currentProps = props();
     if (previousProps !== currentProps) {
-      previousProps && targets.forEach((target) => target.removeEventListener.apply(target, previousProps));
+      previousProps &&
+        targets.forEach(target =>
+          target.removeEventListener.apply(target, previousProps as ValidAddEventListenerProps)
+        );
       targets.forEach(add);
     }
     return currentProps;
@@ -81,6 +112,6 @@ export function createEventListener<E extends Record<string, Event> = {}>(
     targets.forEach(remove);
   });
   return [add, remove];
-};
+}
 
 export default createEventListener;
