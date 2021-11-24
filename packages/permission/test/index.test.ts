4e990972bc97c9c220aa2c85391f9884749b7115
Damian Tarnawski <gthetarnav@gmail.com>
2021-11-24 17:32:11 +0100

    move permission tests from jest to uvu, add licence file

--- packages/permission/test/index.test.ts
+++ packages/permission/test/index.test.ts
@@ -1,41 +1,41 @@
+import { __permissions__ } from "./setup";
 import { createEffect, createRoot } from "solid-js";
+import { test } from "uvu";
+import * as assert from "uvu/assert";
 import { createPermission } from "../src";
 
-describe("createPermission", () => {
-  test("reads permission", () =>
-    createRoot(
-      dispose =>
-        new Promise<void>(resolve => {
-          const permission = createPermission("microphone" as PermissionName);
-          const expectedStates = ["unknown", "granted"];
-          createEffect(() => {
-            expect(permission()).toBe(expectedStates.shift());
-            if (expectedStates.length === 0) {
-              dispose();
-              resolve();
-            }
-          });
-        })
-    ));
+test("reads permission", () =>
+  new Promise<void>(resolve =>
+    createRoot(dispose => {
+      const permission = createPermission("microphone" as PermissionName);
+      const expectedStates = ["unknown", "granted"];
+      createEffect(() => {
+        assert.is(permission(), expectedStates.shift());
+        if (expectedStates.length === 0) {
+          dispose();
+          resolve();
+        }
+      });
+    })
+  ));
 
-  test("reads permission updates", () =>
-    createRoot(
-      dispose =>
-        new Promise<void>(resolve => {
-          const permission = createPermission("camera" as PermissionName);
-          const expectedStates = ["unknown", "denied", "granted"];
-          createEffect(() => {
-            expect(permission()).toBe(expectedStates.shift());
-            if (expectedStates.length === 2) {
-              setTimeout(() => {
-                (window as any).__permissions__.camera.state = "granted";
-                (window as any).__permissions__.camera.dispatchEvent(new Event("change"));
-              }, 50);
-            } else if (expectedStates.length === 0) {
-              dispose();
-              resolve();
-            }
-          });
-        })
-    ));
-});
+test("reads permission updates", () =>
+  new Promise<void>(resolve =>
+    createRoot(dispose => {
+      const permission = createPermission("camera" as PermissionName);
+      const expectedStates = ["unknown", "denied", "granted"];
+      createEffect(() => {
+        assert.is(permission(), expectedStates.shift());
+        if (expectedStates.length === 2) {
+          setTimeout(() => {
+            __permissions__.camera.__dispatchEvent("granted");
+          }, 50);
+        } else if (expectedStates.length === 0) {
+          dispose();
+          resolve();
+        }
+      });
+    })
+  ));
+
+test.run();
