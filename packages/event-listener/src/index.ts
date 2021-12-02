4a635d5051028d4397579a6fb1e9bcac549f2a91
Damian Tarnawski <gthetarnav@gmail.com>
2021-12-02 16:10:31 +0100

    rewrite event-listener

--- packages/event-listener/src/index.ts
+++ packages/event-listener/src/index.ts
@@ -1,45 +1,37 @@
-import { Accessor, createEffect, onCleanup } from "solid-js";
-import type { JSX } from "solid-js";
+import { accessAsArray, Fn, ItemsOf, Many, MaybeAccessor } from "solid-fns";
+import { createEffect, JSX, onCleanup } from "solid-js";
 
-export type EventMapOf<T> = T extends Window
+export type EventMapOf<Target> = Target extends Window
   ? WindowEventMap
-  : T extends Document
+  : Target extends Document
   ? DocumentEventMap
-  : T extends HTMLElement
+  : Target extends HTMLElement
   ? HTMLElementEventMap
-  : T extends MediaQueryList
+  : Target extends MediaQueryList
   ? MediaQueryListEventMap
-  : {};
-
-export type EventMapOfMultiple<T> = T extends EventTarget
-  ? EventMapOf<T>
-  : T extends EventTarget[]
-  ? EventMapOf<T[number]>
   : never;
 
-export type EventListenerProps<
-  T extends EventTarget | EventTarget[],
-  E extends Record<string, Event> = {}
-> = [
-  name: [{}, {}] extends [EventMapOfMultiple<T>, E]
-    ? string
-    : string & (keyof EventMapOfMultiple<T> | keyof E),
-  handler: EventListenerOrEventListenerObject | null,
-  options?: AddEventListenerOptions
-];
+export type EventHandler<
+  EventMap extends
+    | HTMLElementEventMap
+    | WindowEventMap
+    | DocumentEventMap
+    | MediaQueryListEventMap
+    | Record<string, Event>,
+  EventName extends keyof EventMap
+> = (event: EventMap[EventName]) => void;
 
-export type CreateEventListenerReturn = [
-  add: (el: EventTarget) => void,
-  remove: (el: EventTarget) => void
-];
+export type EventListenerReturn = [stop: Fn, start: Fn];
+
+type EventListenerDirectiveProps =
+  | [string, (e: any) => void, boolean | AddEventListenerOptions]
+  | [string, (e: any) => void];
 
 declare module "solid-js" {
   namespace JSX {
     interface Directives {
-      createEventListener: (
-        ref: HTMLElement,
-        props: Accessor<EventListenerProps<HTMLElement, {}>>
-      ) => [add: (target: EventTarget) => void, remove: (target: EventTarget) => void];
+      // directive types are very premissive to prevent type errors from incompatible types, since props cannot be generic
+      createEventListener: EventListenerDirectiveProps;
     }
   }
 }
@@ -47,82 +39,106 @@ declare module "solid-js" {
 // only here so the `JSX` import won't be shaken off the tree:
 export type E = JSX.Element;
 
-type CreateEventListenerFn = <
-  EventMap extends Record<string, Event>,
-  Target extends EventTarget | EventTarget[]
+// typical dom events
+export function createEventListener<
+  Target extends Window | Document | HTMLElement | MediaQueryList,
+  EventMap extends EventMapOf<Target>,
+  EventName extends keyof EventMap
 >(
-  target: Target,
-  ...props: [Accessor<EventListenerProps<Target, EventMap>>] | EventListenerProps<Target, EventMap>
-) => CreateEventListenerReturn;
+  target: MaybeAccessor<Many<Target>>,
+  eventName: EventName,
+  handler: EventHandler<EventMap, EventName>,
+  options?: boolean | AddEventListenerOptions
+): EventListenerReturn;
 
-/**
- * Creates an event listener helper primitive.
- *
- * @param target - ref to HTMLElement, EventTarget or Array thereof
- * @param nameOrProps - name of the event or Accessor with the event props ([name, handler, options?])
- * @param handler - if nameOrProps contains a name, you can specify the handler here
- * @param options - if nameOrProps contains a name, you can specify event listener options
- *
- * @example
- * ```tsx
- * const [add, remove] = createEventListener(
- *   document.getElementById("mybutton"),
- *   "mouseDown",
- *   () => console.log("Click")
- * );
- * // or as a directive
- * <MyButton use:createEventListener={() => ['click', () => console.log("Click")]}>Click!</MyButton>
- * // you can provide your own event map type:
- * createEventListener<{ myCustomEvent: Event }, Window>(window, 'myCustomEvent', () => console.log("yup!"));
- * ```
- */
-export const createEventListener: CreateEventListenerFn = (target, ...propsArray) => {
-  const targets: EventTarget[] = Array.isArray(target) ? target : [target];
-  type EventProps = [
-    name: string,
-    handler: EventListenerOrEventListenerObject | null,
-    options?: EventListenerOptions
-  ];
-  const props: Accessor<EventProps> =
-    typeof propsArray[0] === "function" ? propsArray[0] : () => propsArray as EventProps;
-  const add = (target: EventTarget) => {
-    targets.includes(target) || targets.push(target);
-    const [name, handler, options] = props();
-    if (name && handler !== undefined) {
-      target.addEventListener.apply(target, props());
-    }
+// custom events - passed EventMap as the first Generic
+export function createEventListener<
+  EventMap extends Record<string, Event>,
+  EventName extends keyof EventMap = keyof EventMap
+>(
+  target: MaybeAccessor<Many<EventTarget>>,
+  eventName: EventName,
+  handler: (e: EventMap[EventName]) => void,
+  options?: boolean | AddEventListenerOptions
+): EventListenerReturn;
+
+// custom events - passed EventNames as the first Generic
+export function createEventListener<
+  EventNames extends string,
+  EventMap extends Record<string, Event> = Record<ItemsOf<[EventNames]>, Event>,
+  EventName extends keyof EventMap = keyof EventMap
+>(
+  target: MaybeAccessor<Many<EventTarget>>,
+  eventName: EventName,
+  handler: EventHandler<EventMap, EventName>,
+  options?: boolean | AddEventListenerOptions
+): EventListenerReturn;
+
+// directive usage
+export function createEventListener<
+  EventNames extends string,
+  EventMap extends Record<string, Event> = Record<ItemsOf<[EventNames]>, Event>,
+  EventName extends keyof EventMap = keyof EventMap
+>(target: Element, props: () => EventListenerDirectiveProps): EventListenerReturn;
+
+export function createEventListener(
+  target: MaybeAccessor<Many<EventTarget>>,
+  ...rest: any[]
+): EventListenerReturn {
+  let eventName: string,
+    handler: (e: any) => void,
+    options: boolean | AddEventListenerOptions | undefined;
+
+  if (rest.length > 1) {
+    // function usage
+    eventName = rest[0];
+    handler = rest[1];
+    options = rest[2];
+  } else {
+    // directive usage
+    const props = rest[0]();
+    eventName = props[0];
+    handler = props[1];
+    options = props[2];
+  }
+
+  let toCleanup: Fn[] = [];
+  const stop = () => {
+    toCleanup.forEach(fn => fn());
+    toCleanup = [];
   };
-  const remove = (target: EventTarget) => {
-    targets.forEach((t, index) => t === target && targets.splice(index, 1));
-    target.removeEventListener.apply(target, props());
+  const start = () => {
+    stop();
+    accessAsArray(target).forEach(target => {
+      target.addEventListener(eventName, handler, options);
+      toCleanup.push(() => target.removeEventListener(eventName, handler, options));
+    });
   };
-  // we need to directly add the event, otherwise we cannot dispatch it before the next effect runs
-  targets.forEach(add);
-  createEffect(previousProps => {
-    const currentProps = props();
-    if (previousProps !== currentProps) {
-      previousProps &&
-        targets.forEach(target => target.removeEventListener.apply(target, previousProps));
-      targets.forEach(add);
-    }
-    return currentProps;
-  }, props());
-  onCleanup(() => {
-    targets.forEach(remove);
-  });
-  return [add, remove];
-};
 
-export default createEventListener;
+  createEffect(start);
+  onCleanup(stop);
 
-// /* TypeCheck */
-// wrong event names:
-// createEventListener<{}, Document>(document, () => ['fullscreenchenge', () => console.log('test')]);
-// createEventListener<{}, Window>(window, 'fullscreenchange', () => console.log('test'));
-// valid events:
-// createEventListener<{}, Document>(document, () => ['fullscreenchange', () => console.log('test')]);
-// createEventListener<{}, Document>(document, () => ['abort', () => console.log('test')]);
-// createEventListener<{}, Window>(window, 'abort', () => console.log('test'));
-// createEventListener<{test: Event}, Window>(window, 'scroll', () => console.log('test'))
-// createEventListener<{test: Event}, EventTarget>(new EventTarget(), 'test', () => console.log('test'));
-// /**/
+  return [stop, start];
+}
+
+// // /* TypeCheck */
+// const mouseHandler = (e: MouseEvent) => {};
+// const touchHandler = (e: TouchEvent) => {};
+// const el = document.createElement("div");
+// // dom events
+// createEventListener(window, "mousemove", mouseHandler);
+// createEventListener(document, "touchstart", touchHandler);
+// createEventListener(el, "mousemove", mouseHandler);
+// createEventListener(() => el, "touchstart", touchHandler);
+// // custom events
+// createEventListener<"custom" | "test">(el, "test", e => {});
+// createEventListener<{ test: Event }>(window, "test", e => {});
+// createEventListener<{ test: Event; custom: MouseEvent }, "custom">(
+//   () => el,
+//   "custom",
+//   mouseHandler
+// );
+// createEventListener<{ test: Event }>(new EventTarget(), "test", () => console.log("test"));
+// // directive
+// createEventListener(el, () => ["mousemove", mouseHandler, { passive: true }]);
+// createEventListener(el, () => ["custom", e => {}]);
