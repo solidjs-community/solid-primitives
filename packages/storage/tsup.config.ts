ef68328a7845520253a7b5fa94f1e7f46fd3dfb8
David Di Biase <1168397+davedbase@users.noreply.github.com>
2021-12-04 17:00:16 -0500

    Update storage

--- packages/storage/tsup.config.ts
+++ packages/storage/tsup.config.ts
@@ -1,2 +1,15 @@
+import { defineConfig } from "tsup";
 import defaultConfig from "../../tsup.config";
-export default defaultConfig;
+
+export default defineConfig(() => {
+  return {
+    ...defaultConfig,
+    dts: "src/types.ts",
+    entryPoints: [
+      "src/index.ts",
+      "src/cookies.ts",
+      "src/storage.ts",
+      "src/tools.ts"
+    ]
+  };
+});
