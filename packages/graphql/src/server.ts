b4bf8a787964b411a4f53c9bcabd2bdee9a44b48
David Di Biase <1168397+davedbase@users.noreply.github.com>
2021-12-04 14:21:03 -0500

    Updated graphql

--- packages/graphql/src/server.ts
+++ packages/graphql/src/server.ts
@@ -2,19 +2,6 @@ import { Accessor } from "solid-js";
 import { default as createGraphQL } from "./index";
 import nodeFetch from 'node-fetch';
 
-/**
- * Creates a reactive GraphQL query client.
- *
- * @param string URL as string or accessor
- * @param headers A list of headers to supply the client
- * @param function Fetch library to use for the request
- * @returns Returns a query generator the produces Solid resources for queries
- *
- * @example
- * ```ts
- * const newQuery = createGraphQLClient("https://foobar.com/v1/api", { authorization: "Bearer mytoken" });
- * ```
- */
 const createGraphQLClient = (
   url: string | Accessor<string>,
   headers: RequestHeaders = {},
