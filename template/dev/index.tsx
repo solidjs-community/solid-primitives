54c5434b799ec6e3c646e1357179776e18ff3621
Damian Tarnawski <gthetarnav@gmail.com>
2022-01-08 21:43:56 +0100

    update template dev playground

--- template/dev/index.tsx
+++ template/dev/index.tsx
@@ -1,11 +1,16 @@
-import type { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import { render } from "solid-js/web";
 import "uno.css";
 
 const App: Component = () => {
+  const [count, setCount] = createSignal(0);
+  const increment = () => setCount(count() + 1);
+
   return (
-    <div class="p-24 box-border w-full min-h-screen flex flex-col justify-center items-center">
-      Apply your primitive test here for testing.
+    <div class="p-24 box-border w-full min-h-screen flex flex-col justify-center items-center space-y-12 bg-gray-800 text-white">
+      <button class="btn" onClick={increment}>
+        {count()}
+      </button>
     </div>
   );
 };
