e2d9e2521582fa18fbdd6ef81a5c5e00edd077a0
thetarnav <gthetarnav@gmail.com>
2022-01-27 22:36:15 +0100

    feat(pointer): added two directives & jsdoc

--- packages/pointer/dev/index.tsx
+++ packages/pointer/dev/index.tsx
@@ -1,32 +1,50 @@
-import { createPointerListener, createPointerPosition } from "../src";
+import {
+  createPointerListener,
+  createPointerPosition,
+  getPositionToElement,
+  pointerPosition,
+  pointerHover
+} from "../src";
 import { Component, createSignal } from "solid-js";
 import { render } from "solid-js/web";
 import "uno.css";
+pointerPosition;
+pointerHover;
 
 const App: Component = () => {
   const [poz, setPoz] = createSignal({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = createSignal(false);
 
   let ref!: HTMLDivElement;
 
-  createPointerListener({
-    // target: () => ref,
-    // pointerTypes: ["touch"],
-    onStart: e => console.log("start", e.x, e.y),
-    onMove: e => setPoz({ x: e.x, y: e.y }),
-    onend: e => console.log("end", e.x, e.y),
-    onLostCapture: e => console.log("lost")
-  });
+  // createPointerListener({
+  //   // target: () => ref,
+  //   // pointerTypes: ["touch"],
+  //   onStart: e => console.log("start", e.x, e.y),
+  //   onMove: e => setPoz({ x: e.x, y: e.y }),
+  //   onend: e => console.log("end", e.x, e.y),
+  //   onLostCapture: e => console.log("lost")
+  // });
 
   const state = createPointerPosition();
 
   return (
     <div
-      class="p-24 box-border w-full min-h-screen flex flex-col justify-center items-center space-y-4 bg-gray-800 text-white"
+      class="p-24 box-border w-full min-h-150vh flex flex-col justify-center items-center space-y-4 bg-gray-800 text-white"
+      classList={{
+        "!bg-cyan-700": isHovering()
+      }}
       style={{ "touch-action": "none" }}
     >
-      <div ref={ref} class="wrapper-v">
+      <div
+        ref={ref}
+        class="wrapper-v"
+        // use:pointerPosition={(poz, el) => console.log(getPositionToElement(poz, el))}
+        use:pointerHover={setIsHovering}
+      >
         <p class="caption">x = {Math.round(poz().x)}</p>
         <p class="caption">y = {Math.round(poz().y)}</p>
+        <p class="caption">{isHovering() ? "hovering" : "not hovering"}</p>
       </div>
       <div ref={ref} class="wrapper-v">
         <p class="break-words max-w-80vw">{JSON.stringify(state())}</p>
--- packages/pointer/src/index.ts
+++ packages/pointer/src/index.ts
@@ -4,12 +4,18 @@ import {
   MaybeAccessor,
   forEachEntry,
   includes,
-  createCallbackStack
+  createCallbackStack,
+  Directive
 } from "@solid-primitives/utils";
 import { pick, split } from "@solid-primitives/immutable";
-import { Accessor, createSignal } from "solid-js";
+import { Accessor, createSignal, JSX } from "solid-js";
 
 export type PointerType = "mouse" | "touch" | "pen";
+
+export type Position = {
+  x: number;
+  y: number;
+};
 
 type PointerEventNames =
   | "over"
@@ -54,6 +60,25 @@ export type PointerState = {
   y: number;
 };
 
+declare module "solid-js" {
+  namespace JSX {
+    interface Directives {
+      pointerPosition: PointerPositionDirectiveProps;
+      pointerHover: PointerHoverDirectiveProps;
+    }
+  }
+}
+export type E = JSX.Element;
+
+type PointerPositionDirectiveHandler = (poz: PointerState, el: Element) => void;
+type PointerPositionDirectiveProps =
+  | PointerPositionDirectiveHandler
+  | { pointerTypes?: PointerType[]; handler: PointerPositionDirectiveHandler };
+type PointerHoverDirectiveHandler = (hovering: boolean, el: Element) => void;
+type PointerHoverDirectiveProps =
+  | PointerHoverDirectiveHandler
+  | { pointerTypes?: PointerType[]; handler: PointerHoverDirectiveHandler };
+
 const parseOnEventName = <T extends string>(name: T) =>
   name.substring(2).toLowerCase() as ReverseOnEventName<T>;
 const parseEventHandlers = <H extends Record<AnyOnEventName, Get<any>>>(
@@ -64,6 +89,27 @@ const parseEventHandlers = <H extends Record<AnyOnEventName, Get<any>>>(
   return result;
 };
 
+// TODO: possible helpers for managing multiple pointers?
+
+/**
+ * Setups event listeners for pointer events, that will get automatically removed on cleanup.
+ * @param config event handlers, target, and chosen pointer types
+ * - `target` - specify the target to attach the listeners to. Will default to `document.body`
+ * - `pointerTypes` - specify array of pointer types you want to listen to. By default listens to `["mouse", "touch", "pen"]`
+ * - your event handlers: e.g. `onstart`, `onend`, `onMove`, ...
+ * @returns function stopping currently attached listener
+ *
+ * @example
+ * createPointerListener({
+ *    // pass a function if the element has to mount
+ *    target: () => el,
+ *    pointerTypes: ["touch"],
+ *    onStart: e => console.log("start", e.x, e.y),
+ *    onMove: e => console.log({ x: e.x, y: e.y }),
+ *    onend: e => console.log("end", e.x, e.y),
+ *    onLostCapture: e => console.log("lost")
+ * });
+ */
 export function createPointerListener(
   config: Handlers & {
     target?: MaybeAccessor<Window | Document | HTMLElement>;
@@ -151,7 +197,23 @@ const pointerStateKeys: Exclude<keyof PointerState, "active">[] = [
   "twist",
   "pointerType"
 ];
+const getPointerState = (e: PointerEvent, active: boolean) =>
+  ({ ...pick(e, ...pointerStateKeys), active } as PointerState);
 
+/**
+ * Returns a signal with autoupdating Pointer position.
+ * @param config primitive config:
+ * - `target` - specify the target to attach the listeners to. Will default to `document.body`
+ * - `pointerTypes` - specify array of pointer types you want to listen to. By default listens to `["mouse", "touch", "pen"]`
+ * - `value` - set the initial value of the returned signal *(before the first event)*
+ * @returns position signal
+ *
+ * @example
+ * const position = createPointerPosition({
+ *    target: document.querySelector('my-el'),
+ *    pointerTypes: ["touch"]
+ * });
+ */
 export function createPointerPosition(
   config: {
     target?: MaybeAccessor<Window | Document | HTMLElement>;
@@ -160,8 +222,7 @@ export function createPointerPosition(
   } = {}
 ): Accessor<PointerState> {
   const [state, setState] = createSignal<any>(config.value ?? defaultState);
-  const handler = (e: PointerEvent, active = true) =>
-    setState({ ...pick(e, ...pointerStateKeys), active });
+  const handler = (e: PointerEvent, active = true) => setState(getPointerState(e, active));
   createPointerListener({
     target: config.target,
     pointerTypes: config.pointerTypes,
@@ -171,3 +232,60 @@ export function createPointerPosition(
   });
   return state;
 }
+
+/**
+ * A non-reactive helper function. It turns a position relative to the screen/window, to be relative to an element.
+ * @param poz object containing `x` & `y`
+ * @param el element to calculate the position of
+ * @returns the `poz` with `x` and `y` changed, and `isInside` added
+ */
+export const getPositionToElement = <T extends Position>(
+  poz: T,
+  el: Element
+): T & { isInside: boolean } => {
+  const { top, left, width, height } = el.getBoundingClientRect(),
+    x = poz.x - left,
+    y = poz.y - top;
+  return {
+    ...poz,
+    x,
+    y,
+    isInside: x >= 0 && y >= 0 && x <= width && y <= height
+  };
+};
+
+/**
+ * A directive for getting pointer position.
+ */
+export const pointerPosition: Directive<PointerPositionDirectiveProps> = (el, props) => {
+  const { pointerTypes, handler } = (() => {
+    const v = props();
+    return typeof v === "function" ? { handler: v, pointerTypes: undefined } : v;
+  })();
+  const runHandler = (e: PointerEvent, active = true) => handler(getPointerState(e, active), el);
+  createPointerListener({
+    target: el as HTMLElement,
+    pointerTypes: pointerTypes,
+    onstart: runHandler,
+    onMove: runHandler,
+    onend: e => runHandler(e, false)
+  });
+};
+
+// TODO: track pointer id's of start & end events, to not disable the hovering state when at least one pointer is still hovering
+
+/**
+ * A directive for checking if the element is being hovered by a pointer.
+ */
+export const pointerHover: Directive<PointerHoverDirectiveProps> = (el, props) => {
+  const { pointerTypes, handler } = (() => {
+    const v = props();
+    return typeof v === "function" ? { handler: v, pointerTypes: undefined } : v;
+  })();
+  createPointerListener({
+    target: el as HTMLElement,
+    pointerTypes: pointerTypes,
+    onstart: () => handler(true, el),
+    onend: () => handler(false, el)
+  });
+};
