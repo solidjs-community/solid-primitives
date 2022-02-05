706d84c1e76b544a6046176353bcfce594174ac2
thetarnav <gthetarnav@gmail.com>
2022-02-06 00:03:38 +0100

    test(signal-builders): add simple test

--- packages/signal-builders/test/index.test.ts
+++ packages/signal-builders/test/index.test.ts
@@ -1,10 +1,21 @@
-import {} from "../src";
-import { createRoot } from "solid-js";
+import { push, sort } from "../src";
+import { createRoot, createSignal } from "solid-js";
 import { suite } from "uvu";
 import * as assert from "uvu/assert";
+import { compare } from "@solid-primitives/utils";
 
-const test = suite("createPrimitiveTemplate");
+const test = suite("*");
 
-test("createPrimitiveTemplate return values", () => createRoot(dispose => {}));
+test("", () =>
+  createRoot(dispose => {
+    const [list, setList] = createSignal([4, 3, 2, 1]);
+    const [item, setItem] = createSignal(0);
+    const res = sort(push(list, item), compare);
+    assert.equal(res(), [0, 1, 2, 3, 4]);
+    setList([1, 2, 3, 5, 4]);
+    setItem(1);
+    assert.equal(res(), [1, 1, 2, 3, 4, 5]);
+    dispose();
+  }));
 
 test.run();
