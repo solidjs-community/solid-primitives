65d830616fe7e4d89892fa831f26308965e00630
thetarnav <gthetarnav@gmail.com>
2022-02-19 14:57:44 +0100

    context: add demo

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -5,11 +5,4 @@ export type CreateContextProvider = <T, P extends Record<string, any>>(
   factoryFn: (props: P) => T
 ) => [provider: Component<P>, useContext: () => T | undefined];
 
-export type CreateDefiniteContextProvider = <T, P extends Record<string, any>>(
-  factoryFn: (props: P) => T
-) => [provider: Component<P>, useContext: () => T];
-
-export const definite = <T>(v: T) => v as NonNullable<T>;
-
-export const createDefiniteContextProvider = createContextProvider as CreateDefiniteContextProvider;
 export { createContextProvider };
