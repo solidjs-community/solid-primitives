8a2a0dd666184f565855937e808b1c61de047e5e
thetarnav <gthetarnav@gmail.com>
2022-03-08 22:55:31 +0100

    destructure: package refactor merge and refactor `spread` & `destructure` add `wrapGetters` primitive

--- packages/destructure/src/index.ts
+++ packages/destructure/src/index.ts
@@ -1,97 +1,200 @@
-import { createMemo, Accessor } from "solid-js";
+import { createMemo, Accessor, runWithOwner, getOwner, onCleanup, createRoot } from "solid-js";
 import {
   access,
   isFunction,
-  keys,
   MaybeAccessor,
-  MaybeAccessorValue,
   AnyObject,
   Values,
-  Definite,
-  NonIterable
+  isObject,
+  isArray,
+  AnyFunction,
+  Fn
 } from "@solid-primitives/utils";
 import type { MemoOptions } from "solid-js/types/reactive/signal";
+import { Store } from "solid-js/store";
 
-type ReactiveSource = MaybeAccessor<[] | any[] | AnyObject>;
-type AccessorProps<T> = {
-  [K in keyof T]: Accessor<T[K]>;
+type ReactiveSource = [] | any[] | AnyObject;
+
+export type DestructureOptions<T extends ReactiveSource> = MemoOptions<Values<T>> & {
+  cache?: boolean;
+  lazy?: boolean;
+  deep?: boolean;
+};
+export type GettersOptions = MemoOptions<any> & {
+  deep?: boolean;
 };
 
-export type SpreadOptions<T extends ReactiveSource> = MemoOptions<Values<T>> & { cache?: boolean };
+export type Spread<T extends ReactiveSource> = {
+  readonly [K in keyof T]: Accessor<T[K]>;
+};
+export type DeepSpread<T extends ReactiveSource> = {
+  readonly [K in keyof T]: T[K] extends ReactiveSource
+    ? T[K] extends AnyFunction
+      ? Accessor<T[K]>
+      : DeepSpread<T[K]>
+    : Accessor<T[K]>;
+};
+export type Destructure<T extends ReactiveSource> = {
+  readonly [K in keyof T]-?: Accessor<T[K]>;
+};
+export type DeepDestructure<T extends ReactiveSource> = {
+  readonly [K in keyof T]-?: T[K] extends ReactiveSource
+    ? T[K] extends AnyFunction
+      ? Accessor<T[K]>
+      : DeepDestructure<T[K]>
+    : Accessor<T[K]>;
+};
 
-export type Spread<T extends ReactiveSource> = Readonly<AccessorProps<T>>;
-
-export type Destructure<T extends ReactiveSource> = Readonly<
-  NonIterable<Definite<AccessorProps<T>>>
->;
+const isReactiveObject = (value: any): boolean => isObject(value) || isArray(value);
 
 /**
  * Cashed object getters.
  * @description When a key is accessed for the first time, the `get` function is executed, later a cached value is used instead.
  */
-function createProxyCache<T extends AnyObject>(
-  get: <K extends keyof T>(key: K) => T[K]
-): Readonly<NonIterable<T>> {
-  return new Proxy({} as NonIterable<T>, {
-    get: (target, key) => {
-      const saved = Reflect.get(target, key);
-      if (saved) return saved;
-      const value = get(key as keyof T);
-      Reflect.set(target, key, value);
-      return value;
+function createProxyCache(obj: object, get: (key: any) => any): any {
+  return new Proxy(
+    {},
+    {
+      get: (target, key) => {
+        if (key === Symbol.iterator || key === "length") return Reflect.get(obj, key);
+        const saved = Reflect.get(target, key);
+        if (saved) return saved;
+        const value = get(key);
+        Reflect.set(target, key, value);
+        return value;
+      },
+      set: () => false
     }
-  });
+  );
 }
 
 /**
- * Access properties of an reactive object as if they were signals. For destructuring props, stores, or signals returning reactive objects.
- * @description result keys are created lazily, so looping over the keys is discouraged.
+ * Destructures an reactive object *(e.g. store or component props)* or a signal of one into a tuple/map of signals for each object key.
  * @param source reactive object or signal returning one
+ * @param options memo options + primitive configuration:
+ * - `cache` - wraps accessors in `createMemo`, making each property update independently. *(enabled by default for signal source)*
+ * - `lazy` - property accessors are created on key read. enable if you want to only a subset of source properties, or use properties initially missing
+ * - `deep` - destructure nested objects
  * @returns object of the same keys as the source, but with values turned into accessors.
- * @example // destructure objects
- * const { name, age } = destructure({ name: "John", age: 36 })
- * name() // => "John"
- * age() // => 36
- */
-export function destructure<A extends ReactiveSource, T extends MaybeAccessorValue<A>>(
-  source: A,
-  options: SpreadOptions<T> = {}
-): Destructure<T> {
-  const cache = options.cache ?? isFunction(source);
-  return createProxyCache(key => {
-    const calc = isFunction(source)
-      ? () => Reflect.get(source(), key)
-      : () => Reflect.get(source, key);
-    return cache ? createMemo(calc, options) : calc;
-  });
-}
-
-/**
- * Spreads an reactive object *(store or props)* or a reactive object signal into a tuple/map of signals for each object key. **(source object needs to have static keys – all the keys are eagerly spread)**
- * @param source reactive object or signal returning one
  * @example // spread tuples
- * const [first, second, third] = spread(() => [1,2,3])
+ * const [first, second, third] = destructure(() => [1,2,3])
  * first() // => 1
  * second() // => 2
  * third() // => 3
  * @example // spread objects
- * const { name, age } = spread({ name: "John", age: 36 })
+ * const { name, age } = destructure({ name: "John", age: 36 })
  * name() // => "John"
  * age() // => 36
  */
-export function spread<A extends ReactiveSource, T extends MaybeAccessorValue<A>>(
-  source: A,
-  options: SpreadOptions<T> = {}
-): Spread<T> {
-  const cache = options.cache ?? isFunction(source);
+export function destructure<T extends ReactiveSource, O extends DestructureOptions<T>>(
+  source: MaybeAccessor<T>,
+  options?: O
+): O extends { lazy: true; deep: true }
+  ? DeepDestructure<T>
+  : O["lazy"] extends true
+  ? Destructure<T>
+  : O["deep"] extends true
+  ? DeepSpread<T>
+  : Spread<T> {
+  const config: DestructureOptions<T> = options ?? {};
+  const cache = config.cache ?? isFunction(source);
   const getter = isFunction(source)
     ? (key: any) => () => source()[key]
     : (key: any) => () => source[key];
   const obj = access(source);
+
+  // lazy (use proxy)
+  if (config.lazy) {
+    const owner = getOwner()!;
+    return createProxyCache(obj, key => {
+      const calc = getter(key);
+      if (config.deep && isReactiveObject(obj[key]))
+        return runWithOwner(owner, () => destructure(calc, { ...config, cache }));
+      return cache ? runWithOwner(owner, () => createMemo(calc, undefined, options)) : calc;
+    });
+  }
+
+  // eager (loop keys)
   const result = obj.constructor();
-  for (const key of keys(obj)) {
+  for (const [key, value] of Object.entries(obj)) {
     const calc = getter(key);
-    result[key] = cache ? createMemo(calc) : calc;
+    if (config.deep && isReactiveObject(value))
+      result[key] = destructure(calc, { ...config, cache });
+    else result[key] = cache ? createMemo(calc, undefined, options) : calc;
   }
   return result;
+}
+
+function createProxyCacheGet<T extends ReactiveSource>(
+  get: (key: keyof T) => Values<T>,
+  options?: MemoOptions<Values<T>>
+): [proxy: Readonly<T>, dispose: Fn] {
+  const [root, dispose] = createRoot(dispose => [getOwner()!, dispose]);
+  const getMemo = (key: keyof T) =>
+    runWithOwner(root, () => createMemo(() => get(key), undefined, options));
+
+  const proxy = new Proxy(
+    {},
+    {
+      get(obj, key) {
+        const saved = Reflect.get(obj, key);
+        if (saved) saved();
+        const memo = getMemo(key as keyof T);
+        Reflect.set(obj, key, memo);
+        return memo();
+      },
+      set: () => false
+    }
+  ) as T;
+  return [proxy, dispose];
+}
+
+/**
+ * Wraps object/array signal with getters for every key, making accessing properties similar to component props/store. Properties are cached with memos on access, so th primitive needs to be used in a reactive context.
+ * @param signal signal source of the object to wrap
+ * @param options memo options object + `deep` for wrapping nested objects as well (by default source object is wrapped shallowly)
+ * @returns wrapped readonly object with reactive getters
+ * @example
+ * const [getUser, setUser] = createSignal({
+ *    name: "John",
+ *    age: 35,
+ *    weaponOfChoice: "Machete"
+ * })
+ * const user = wrapGetters(getUser)
+ * user.age // => 35 (reactive on updates to that property)
+ */
+export function wrapGetters<T extends ReactiveSource, O extends GettersOptions>(
+  signal: Accessor<T>,
+  options?: O
+): O["deep"] extends true ? Store<T> : Readonly<T> {
+  const proxies = new Map<object, { proxy: object; dispose: Fn }>();
+  let trash: Set<object> | undefined;
+  onCleanup(() => proxies.forEach(({ dispose }) => dispose()));
+  return wrap(signal(), signal);
+
+  function wrap<T extends ReactiveSource>(obj: T, signal: Accessor<T>): T {
+    if (!trash) {
+      // dispose and remove saved objects that weren't used from previous iteration
+      trash = new Set(proxies.keys());
+      queueMicrotask(() => {
+        trash?.forEach(obj => {
+          proxies.get(obj)!.dispose();
+          proxies.delete(obj);
+        });
+        trash = undefined;
+      });
+    }
+    if (proxies.has(obj)) {
+      trash.delete(obj);
+      return proxies.get(obj)!.proxy as T;
+    }
+
+    const [proxy, dispose] = createProxyCacheGet<T>(key => {
+      const value = signal()[key];
+      return options?.deep && isReactiveObject(value) ? wrap(value, () => signal()[key]) : value;
+    }, options);
+
+    proxies.set(obj, { proxy, dispose });
+    return proxy;
+  }
 }
--- packages/destructure/test/index.test.ts
+++ packages/destructure/test/index.test.ts
@@ -2,14 +2,14 @@ import { suite } from "uvu";
 import * as assert from "uvu/assert";
 import { createComputed, createRoot, createSignal } from "solid-js";
 import { tuple } from "@solid-primitives/utils";
-import { destructure, spread } from "../src";
+import { destructure } from "../src";
 
 const testSpread = suite("spread");
 
 testSpread("spread array", () =>
   createRoot(dispose => {
     const [numbers, setNumbers] = createSignal(tuple([1, 2, 3]));
-    const [first, second, last] = spread(numbers);
+    const [first, second, last] = destructure(numbers);
 
     const updates = {
       a: 0,
@@ -51,85 +51,6 @@ testSpread("spread array", () =>
 );
 
 testSpread("spread object", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal({
-      a: 1,
-      b: 2,
-      c: 3
-    });
-    const { a, b, c } = spread(numbers);
-
-    const updates = {
-      a: 0,
-      b: 0,
-      c: 0
-    };
-    createComputed(() => {
-      a();
-      updates.a++;
-    });
-    createComputed(() => {
-      b();
-      updates.b++;
-    });
-    createComputed(() => {
-      c();
-      updates.c++;
-    });
-
-    assert.is(a(), 1);
-    assert.is(b(), 2);
-    assert.is(c(), 3);
-
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 1);
-    assert.is(updates.c, 1);
-
-    setNumbers({
-      a: 1,
-      b: 6,
-      c: 7
-    });
-    assert.is(a(), 1);
-    assert.is(b(), 6);
-    assert.is(c(), 7);
-
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 2);
-    assert.is(updates.c, 2);
-
-    dispose();
-  })
-);
-
-testSpread("spread is lazy", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
-      a: 0
-    });
-
-    const { a, b } = spread(numbers);
-
-    assert.is(a(), 0);
-    assert.is(b, undefined);
-
-    setNumbers({
-      a: 2,
-      b: 3
-    });
-
-    assert.is(a(), 2);
-    assert.is(b, undefined);
-
-    dispose();
-  })
-);
-
-testSpread.run();
-
-const testDest = suite("destructure");
-
-testDest("destructure object", () =>
   createRoot(dispose => {
     const [numbers, setNumbers] = createSignal({
       a: 1,
@@ -181,13 +102,92 @@ testDest("destructure object", () =>
   })
 );
 
-testDest("destructure is lazy", () =>
+testSpread("spread is eager", () =>
   createRoot(dispose => {
     const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
       a: 0
     });
 
     const { a, b } = destructure(numbers);
+
+    assert.is(a(), 0);
+    assert.is(b, undefined);
+
+    setNumbers({
+      a: 2,
+      b: 3
+    });
+
+    assert.is(a(), 2);
+    assert.is(b, undefined);
+
+    dispose();
+  })
+);
+
+testSpread.run();
+
+const testDest = suite("destructure");
+
+testDest("destructure object", () =>
+  createRoot(dispose => {
+    const [numbers, setNumbers] = createSignal({
+      a: 1,
+      b: 2,
+      c: 3
+    });
+    const { a, b, c } = destructure(numbers, { lazy: true });
+
+    const updates = {
+      a: 0,
+      b: 0,
+      c: 0
+    };
+    createComputed(() => {
+      a();
+      updates.a++;
+    });
+    createComputed(() => {
+      b();
+      updates.b++;
+    });
+    createComputed(() => {
+      c();
+      updates.c++;
+    });
+
+    assert.is(a(), 1);
+    assert.is(b(), 2);
+    assert.is(c(), 3);
+
+    assert.is(updates.a, 1);
+    assert.is(updates.b, 1);
+    assert.is(updates.c, 1);
+
+    setNumbers({
+      a: 1,
+      b: 6,
+      c: 7
+    });
+    assert.is(a(), 1);
+    assert.is(b(), 6);
+    assert.is(c(), 7);
+
+    assert.is(updates.a, 1);
+    assert.is(updates.b, 2);
+    assert.is(updates.c, 2);
+
+    dispose();
+  })
+);
+
+testDest("destructure is lazy", () =>
+  createRoot(dispose => {
+    const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
+      a: 0
+    });
+
+    const { a, b } = destructure(numbers, { lazy: true });
 
     assert.is(a(), 0);
     assert.is(b(), undefined);
