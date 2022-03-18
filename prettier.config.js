92de157fe39fc5fa78ff565a735c833203bc5b17
Joe Pea <joe@trusktr.io>
2022-03-18 11:34:26 -0700

    format prettier config with itself

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "trailingComma": "none",
-  "tabWidth": 2,
-  "semi": true,
-  "singleQuote": false,
-  "arrowParens": "avoid",
-  "printWidth": 100
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+  printWidth: 100
 };
