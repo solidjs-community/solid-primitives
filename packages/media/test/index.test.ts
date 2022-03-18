aebc5119e2bd3ee3a3b06e15ddfacaf971d519ae
Aditya Agarwal <adityaa803@gmail.com>
2022-03-18 23:19:38 +0530

    add vitest tests

--- packages/media/test/index.test.ts
+++ packages/media/test/index.test.ts
@@ -1,48 +1,162 @@
-import { suite } from "uvu";
-import * as assert from "uvu/assert";
-import { createRoot } from "solid-js";
+import { describe, test, expect, vi, beforeEach, beforeAll, afterEach, afterAll } from "vitest";
+import { createEffect, createRoot, onMount } from "solid-js";
 import createMediaQuery, { createBreakpoints } from "../src/index";
 
-suite("createMediaQuery", (): void => {});
+describe("createMediaQuery", () => {
+  const breakpoints = {
+    sm: "640px",
+    lg: "1024px",
+    xl: "1280px"
+  } as const;
 
-const testResponsive = suite("createBreakpoints");
+  const matchMediaMock = vi.fn();
+  const addListenerMock = vi.fn();
+  const removeListenerMock = vi.fn();
+  let originalMatchMedia, matchingBreakpoints;
 
-const breakpoints = {
-  sm: "640px",
-  lg: "1024px",
-  xl: "1280px"
-} as const;
+  function checkMatch(query: string) {
+    const isMatch = matchingBreakpoints.some(breakpoint => query.includes(breakpoint));
+    return isMatch;
+  }
 
-let originalWindow = global.window;
+  beforeAll(() => {
+    originalMatchMedia = window.matchMedia;
+    window.matchMedia = matchMediaMock;
+  });
 
-testResponsive.before.each(() => {
-  global.window = {
-    matchMedia(query) {
+  beforeEach(() => {
+    matchingBreakpoints = [];
+    matchMediaMock.mockImplementation((query: string) => {
       return {
-        matches: false,
+        matches: checkMatch(query),
         query,
-        addEventListener() {},
-        removeEventListener() {}
+        addEventListener: addListenerMock,
+        removeEventListener: removeListenerMock
       };
-    }
-  };
-});
-
-testResponsive.after.each(() => {
-  global.window = originalWindow;
-});
-
-testResponsive("give the smallest breakpoint by default", () => {
-  createRoot(dispose => {
-    const { matches, minMatch } = createBreakpoints(breakpoints);
-    assert.equal(matches(), {
-      sm: false,
-      lg: false,
-      xl: false
     });
-    assert.equal(minMatch('sm'), false);
-    dispose();
+  });
+
+  afterAll(() => {
+    window.matchMedia = originalMatchMedia;
+  });
+
+  test("give the smallest breakpoint by default", () => {
+    createRoot(dispose => {
+      const { matches, minMatch } = createBreakpoints(breakpoints);
+      expect(matches()).toEqual({
+        sm: false,
+        lg: false,
+        xl: false
+      });
+      expect(minMatch("sm")).toEqual(false);
+      expect(minMatch("unknown")).toEqual(false);
+      dispose();
+    });
+  });
+
+  test("give sm breakpoint when min-width: 640px media query matches", () => {
+    createRoot(dispose => {
+      matchingBreakpoints = [breakpoints.sm];
+      const { matches, minMatch } = createBreakpoints(breakpoints);
+      expect(matches()).toEqual({
+        sm: true,
+        lg: false,
+        xl: false
+      });
+      expect(minMatch("sm")).toEqual(true);
+      expect(minMatch("lg")).toEqual(false);
+      dispose();
+    });
+  });
+
+  test("give lg breakpoint when min-width: 1024px media query matches", () => {
+    createRoot(dispose => {
+      matchingBreakpoints = [breakpoints.sm, breakpoints.lg];
+      const { matches, minMatch } = createBreakpoints(breakpoints);
+      expect(matches()).toEqual({
+        sm: true,
+        lg: true,
+        xl: false
+      });
+      expect(minMatch("sm")).toEqual(true);
+      expect(minMatch("lg")).toEqual(true);
+      expect(minMatch("xl")).toEqual(false);
+      dispose();
+    });
+  });
+
+  test("update breakpoint when media query change event is triggered", async () => {
+    const actualOutput = await new Promise(resolve => {
+      createRoot(dispose => {
+        const { matches, minMatch } = createBreakpoints(breakpoints);
+        const output = [];
+
+        createEffect(() => {
+          output.push({
+            matches: matches(),
+            minMatch: {
+              sm: minMatch("sm"),
+              lg: minMatch("lg")
+            }
+          });
+
+          if (output.length >= 2) {
+            dispose();
+            resolve(output);
+          }
+        });
+
+        onMount(() => {
+          const smListener = addListenerMock.mock.calls[0][1];
+          const lgListener = addListenerMock.mock.calls[1][1];
+          smListener({
+            matches: true
+          });
+          lgListener({
+            matches: true
+          });
+        });
+      });
+    });
+
+    expect(actualOutput).toMatchInlineSnapshot(`
+      [
+        {
+          "matches": {
+            "lg": false,
+            "sm": false,
+            "xl": false,
+          },
+          "minMatch": {
+            "lg": false,
+            "sm": false,
+          },
+        },
+        {
+          "matches": {
+            "lg": true,
+            "sm": true,
+            "xl": false,
+          },
+          "minMatch": {
+            "lg": true,
+            "sm": true,
+          },
+        },
+      ]
+    `);
+    expect(removeListenerMock).toBeCalledTimes(3);
+  });
+
+  test("do not setup listeners if watchChange is false", () => {
+    createRoot(dispose => {
+      createBreakpoints(breakpoints, false);
+
+      onMount(() => {
+        expect(addListenerMock).toBeCalledTimes(0);
+        dispose();
+      });
+    });
+    expect(removeListenerMock).toBeCalledTimes(0);
   });
 });
-
-testResponsive.run();
