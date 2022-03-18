bf3bba2a0d7d5d41d6c605e0000f3886c1255ee8
Aditya Agarwal <adityaa803@gmail.com>
2022-03-18 21:07:11 +0530

    chore(media): add createResponsive primitive

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,4 +1,4 @@
-import { createSignal, getOwner, onCleanup } from "solid-js";
+import { createEffect, createMemo, createSignal, getOwner, onCleanup } from "solid-js";
 import { isServer } from "solid-js/web";
 
 /**
@@ -34,6 +34,89 @@ const createMediaQuery = (
   }
   const [state, setState] = createSignal(initialState);
   return state;
+};
+
+type Breakpoints = Record<string, string>;
+type MqlInstances = Record<string, MediaQueryList>;
+type Matches = Record<string, Boolean>;
+
+/**
+ * Creates a multi-breakpoint monitor to make responsive components easily.
+ * 
+ * @param breakpoints Map of breakpoint names and their widths
+ * @param boolean If true watches changes and reports state reactively
+ * 
+ * @example
+ * ```ts
+ * const breakpoints = {
+    sm: "640px",
+    lg: "1024px",
+    xl: "1280px",
+  } as const;
+ * const { minMatch, matches } = createMatchBreakpoints(breakpoints);
+ * console.log(minMatch('lg'), matches());
+ * ```
+ */
+export const createResponsive = (breakpoints: Breakpoints, watchChange: boolean = true) => {
+  const mqlInstances = createMemo(() => {
+    const mqlInstances: MqlInstances = {};
+
+    Object.entries(breakpoints).forEach(([token, width]) => {
+      const mediaquery = `(min-width: ${width})`;
+      const instance = window.matchMedia(mediaquery);
+
+      mqlInstances[token] = instance;
+    });
+
+    return mqlInstances;
+  });
+
+  function getInitialMatches() {
+    const matches: Matches = {};
+
+    Object.entries(mqlInstances()).forEach(([token, mql]) => {
+      matches[token] = mql.matches;
+    });
+
+    return matches;
+  }
+
+  const [matches, setMatches] = createSignal(getInitialMatches());
+
+  function minMatch(token: string) {
+    const isMatching = matches()[token];
+
+    return Boolean(isMatching);
+  }
+
+  createEffect(() => {
+    if (!watchChange) {
+      return;
+    }
+
+    Object.entries(mqlInstances()).forEach(([token, mql]) => {
+      const listener = (event: MediaQueryListEvent) => {
+        setMatches(prev => {
+          return {
+            ...prev,
+            [token]: event.matches
+          };
+        });
+      };
+      mql.addEventListener("change", listener);
+
+      onCleanup(() => {
+        mql.removeEventListener("change", listener);
+      });
+    });
+  });
+
+  return {
+    /** returns `true` if screen is matching given breakpoint or higher */
+    minMatch,
+    /** accessor for map of currently matching breakpoints. */
+    matches
+  };
 };
 
 export default createMediaQuery;
--- packages/media/test/index.test.ts
+++ packages/media/test/index.test.ts
@@ -1,4 +1,48 @@
-import { render } from "solid-testing-library";
-import createMediaQuery from "../src/index";
+import { suite } from "uvu";
+import * as assert from "uvu/assert";
+import { createRoot } from "solid-js";
+import createMediaQuery, { createResponsive } from "../src/index";
 
-describe("createMediaQuery", (): void => {});
+suite("createMediaQuery", (): void => {});
+
+const testResponsive = suite("createResponsive");
+
+const breakpoints = {
+  sm: "640px",
+  lg: "1024px",
+  xl: "1280px"
+} as const;
+
+let originalWindow = global.window;
+
+testResponsive.before.each(() => {
+  global.window = {
+    matchMedia(query) {
+      return {
+        matches: false,
+        query,
+        addEventListener() {},
+        removeEventListener() {}
+      };
+    }
+  };
+});
+
+testResponsive.after.each(() => {
+  global.window = originalWindow;
+});
+
+testResponsive("give the smallest breakpoint by default", () => {
+  createRoot(dispose => {
+    const { matches, minMatch } = createResponsive(breakpoints);
+    assert.equal(matches(), {
+      sm: false,
+      lg: false,
+      xl: false
+    });
+    assert.equal(minMatch('sm'), false);
+    dispose();
+  });
+});
+
+testResponsive.run();
