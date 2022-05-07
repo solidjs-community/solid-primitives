d5aa90132cff3335b6c1db0578b96711db207719
David Di Biase <ddibiase@MacBook-Pro.local>
2022-05-07 16:05:55 -0400

    Changed output of createAudio to a store

--- packages/audio/src/index.ts
+++ packages/audio/src/index.ts
@@ -1,4 +1,5 @@
 import { createSignal, Accessor, batch, onMount, onCleanup, createEffect } from "solid-js";
+import { createStore, Store } from "solid-js/store";
 
 // Set of control enums
 export enum AudioState {
@@ -21,7 +22,7 @@ type AudioSource =
  *
  * @param src Audio file path or MediaSource to be played
  * @param handlers An array of handlers to bind against the player
- * @return
+ * @return A basic audio player instance
  */
 export const makeAudio = (
   src: string | undefined,
@@ -48,13 +49,15 @@ export const makeAudio = (
  * @param src Audio file path or MediaSource to be played
  * @return options - @type Object
  * @return options.start - Start playing
- * @return options.stop - Stop playing
- * @return options.state - Current state of the player as a Symbol
+ * @return options.pause - Pause playing
+ * @return options.seek - Seeks to a location in the playhead
+ * @return options.setVolume - Sets the volume of the player
+ * @return options.player - Raw player instance
  * @return Returns a location signal and one-off async query callback
  *
  * @example
  * ```ts
- * const [start, pause] = makeAudio('./example1.mp3);
+ * const { start, pause } = makeAudio('./example1.mp3);
  * ```
  */
 export const makeAudioPlayer = (
@@ -78,7 +81,7 @@ export const makeAudioPlayer = (
 };
 
 /**
- * Creates a simple audio manager with most control actions.
+ * Creates an audio primitive with basic control actions.
  *
  * @param src Audio file path or MediaSource to be played
  * @param volume Volume setting for the audio file
@@ -90,35 +93,42 @@ export const makeAudioPlayer = (
  * @return options.duration - Duration of the audio clip
  * @return options.seek - A function to support seeking
  * @return options.setVolume - A function to change the volume setting
- * @return Returns a location signal and one-off async query callback
+ * @return store - @type Store
+ * @return store.currentTime - Current time of the playhead
+ * @return store.state - Current state of the player
+ * @return store.duration - Duratio of the audio player
  *
  * @example
  * ```ts
- * const [start, pause, seek, currentTime, duration] = createAudio('./example1.mp3, playing);
+ * const [playing setPlaying] = createSignal(false);
+ * const { state, currentTime, duration } = createAudio('./example1.mp3, playing);
+ * setPlaying(true);
  * ```
  */
 export const createAudio = (
   src: AudioSource,
   playing: Accessor<boolean>,
   volume?: Accessor<number>
-): {
-  state: () => AudioState;
-  currentTime: () => number;
-  duration: () => number;
-} => {
-  const [state, setState] = createSignal<AudioState>(AudioState.STOPPED);
-  const [currentTime, setCurrentTime] = createSignal<number>(0);
-  const [duration, setDuration] = createSignal<number>(0);
+): Store<{
+  state: AudioState,
+  currentTime: number,
+  duration: number
+}> => {
+  const [store, setStore] = createStore({
+    currentTime: 0,
+    duration: 0,
+    state: AudioState.STOPPED,
+  });
   const { play, pause, setVolume, player } = makeAudioPlayer(src, {
     loadeddata: () =>
       batch(() => {
-        setState(AudioState.READY);
-        setDuration(player.duration);
+        setStore('state', AudioState.READY);
+        setStore('duration', player.duration);
       }),
-    timeupdate: () => setCurrentTime(player.currentTime),
-    loadstart: () => setState(AudioState.LOADING),
-    playing: () => setState(AudioState.PLAYING),
-    pause: () => setState(AudioState.PAUSED)
+    timeupdate: () => setStore('currentTime', player.currentTime),
+    loadstart: () => setStore('state', AudioState.LOADING),
+    playing: () => setStore('state', AudioState.PLAYING),
+    pause: () => setStore('state', AudioState.PAUSED)
   });
   if (src instanceof Function) {
     const srcKey = typeof src() === "string" ? "src" : "srcObject";
@@ -129,5 +139,5 @@ export const createAudio = (
   if (typeof volume !== "undefined") {
     createEffect(() => setVolume(volume()));
   }
-  return { currentTime, state, duration };
+  return store;
 };
