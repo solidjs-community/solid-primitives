2943ea2936aca1bd081bd5f50242a0a489b36451
Damian Tarnawski <gthetarnav@gmail.com>
2022-05-26 11:47:45 +0200

    remove useless if check, add jsdoc

--- packages/props/src/omitAndPick.ts
+++ packages/props/src/omitAndPick.ts
@@ -1,3 +1,14 @@
+/**
+ * Create a new subset props object by **omitting unwanted keys** from the source props.
+ * @param props source props object
+ * @param keys property names of {@link props} to omit
+ * @returns new reactive props object with selected {@link keys} removed
+ * @example
+ * ```tsx
+ * const attrs = omitProps({ count: number, selected: boolean, class?: string }, "count", "selected")
+ * <div {...attrs}>Hello!</div>
+ * ```
+ */
 export function omitProps<T extends Record<string, unknown>, K extends keyof T>(
   props: T,
   ...keys: K[]
@@ -5,12 +16,22 @@ export function omitProps<T extends Record<string, unknown>, K extends keyof T>(
   const newProps = {} as Omit<T, K>;
   for (const key in props) {
     if (keys.includes(key as any)) continue;
-    const descriptor = Object.getOwnPropertyDescriptor(props, key);
-    descriptor && Object.defineProperty(newProps, key, descriptor);
+    Object.defineProperty(newProps, key, Object.getOwnPropertyDescriptor(props, key)!);
   }
   return newProps;
 }
 
+/**
+ * Create a new subset props object by **selecting only specified keys** from the source props.
+ * @param props source props object
+ * @param keys property names of {@link props} to include in returned object
+ * @returns new reactive props object with selected {@link keys}
+ * @example
+ * ```tsx
+ * const attrs = pickProps({ count: number, checked: boolean, class?: string }, "class", "checked")
+ * <input type="checkbox" {...attrs}/>
+ * ```
+ */
 export function pickProps<T extends Record<string, unknown>, K extends keyof T>(
   props: T,
   ...keys: K[]
