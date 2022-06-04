ad88fc8e160240ec70bc41ecd95c67a0cc468ea6
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-04 13:22:51 +0200

    refactor scroll package

--- packages/scroll/src/index.ts
+++ packages/scroll/src/index.ts
@@ -1,35 +1,72 @@
-import { createSignal, onCleanup, createEffect } from "solid-js";
+import { access, createStaticStore, MaybeAccessor } from "@solid-primitives/utils";
+import { createEventListener } from "@solid-primitives/event-listener";
+import { createComputed, on, onMount } from "solid-js";
 
 /**
- * Create a primitive for monitoring scrolling.
- *
- * @param target - Element or window to target, defaults to window
- *
- * @example
- * ```ts
- * let ref;
- * const position = createScrollObserver(() => ref)
- * ```
+ * Get an `{ x: number, y: number }` object of element/window scroll position.
  */
-const createScrollObserver = <T extends HTMLElement>(
-  target: () => T | Window = () => window
-): (() => number | null) => {
-  const getPosition = (): number | null =>
-    // @ts-ignore
-    target && typeof target() !== "undefined" ? target().pageYOffset ?? target().scrollTop : null;
-  const [position, setPosition] = createSignal<number | null>(getPosition());
-  const handleScroll = () => setPosition(getPosition());
-  const remove = () => target() && target().removeEventListener("scroll", handleScroll);
-  createEffect(() => {
-    if (!target()) {
-      return;
-    } else if (target()) {
-      remove();
-    }
-    target().addEventListener("scroll", handleScroll);
-  });
-  onCleanup(remove);
-  return position;
+export function getScrollPosition(target: Element | Window): {
+  x: number;
+  y: number;
 };
+export function getScrollPosition(target: Element | Window | undefined): {
+  x: number | null;
+  y: number | null;
+};
+export function getScrollPosition(target: Element | Window | undefined): {
+  x: number | null;
+  y: number | null;
+} {
+  if (!target) return { x: null, y: null };
+  if (target instanceof Window)
+    return {
+      x: target.scrollX,
+      y: target.scrollY
+    };
+  return {
+    x: target.scrollLeft,
+    y: target.scrollTop
+  };
+}
 
-export default createScrollObserver;
+/**
+ * Reactive primitive providing a store-like object with current scroll position of specified target.
+ * @param target element/window to listen to scroll events. can be a reactive singal.
+ * @returns a store-like reactive object `{ x: number, y: number }` of current scroll position of {@link target}
+ * @example
+ * // target will be window by default
+ * const windowScroll = createScrollPosition();
+ *
+ * createEffect(() => {
+ *   // returned object is a reactive store-like structure
+ *   windowScroll.x; // => number
+ *   windowScroll.y; // => number
+ * });
+ */
+export function createScrollPosition(target?: MaybeAccessor<Window>): {
+  readonly x: number;
+  readonly y: number;
+};
+export function createScrollPosition(target: MaybeAccessor<Element | Window>): {
+  readonly x: number;
+  readonly y: number;
+};
+export function createScrollPosition(target: MaybeAccessor<Element | Window | undefined>): {
+  readonly x: number | null;
+  readonly y: number | null;
+};
+export function createScrollPosition(
+  target: MaybeAccessor<Element | Window | undefined> = window
+): {
+  readonly x: number | null;
+  readonly y: number | null;
+} {
+  const [pos, setPos] = createStaticStore(getScrollPosition(access(target)));
+  const updatePos = () => setPos(getScrollPosition(access(target)));
+  if (typeof target === "function") {
+    createComputed(on(target, ref => setPos(getScrollPosition(ref)), { defer: true }));
+    onMount(updatePos);
+  }
+  createEventListener(target, "scroll", updatePos);
+  return pos;
+}
