f24206412fb5b3bf06b62b46df1508548ea70540
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-09 20:09:24 +0200

    refactor visibility-observer pkg

--- packages/visibility-observer/src/index.ts
+++ packages/visibility-observer/src/index.ts
@@ -1,20 +1,38 @@
-import { onMount, createSignal, onCleanup, Accessor } from "solid-js";
+import { createSignal, Accessor } from "solid-js";
+import { makeEventListener } from "@solid-primitives/event-listener";
+import { createSharedRoot } from "@solid-primitives/rootless";
 
 /**
- * Create a visibility observer is a helper primitive for tracking when the page is visible.
+ * Creates a signal with a boolean value identifying the page visibility state.
  *
- * @return Returns a signel with a boolean value identifying the visibility state.
+ * @example
+ * ```ts
+ * const visible = createPageVisibility();
  *
+ * createEffect(() => {
+ *    visible() // => boolean
+ * })
+ * ```
  */
-const createPageVisibilityObserver = (): Accessor<boolean> => {
-  const visible = () => (document ? document.visibilityState === "visible" : true);
-  const [state, setState] = createSignal(visible());
-  const cb = () => setState(visible());
-  onMount(() => {
-    document.addEventListener("visibilitychange", cb);
-    onCleanup(() => document.removeEventListener("visibilitychange", cb));
-  });
+export const createPageVisibility = (): Accessor<boolean> => {
+  const [state, setState] = createSignal(document.visibilityState === "visible");
+  const cb = () => setState(document.visibilityState === "visible");
+  makeEventListener(document, "visibilitychange", cb);
   return state;
 };
 
-export default createPageVisibilityObserver;
+/**
+ * Returns a signal with a boolean value identifying the page visibility state.
+ *
+ * This is a [shared root](https://github.com/solidjs-community/solid-primitives/tree/main/packages/rootless#createSharedRoot) primitive.
+ *
+ * @example
+ * ```ts
+ * const visible = usePageVisibility();
+ *
+ * createEffect(() => {
+ *    visible() // => boolean
+ * })
+ * ```
+ */
+export const usePageVisibility = createSharedRoot(createPageVisibility);
--- packages/visibility-observer/src/server.ts
+++ packages/visibility-observer/src/server.ts
@@ -1,13 +1,5 @@
-import { Accessor } from "solid-js";
+import * as API from "./index";
 
-/**
- * Create a visibility observer is a helper primitive for tracking when the page is visible.
- *
- * @return Returns a signel with a boolean value identifying the visibility state.
- *
- */
-const createPageVisibilityObserver = (): Accessor<boolean> => {
-  return () => true;
-};
+export const createPageVisibility: typeof API.createPageVisibility = () => () => true;
 
-export default createPageVisibilityObserver;
+export const usePageVisibility: typeof API.usePageVisibility = () => () => true;
