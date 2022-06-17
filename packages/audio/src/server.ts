e4d0a8d98dd6785d7a1a2124360449f1e296cc40
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-17 23:05:46 +0200

    correct server.ts

--- packages/audio/src/server.ts
+++ packages/audio/src/server.ts
@@ -1,31 +1,28 @@
-export const makeAudio = (
-  _src: AudioSource,
-  _handlers: Array<[string, EventListener]>
-): HTMLAudioElement => ({} as HTMLAudioElement);
+import { noop } from "@solid-primitives/utils";
+import * as API from "./index";
 
-export const makeAudioPlayer = (
-  _src: AudioSource
-): HTMLAudioElement => {
-  return {} as HTMLAudioElement;
-};
+export const makeAudio: typeof API.makeAudio = () => ({} as HTMLAudioElement);
 
-export const createAudioManager = (
-  _src: AudioSource,
-  _volume: number = 1
-): {
-  seek: (time: number) => void,
-  state: AudioState,
-  currentTime: number,
-  duration: number,
-  player: HTMLAudioElement
-} => {
-  return {
-    state: AudioState.LOADING,
-    duration: 0,
-    seek: (_time: number) => {
-      /* noop */
-    },
+export const makeAudioPlayer: typeof API.makeAudioPlayer = () => ({
+  pause: noop,
+  play: noop,
+  player: {} as HTMLAudioElement,
+  seek: noop,
+  setVolume: noop
+});
+
+export const createAudio: typeof API.createAudio = () => [
+  {
+    state: API.AudioState.LOADING,
     currentTime: 0,
+    duration: 0,
+    volume: 0,
     player: {} as HTMLAudioElement
-  };
-};
+  },
+  {
+    seek: noop,
+    setVolume: noop,
+    play: noop,
+    pause: noop
+  }
+];
