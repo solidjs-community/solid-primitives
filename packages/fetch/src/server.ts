5e2f2d105f3935e6492cdb5cbc236a04f28564b5
atk <atk@users.noreply.github.com>
2022-08-15 04:23:29 +0000

    Format

--- packages/fetch/src/server.ts
+++ packages/fetch/src/server.ts
@@ -14,19 +14,19 @@ export { withCache, withCacheStorage } from "./cache";
 
 let fetchFallback: typeof fetch;
 try {
-  const nodeFetch = require('node-fetch');
+  const nodeFetch = require("node-fetch");
   fetchFallback = nodeFetch;
-} catch(_e) {
+} catch (_e) {
   fetchFallback = () => {
     console.warn(
       '"\x1b[33m⚠️ package missing to run createFetch on the server.\n Please run:\x1b[0m\n\nnpm i node-fetch\n"'
     );
     return Promise.reject();
-  }
+  };
 }
 
 const fetchRequest = !globalThis.fetch
   ? (fetch = fetchFallback) => originalFetchRequest(fetch)
   : originalFetchRequest;
 
-export { fetchRequest }
+export { fetchRequest };
