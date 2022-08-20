1b101ac784c80c8cdbacfaaa3c737b0de508f918
AyloSrd <aylo.srd@gmail.com>
2022-08-20 19:00:16 +0200

    created primitive, working in sandbox, need to make tests

---
+++ packages/event-dispatcher/README.md
@@ -0,0 +1,36 @@
+<p>
+  <img width="100%" src="https://assets.solidjs.com/banner?type=Primitives&background=tiles&project=event-dispatcher" alt="Solid Primitives event-dispatcher">
+</p>
+
+# @solid-primitives/event-dispatcher
+
+[![turborepo](https://img.shields.io/badge/built%20with-turborepo-cc00ff.svg?style=for-the-badge&logo=turborepo)](https://turborepo.org/)
+[![size](https://img.shields.io/bundlephobia/minzip/@solid-primitives/event-dispatcher?style=for-the-badge&label=size)](https://bundlephobia.com/package/@solid-primitives/event-dispatcher)
+[![version](https://img.shields.io/npm/v/@solid-primitives/event-dispatcher?style=for-the-badge)](https://www.npmjs.com/package/@solid-primitives/event-dispatcher)
+[![stage](https://img.shields.io/endpoint?style=for-the-badge&url=https%3A%2F%2Fraw.githubusercontent.com%2Fsolidjs-community%2Fsolid-primitives%2Fmain%2Fassets%2Fbadges%2Fstage-0.json)](https://github.com/solidjs-community/solid-primitives#contribution-process)
+
+A sample primitive that is made up for templating with the following options:
+
+`createPrimitiveTemplate` - Provides a getter and setter for the primitive.
+
+## Installation
+
+```bash
+npm install @solid-primitives/event-dispatcher
+# or
+yarn add @solid-primitives/event-dispatcher
+```
+
+## How to use it
+
+```ts
+const [value, setValue] = createPrimitiveTemplate(false);
+```
+
+## Demo
+
+You can use this template for publishing your demo on CodeSandbox: https://codesandbox.io/s/solid-primitives-demo-template-sz95h
+
+## Changelog
+
+See [CHANGELOG.md](./CHANGELOG.md)
---
+++ packages/event-dispatcher/src/createEventDispatcher.ts
@@ -0,0 +1,46 @@
+import { create_custom_evt, get_event_listener_name } from "./utils"
+
+type Handlers<T> = {
+  [Property in keyof T as Property extends `on${infer EventName}` ? Uncapitalize<EventName> : never]: T[Property]
+}
+
+type DispatcherOptions = {
+  cancelable: boolean,
+}
+
+export function createEventDispatcher<Props>(props: Props) {
+  return function<N extends keyof Handlers<Props> & string>(
+    ...args: Handlers<Props>[N] extends (undefined | ((evt?: CustomEvent<infer D> | undefined) => any)) ?
+    // this will handle typing for callbacks with optional payloads like (e.g. onMyEvent: (evt?: CustomEvent<string> => void))
+    [
+        eventName: N,
+        payload?: D | undefined,
+        dispatcherOptions?: DispatcherOptions,
+      ]
+    : Handlers<Props>[N] extends (undefined | ((evt: CustomEvent<infer D>) => any)) ?
+      // this will handle typing where the payload is not nullable (e.g. onMyEvent: (evt: CustomEvent<string> => void))
+      [
+        eventName: N,
+        payload: D,
+        dispatcherOptions?: DispatcherOptions,
+      ]
+    :
+      // this will handle callabcks with no arguments (e.g. onMyEvent: () => void)
+      [
+        eventName: N,
+        payload?: any,
+        dispatcherOptions?: DispatcherOptions,
+      ]
+  ): boolean {
+    const [eventName, payload, dispatcherOptions] = args
+    const propName = get_event_listener_name(eventName) as string & keyof Props
+    const cb = props[propName]
+
+    if (typeof cb !== 'function') return true
+
+    const customEvt = create_custom_evt(eventName, payload, dispatcherOptions?.cancelable ?? false)
+    cb(customEvt)
+
+    return !customEvt.defaultPrevented
+  }
+}
