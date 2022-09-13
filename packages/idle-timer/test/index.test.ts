82abaf85a59a16752d7d4012d4be097bf5771316
AyloSrd <aylo.srd@gmail.com>
2022-09-13 13:12:35 +0200

    added testts for config options, need to figure out a way to test with jsdom

--- packages/idle-timer/test/index.test.ts
+++ packages/idle-timer/test/index.test.ts
@@ -1,68 +1,110 @@
 import { makeIdleTimer } from "../src";
-import { createRoot, onMount } from "solid-js";
-import { describe, test, expect } from "vitest";
+import { createRoot, createSignal, ErrorBoundary, onMount } from "solid-js";
+import { render } from "solid-js/web";
+import { describe, test, expect, JSDOMOptions } from "vitest";
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 describe('makeIdleTimer', () => {
-    test(
-        'signals when the user is prompted and idle according to the configuration timeouts',
-        async () => await createRoot(async dispose => {
-            const { isIdle, isPrompted, stop } = makeIdleTimer({
-                idleTimeout: 5,
-                promptTimeout: 5,
-            })
+  test(
+    'signals when the user is prompted and idle according to the configuration timeouts',
+    async () => await createRoot(async dispose => {
+      const { isIdle, isPrompted, stop } = makeIdleTimer({
+        idleTimeout: 5,
+        promptTimeout: 5,
+      })
 
-            await sleep(2)
-            expect(isPrompted(), 'user is not prompted yet').toBe(false)
-            expect(isIdle(), 'user is not idle yet').toBe(false)
+      await new Promise(resolve => onMount(() => {
+        resolve(true)
+      }))
 
-            await sleep(5)
-            expect(isPrompted(), 'user has been prompted').toBe(true)
-            expect(isIdle(), 'user is not idle yet').toBe(false)
+      await sleep(2)
+      expect(isPrompted(), 'user is not prompted yet').toBe(false)
+      expect(isIdle(), 'user is not idle yet').toBe(false)
 
-            await sleep(5)
-            expect(isPrompted(), 'user is not in the prompted phase anymore').toBe(false)
-            expect(isIdle(), 'user is idle').toBe(true)
+      await sleep(5)
+      expect(isPrompted(), 'user has been prompted').toBe(true)
+      expect(isIdle(), 'user is not idle yet').toBe(false)
 
-            stop()
-            dispose()
-        })
-    )
+      await sleep(5)
+      expect(isPrompted(), 'user is not in the prompted phase anymore').toBe(false)
+      expect(isIdle(), 'user is idle').toBe(true)
 
-    test(
-        'start and stop should successfully bind, unbind the event listeners, reset should clean and restart the timers',
-        async () => await createRoot(async dispose => {
-            const { isIdle, reset,start, stop } = makeIdleTimer({
-                idleTimeout: 5,
-                startManually: true,
-            })
+      stop()
+      dispose()
+    })
+  )
 
-            await sleep(0)
-            expect(isIdle(), 'user is not idle yet, events are not bound yet').toBe(false)
+  test(
+    'start and stop should successfully bind, unbind the event listeners, reset should clean and restart the timers',
+    async () => await createRoot(async dispose => {
+      const { isIdle, reset,start, stop } = makeIdleTimer({
+        idleTimeout: 5,
+        startManually: true,
+      })
 
-            start()
+      await new Promise(resolve => onMount(() => {
+        resolve(true)
+      }))
 
-            await sleep(10)
-            expect(isIdle(), 'user is idle').toBe(true)
+      await sleep(0)
+      expect(isIdle(), 'user is not idle yet, events are not bound yet').toBe(false)
 
-            reset()
+      start()
 
-            await sleep(4)
-            expect(isIdle(), 'user is not idle yet, timers have restarted').toBe(false)
-            await sleep(10)
-            expect(isIdle(), 'user is idle').toBe(true)
+      await sleep(10)
+      expect(isIdle(), 'user is idle').toBe(true)
 
+      reset()
 
-            stop()
-            await sleep(1)
-            expect(isIdle(), 'user is not idle anymore, timers have been cleaned up').toBe(false)
-            await sleep(10)
-            expect(isIdle(), 'user is idle, event listeners are unbound, timers have not restarted').toBe(false)
-
-            dispose()
+      await sleep(4)
+      expect(isIdle(), 'user is not idle yet, timers have restarted').toBe(false)
+      await sleep(10)
+      expect(isIdle(), 'user is idle').toBe(true)
 
 
-        })
-    )
+      stop()
+      await sleep(1)
+      expect(isIdle(), 'user is not idle anymore, timers have been cleaned up').toBe(false)
+      await sleep(10)
+      expect(isIdle(), 'user is idle, event listeners are unbound, timers have not restarted').toBe(false)
+
+      dispose()
+    })
+  )
+
+  test(
+    'configuration options shall work',
+    async () => await createRoot(async dispose => {
+      let currStatus =  'initial'
+
+      const { start, stop } = makeIdleTimer({
+        promptTimeout: 15,
+        idleTimeout: 15,
+        startManually: true,
+        onActive: () => currStatus = 'active',
+        onIdle: () => currStatus = 'idle',
+        onPrompt: () => currStatus = 'prompted',
+      })
+
+      await new Promise(resolve => onMount(() => {
+        resolve(true)
+      }))
+
+      await sleep(10)
+      expect(currStatus, 'events are not bound yet, the status has not cahnged').toBe('initial')
+
+      start()
+
+      await sleep(20)
+      expect(currStatus, 'timers started, user should be in the prompt phase, onPrompt should have been called by now').toBe('prompted')
+
+      await sleep(20)
+      expect(currStatus, 'prompt timer has expired, onIdle should have been called by now').toBe('idle')
+
+      stop()
+
+      dispose()
+    })
+  )
 })
