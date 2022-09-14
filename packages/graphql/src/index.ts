e706297c563906487f448c164f1a3318d2795871
teenjuna <ryapolov@pm.me>
2022-09-14 15:01:09 +0700

    feat(graphql): allow passing options to `createGraphQLClient`

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -4,33 +4,32 @@ import { TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { DocumentNode, print } from "graphql";
 
 export type RequestOptions<V extends object = {}> = Modify<
-  Omit<RequestInit, "body">,
-  {
-    headers?: RequestHeaders;
-    variables?: V;
-    fetcher?: typeof fetch;
-  }
+	Omit<RequestInit, "body">,
+	{
+		headers?: RequestHeaders;
+		variables?: V;
+		fetcher?: typeof fetch;
+	}
 >;
 
 export type GraphQLClientQuery = {
-  <T = unknown, V extends object = {}>(
-    query: string | DocumentNode | TypedDocumentNode<T, V>,
-    variables: MaybeAccessor<V | FalsyValue> | undefined,
-    initialValue: T
-  ): ResourceReturn<T>;
-  <T = unknown, V extends object = {}>(
-    query: string | DocumentNode | TypedDocumentNode<T, V>,
-    variables?: MaybeAccessor<V | FalsyValue>,
-    initialValue?: undefined
-  ): ResourceReturn<T | undefined>;
+	<T = unknown, V extends object = {}>(
+		query: string | DocumentNode | TypedDocumentNode<T, V>,
+		variables: MaybeAccessor<V | FalsyValue> | undefined,
+		initialValue: T
+	): ResourceReturn<T>;
+	<T = unknown, V extends object = {}>(
+		query: string | DocumentNode | TypedDocumentNode<T, V>,
+		variables?: MaybeAccessor<V | FalsyValue>,
+		initialValue?: undefined
+	): ResourceReturn<T | undefined>;
 };
 
 /**
  * Creates a reactive GraphQL query client.
  *
  * @param url URL as string or accessor
- * @param headers A list of headers to supply the client
- * @param fetchFn Fetch library to use for the request
+ * @param options Options that will be applied to all the subsequent requests
  * @returns Returns a query generator the produces Solid resources for queries
  *
  * @see https://github.com/solidjs-community/solid-primitives/tree/main/packages/graphql#how-to-use-it
@@ -41,20 +40,19 @@ export type GraphQLClientQuery = {
  * ```
  */
 export const createGraphQLClient =
-  (
-    url: MaybeAccessor<string>,
-    headers?: RequestHeaders,
-    fetchFn?: typeof fetch
-  ): GraphQLClientQuery =>
-  (query, variables: any = {}, initialValue) =>
-    createResource(
-      () => access(variables),
-      (vars: any) => {
-        const variables = typeof vars === "boolean" ? {} : vars;
-        return request(access(url), query, { headers, variables, fetcher: fetchFn });
-      },
-      { initialValue }
-    );
+	(
+		url: MaybeAccessor<string>,
+		options?: RequestOptions,
+	): GraphQLClientQuery =>
+		(query, variables: any = {}, initialValue) =>
+			createResource(
+				() => access(variables),
+				(vars: any) => {
+					const variables = typeof vars === "boolean" ? {} : vars;
+					return request(access(url), query, { variables, ...options });
+				},
+				{ initialValue }
+			);
 
 /**
  * Performs a GraphQL fetch to provided endpoint.
@@ -65,36 +63,36 @@ export const createGraphQLClient =
  * @returns a Promise resolving in JSON value if the request was successful
  */
 export async function request<T = any, V extends object = {}>(
-  url: string,
-  query: string | DocumentNode | TypedDocumentNode<T, V>,
-  options: RequestOptions<V> = {}
+	url: string,
+	query: string | DocumentNode | TypedDocumentNode<T, V>,
+	options: RequestOptions<V> = {}
 ): Promise<T> {
-  const { fetcher = fetch, variables = {}, headers = {}, method = "POST" } = options;
-  const query_ = typeof query == "string" ? query : print(query);
+	const { fetcher = fetch, variables = {}, headers = {}, method = "POST" } = options;
+	const query_ = typeof query == "string" ? query : print(query);
 
-  return fetcher(url, {
-    ...options,
-    method,
-    body: JSON.stringify({ query: query_, variables }),
-    headers: {
-      "content-type": "application/json",
-      ...headers
-    }
-  })
-    .then((r: any) => r.json())
-    .then(({ data, errors }: any) => {
-      if (errors) throw errors;
-      return data;
-    });
+	return fetcher(url, {
+		...options,
+		method,
+		body: JSON.stringify({ query: query_, variables }),
+		headers: {
+			"content-type": "application/json",
+			...headers
+		}
+	})
+		.then((r: any) => r.json())
+		.then(({ data, errors }: any) => {
+			if (errors) throw errors;
+			return data;
+		});
 }
 
 /**
  * Creates a GraphQL query string.
  */
 export const gql = (query: TemplateStringsArray) =>
-  query
-    .join(" ")
-    .replace(/#.+\r?\n|\r/g, "")
-    .replace(/\r?\n|\r/g, "")
-    .replace(/\s{2,}/g, " ")
-    .trim();
+	query
+		.join(" ")
+		.replace(/#.+\r?\n|\r/g, "")
+		.replace(/\r?\n|\r/g, "")
+		.replace(/\s{2,}/g, " ")
+		.trim();
