c65f77e5ed23c438e3d822f6a6d7e2e7978ca869
teenjuna <ryapolov@pm.me>
2022-09-15 14:24:50 +0700

    apply suggestions

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -4,25 +4,25 @@ import { TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { DocumentNode, print } from "graphql";
 
 export type RequestOptions<V extends object = {}> = Modify<
-  Omit<RequestInit, "body">,
-  {
-    headers?: RequestHeaders;
-    variables?: V;
-    fetcher?: typeof fetch;
-  }
+	Omit<RequestInit, "body">,
+	{
+		headers?: RequestHeaders;
+		variables?: V;
+		fetcher?: typeof fetch;
+	}
 >;
 
 export type GraphQLClientQuery = {
-  <T = unknown, V extends object = {}>(
-    query: string | DocumentNode | TypedDocumentNode<T, V>,
-    variables: MaybeAccessor<V | FalsyValue> | undefined,
-    initialValue: T
-  ): ResourceReturn<T>;
-  <T = unknown, V extends object = {}>(
-    query: string | DocumentNode | TypedDocumentNode<T, V>,
-    variables?: MaybeAccessor<V | FalsyValue>,
-    initialValue?: undefined
-  ): ResourceReturn<T | undefined>;
+	<T = unknown, V extends object = {}>(
+		query: string | DocumentNode | TypedDocumentNode<T, V>,
+		variables: MaybeAccessor<V | FalsyValue> | undefined,
+		initialValue: T
+	): ResourceReturn<T>;
+	<T = unknown, V extends object = {}>(
+		query: string | DocumentNode | TypedDocumentNode<T, V>,
+		variables?: MaybeAccessor<V | FalsyValue>,
+		initialValue?: undefined
+	): ResourceReturn<T | undefined>;
 };
 
 /**
@@ -40,16 +40,16 @@ export type GraphQLClientQuery = {
  * ```
  */
 export const createGraphQLClient =
-  (url: MaybeAccessor<string>, options?: RequestOptions): GraphQLClientQuery =>
-  (query, variables: any = {}, initialValue) =>
-    createResource(
-      () => access(variables),
-      (vars: any) => {
-        const variables = typeof vars === "boolean" ? {} : vars;
-        return request(access(url), query, { variables, ...options });
-      },
-      { initialValue }
-    );
+	(url: MaybeAccessor<string>, options?: Omit<RequestOptions, "variables">): GraphQLClientQuery =>
+		(query, variables: any = {}, initialValue) =>
+			createResource(
+				() => access(variables),
+				(vars: any) => {
+					const variables = typeof vars === "boolean" ? {} : vars;
+					return request(access(url), query, { ...options, variables });
+				},
+				{ initialValue }
+			);
 
 /**
  * Performs a GraphQL fetch to provided endpoint.
@@ -60,36 +60,36 @@ export const createGraphQLClient =
  * @returns a Promise resolving in JSON value if the request was successful
  */
 export async function request<T = any, V extends object = {}>(
-  url: string,
-  query: string | DocumentNode | TypedDocumentNode<T, V>,
-  options: RequestOptions<V> = {}
+	url: string,
+	query: string | DocumentNode | TypedDocumentNode<T, V>,
+	options: RequestOptions<V> = {}
 ): Promise<T> {
-  const { fetcher = fetch, variables = {}, headers = {}, method = "POST" } = options;
-  const query_ = typeof query == "string" ? query : print(query);
+	const { fetcher = fetch, variables = {}, headers = {}, method = "POST" } = options;
+	const query_ = typeof query == "string" ? query : print(query);
 
-  return fetcher(url, {
-    ...options,
-    method,
-    body: JSON.stringify({ query: query_, variables }),
-    headers: {
-      "content-type": "application/json",
-      ...headers
-    }
-  })
-    .then((r: any) => r.json())
-    .then(({ data, errors }: any) => {
-      if (errors) throw errors;
-      return data;
-    });
+	return fetcher(url, {
+		...options,
+		method,
+		body: JSON.stringify({ query: query_, variables }),
+		headers: {
+			"content-type": "application/json",
+			...headers
+		}
+	})
+		.then((r: any) => r.json())
+		.then(({ data, errors }: any) => {
+			if (errors) throw errors;
+			return data;
+		});
 }
 
 /**
  * Creates a GraphQL query string.
  */
 export const gql = (query: TemplateStringsArray) =>
-  query
-    .join(" ")
-    .replace(/#.+\r?\n|\r/g, "")
-    .replace(/\r?\n|\r/g, "")
-    .replace(/\s{2,}/g, " ")
-    .trim();
+	query
+		.join(" ")
+		.replace(/#.+\r?\n|\r/g, "")
+		.replace(/\r?\n|\r/g, "")
+		.replace(/\s{2,}/g, " ")
+		.trim();
