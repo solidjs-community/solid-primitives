36d57b4ec8e516faf1a8e8301d2e24ad20cef60b
Damian Tarnawski <gthetarnav@gmail.com>
2022-10-13 21:39:20 +0200

    Convert active-element

--- packages/active-element/test/index.test.ts
+++ packages/active-element/test/index.test.ts
@@ -1,7 +1,6 @@
 import { fireEvent, createEvent } from "solid-testing-library";
 import { createRoot } from "solid-js";
-import { suite } from "uvu";
-import * as assert from "uvu/assert";
+import { describe, test, expect } from "vitest";
 import {
   makeActiveElementListener,
   createActiveElement,
@@ -10,110 +9,95 @@ import {
   focus
 } from "../src";
 
-const testMAEL = suite("makeActiveElementListener");
-
 const dispatchFocusEvent = (target: Element | Window = window, event: "focus" | "blur" = "focus") =>
   fireEvent(target, createEvent(event, window));
 
-testMAEL("works properly", () =>
-  createRoot(dispose => {
-    let events = 0;
-    let captured;
-    makeActiveElementListener(e => ((captured = e), events++));
-    assert.is(captured, undefined);
-    dispatchFocusEvent();
-    assert.is(captured, null);
-    assert.is(events, 1);
+describe("makeActiveElementListener", () => {
+  test("works properly", () =>
+    createRoot(dispose => {
+      let events = 0;
+      let captured;
+      makeActiveElementListener(e => ((captured = e), events++));
+      expect(captured).toBe(undefined);
+      dispatchFocusEvent();
+      expect(captured).toBe(null);
+      expect(events).toBe(1);
 
-    dispose();
-    dispatchFocusEvent();
-    assert.is(events, 1);
+      dispose();
+      dispatchFocusEvent();
+      expect(events).toBe(1);
 
-    const clear = makeActiveElementListener(e => events++);
-    dispatchFocusEvent();
-    assert.is(events, 2);
+      const clear = makeActiveElementListener(e => events++);
+      dispatchFocusEvent();
+      expect(events).toBe(2);
 
-    clear();
-    dispatchFocusEvent();
-    assert.is(events, 2);
-  })
-);
+      clear();
+      dispatchFocusEvent();
+      expect(events).toBe(2);
+    }));
+});
 
-testMAEL.run();
+describe("makeFocusListener", () => {
+  test("works properly", () =>
+    createRoot(dispose => {
+      const el = document.createElement("div");
+      const captured: any[] = [];
+      const clear = makeFocusListener(el, e => captured.push(e));
+      expect(captured).toEqual([]);
+      dispatchFocusEvent(el, "focus");
+      expect(captured).toEqual([true]);
+      dispatchFocusEvent(el, "blur");
+      expect(captured).toEqual([true, false]);
+      clear();
+      dispatchFocusEvent(el, "focus");
+      expect(captured).toEqual([true, false]);
+      makeFocusListener(el, e => captured.push(e));
+      dispatchFocusEvent(el, "blur");
+      expect(captured).toEqual([true, false, false]);
+      dispose();
+      dispatchFocusEvent(el, "focus");
+      expect(captured).toEqual([true, false, false]);
+    }));
+});
 
-const testMFL = suite("makeFocusListener");
+describe("createActiveElement", () => {
+  test("works properly", () =>
+    createRoot(dispose => {
+      const activeEl = createActiveElement();
+      expect(activeEl()).toBe(null);
+      dispose();
+    }));
+});
 
-testMFL("works properly", () =>
-  createRoot(dispose => {
-    const el = document.createElement("div");
-    const captured: any[] = [];
-    const clear = makeFocusListener(el, e => captured.push(e));
-    assert.equal(captured, []);
-    dispatchFocusEvent(el, "focus");
-    assert.equal(captured, [true]);
-    dispatchFocusEvent(el, "blur");
-    assert.equal(captured, [true, false]);
-    clear();
-    dispatchFocusEvent(el, "focus");
-    assert.equal(captured, [true, false]);
-    makeFocusListener(el, e => captured.push(e));
-    dispatchFocusEvent(el, "blur");
-    assert.equal(captured, [true, false, false]);
-    dispose();
-    dispatchFocusEvent(el, "focus");
-    assert.equal(captured, [true, false, false]);
-  })
-);
+describe("createFocusSignal", () => {
+  test("works properly", () =>
+    createRoot(dispose => {
+      const el = document.createElement("div");
+      const activeEl = createFocusSignal(el);
+      expect(activeEl()).toBe(false);
+      dispatchFocusEvent(el, "focus");
+      expect(activeEl()).toBe(true);
+      dispatchFocusEvent(el, "blur");
+      expect(activeEl()).toBe(false);
+      dispose();
+      dispatchFocusEvent(el, "focus");
+      expect(activeEl()).toBe(false);
+    }));
+});
 
-testMFL.run();
-
-const testCAE = suite("createActiveElement");
-
-testCAE("works properly", () =>
-  createRoot(dispose => {
-    const activeEl = createActiveElement();
-    assert.is(activeEl(), null);
-    dispose();
-  })
-);
-
-testCAE.run();
-
-const testCFS = suite("createFocusSignal");
-
-testCFS("works properly", () =>
-  createRoot(dispose => {
-    const el = document.createElement("div");
-    const activeEl = createFocusSignal(el);
-    assert.is(activeEl(), false);
-    dispatchFocusEvent(el, "focus");
-    assert.is(activeEl(), true);
-    dispatchFocusEvent(el, "blur");
-    assert.is(activeEl(), false);
-    dispose();
-    dispatchFocusEvent(el, "focus");
-    assert.is(activeEl(), false);
-  })
-);
-
-testCFS.run();
-
-const testFocus = suite("use:focus");
-
-testFocus("works properly", () =>
-  createRoot(dispose => {
-    const el = document.createElement("div");
-    let captured!: boolean;
-    focus(el, () => e => (captured = e));
-    assert.is(captured, false);
-    dispatchFocusEvent(el, "focus");
-    assert.is(captured, true);
-    dispatchFocusEvent(el, "blur");
-    assert.is(captured, false);
-    dispose();
-    dispatchFocusEvent(el, "focus");
-    assert.is(captured, false);
-  })
-);
-
-testFocus.run();
+describe("use:focus", () => {
+  test("works properly", () =>
+    createRoot(dispose => {
+      const el = document.createElement("div");
+      let captured!: boolean;
+      focus(el, () => e => (captured = e));
+      expect(captured).toBe(false);
+      dispatchFocusEvent(el, "focus");
+      expect(captured).toBe(true);
+      dispatchFocusEvent(el, "blur");
+      expect(captured).toBe(false);
+      dispose();
+      dispatchFocusEvent(el, "focus");
+      expect(captured).toBe(false);
+    }));
+});
