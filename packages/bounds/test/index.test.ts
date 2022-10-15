cadb2196dd20738660b76616fb4b3ee3d1fc260a
Damian Tarnawski <gthetarnav@gmail.com>
2022-10-15 15:49:03 +0200

    Convert `destructure` and `bounds`

--- packages/bounds/test/index.test.ts
+++ packages/bounds/test/index.test.ts
@@ -1,31 +1,20 @@
-import * as assert from "uvu/assert";
-import { suite } from "uvu";
+import { describe, test, expect } from "vitest";
 import { createRoot } from "solid-js";
 import { createElementBounds, getElementBounds } from "../src";
 
+class TestResizeObserver {
+  constructor() {}
+  observe(target: Element, options?: ResizeObserverOptions): void {}
+  unobserve(target: Element): void {}
+  disconnect() {}
+}
+global.ResizeObserver = TestResizeObserver;
+
 const div = document.createElement("div");
 
-const geb = suite("getElementBounds");
-
-geb("getElementBounds", () => {
-  assert.equal(getElementBounds(div), {
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    width: 0,
-    height: 0
-  });
-});
-
-geb.run();
-
-const ceb = suite("createElementBounds");
-
-ceb("returns bounds of element", () =>
-  createRoot(dispose => {
-    const bounds = createElementBounds(div);
-    assert.equal(bounds, {
+describe("getElementBounds", () => {
+  test("getElementBounds", () => {
+    expect(getElementBounds(div)).toEqual({
       top: 0,
       left: 0,
       bottom: 0,
@@ -33,23 +22,35 @@ ceb("returns bounds of element", () =>
       width: 0,
       height: 0
     });
-    dispose();
-  })
-);
+  });
+});
 
-ceb("returns null if there is no initial element", () =>
-  createRoot(dispose => {
-    const bounds = createElementBounds(() => undefined);
-    assert.equal(bounds, {
-      top: null,
-      left: null,
-      bottom: null,
-      right: null,
-      width: null,
-      height: null
-    });
-    dispose();
-  })
-);
+describe("createElementBounds", () => {
+  test("returns bounds of element", () =>
+    createRoot(dispose => {
+      const bounds = createElementBounds(div);
+      expect(bounds).toEqual({
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        width: 0,
+        height: 0
+      });
+      dispose();
+    }));
 
-ceb.run();
+  test("returns null if there is no initial element", () =>
+    createRoot(dispose => {
+      const bounds = createElementBounds(() => undefined);
+      expect(bounds).toEqual({
+        top: null,
+        left: null,
+        bottom: null,
+        right: null,
+        width: null,
+        height: null
+      });
+      dispose();
+    }));
+});
--- packages/destructure/test/destructure.test.ts
+++ packages/destructure/test/destructure.test.ts
@@ -1,254 +1,251 @@
-import { destructure } from "../src";
+import { describe, test, expect } from "vitest";
 import { createComputed, createRoot, createSignal } from "solid-js";
-import { suite } from "uvu";
-import * as assert from "uvu/assert";
+import { destructure } from "../src";
 
-const test = suite("destructure");
+describe("destructure", () => {
+  test("spread array", () =>
+    createRoot(dispose => {
+      const [numbers, setNumbers] = createSignal([1, 2, 3] as [number, number, number]);
+      const [first, second, last] = destructure(numbers);
 
-test("spread array", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal([1, 2, 3] as [number, number, number]);
-    const [first, second, last] = destructure(numbers);
+      const updates = {
+        a: 0,
+        b: 0,
+        c: 0
+      };
+      createComputed(() => {
+        first();
+        updates.a++;
+      });
+      createComputed(() => {
+        second();
+        updates.b++;
+      });
+      createComputed(() => {
+        last();
+        updates.c++;
+      });
 
-    const updates = {
-      a: 0,
-      b: 0,
-      c: 0
-    };
-    createComputed(() => {
-      first();
-      updates.a++;
-    });
-    createComputed(() => {
-      second();
-      updates.b++;
-    });
-    createComputed(() => {
-      last();
-      updates.c++;
-    });
+      expect(first()).toBe(1);
+      expect(second()).toBe(2);
+      expect(last()).toBe(3);
 
-    assert.is(first(), 1);
-    assert.is(second(), 2);
-    assert.is(last(), 3);
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(1);
+      expect(updates.c).toBe(1);
 
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 1);
-    assert.is(updates.c, 1);
+      setNumbers([1, 6, 7]);
+      expect(first()).toBe(1);
+      expect(second()).toBe(6);
+      expect(last()).toBe(7);
 
-    setNumbers([1, 6, 7]);
-    assert.is(first(), 1);
-    assert.is(second(), 6);
-    assert.is(last(), 7);
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(2);
+      expect(updates.c).toBe(2);
 
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 2);
-    assert.is(updates.c, 2);
+      dispose();
+    }));
 
-    dispose();
-  }));
-
-test("spread object", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal({
-      a: 1,
-      b: 2,
-      c: 3
-    });
-    const { a, b, c } = destructure(numbers);
-
-    const updates = {
-      a: 0,
-      b: 0,
-      c: 0
-    };
-    createComputed(() => {
-      a();
-      updates.a++;
-    });
-    createComputed(() => {
-      b();
-      updates.b++;
-    });
-    createComputed(() => {
-      c();
-      updates.c++;
-    });
-
-    assert.is(a(), 1);
-    assert.is(b(), 2);
-    assert.is(c(), 3);
-
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 1);
-    assert.is(updates.c, 1);
-
-    setNumbers({
-      a: 1,
-      b: 6,
-      c: 7
-    });
-    assert.is(a(), 1);
-    assert.is(b(), 6);
-    assert.is(c(), 7);
-
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 2);
-    assert.is(updates.c, 2);
-
-    dispose();
-  }));
-
-test("spread is eager", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
-      a: 0
-    });
-
-    const { a, b } = destructure(numbers);
-
-    assert.is(a(), 0);
-    assert.is(b, undefined);
-
-    setNumbers({
-      a: 2,
-      b: 3
-    });
-
-    assert.is(a(), 2);
-    assert.is(b, undefined);
-
-    dispose();
-  }));
-
-test("destructure object", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal({
-      a: 1,
-      b: 2,
-      c: 3
-    });
-    const { a, b, c } = destructure(numbers, { lazy: true });
-
-    const updates = {
-      a: 0,
-      b: 0,
-      c: 0
-    };
-    createComputed(() => {
-      a();
-      updates.a++;
-    });
-    createComputed(() => {
-      b();
-      updates.b++;
-    });
-    createComputed(() => {
-      c();
-      updates.c++;
-    });
-
-    assert.is(a(), 1);
-    assert.is(b(), 2);
-    assert.is(c(), 3);
-
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 1);
-    assert.is(updates.c, 1);
-
-    setNumbers({
-      a: 1,
-      b: 6,
-      c: 7
-    });
-    assert.is(a(), 1);
-    assert.is(b(), 6);
-    assert.is(c(), 7);
-
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 2);
-    assert.is(updates.c, 2);
-
-    dispose();
-  }));
-
-test("destructure is lazy", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
-      a: 0
-    });
-
-    const { a, b } = destructure(numbers, { lazy: true });
-
-    assert.is(a(), 0);
-    assert.is(b(), undefined);
-
-    setNumbers({
-      a: 2,
-      b: 3
-    });
-
-    assert.is(a(), 2);
-    assert.is(b(), 3);
-
-    dispose();
-  }));
-
-test("destructure recursively nested objects", () =>
-  createRoot(dispose => {
-    const [numbers, setNumbers] = createSignal({
-      nested: {
+  test("spread object", () =>
+    createRoot(dispose => {
+      const [numbers, setNumbers] = createSignal({
         a: 1,
         b: 2,
         c: 3
-      }
-    });
-    const {
-      nested: { a, b, c }
-    } = destructure(numbers, { deep: true });
+      });
+      const { a, b, c } = destructure(numbers);
 
-    const updates = {
-      a: 0,
-      b: 0,
-      c: 0
-    };
-    createComputed(() => {
-      a();
-      updates.a++;
-    });
-    createComputed(() => {
-      b();
-      updates.b++;
-    });
-    createComputed(() => {
-      c();
-      updates.c++;
-    });
+      const updates = {
+        a: 0,
+        b: 0,
+        c: 0
+      };
+      createComputed(() => {
+        a();
+        updates.a++;
+      });
+      createComputed(() => {
+        b();
+        updates.b++;
+      });
+      createComputed(() => {
+        c();
+        updates.c++;
+      });
 
-    assert.is(a(), 1);
-    assert.is(b(), 2);
-    assert.is(c(), 3);
+      expect(a()).toBe(1);
+      expect(b()).toBe(2);
+      expect(c()).toBe(3);
 
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 1);
-    assert.is(updates.c, 1);
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(1);
+      expect(updates.c).toBe(1);
 
-    setNumbers({
-      nested: {
+      setNumbers({
         a: 1,
         b: 6,
         c: 7
-      }
-    });
-    assert.is(a(), 1);
-    assert.is(b(), 6);
-    assert.is(c(), 7);
+      });
+      expect(a()).toBe(1);
+      expect(b()).toBe(6);
+      expect(c()).toBe(7);
 
-    assert.is(updates.a, 1);
-    assert.is(updates.b, 2);
-    assert.is(updates.c, 2);
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(2);
+      expect(updates.c).toBe(2);
 
-    dispose();
-  }));
+      dispose();
+    }));
 
-test.run();
+  test("spread is eager", () =>
+    createRoot(dispose => {
+      const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
+        a: 0
+      });
+
+      const { a, b } = destructure(numbers);
+
+      expect(a()).toBe(0);
+      expect(b).toBe(undefined);
+
+      setNumbers({
+        a: 2,
+        b: 3
+      });
+
+      expect(a()).toBe(2);
+      expect(b).toBe(undefined);
+
+      dispose();
+    }));
+
+  test("destructure object", () =>
+    createRoot(dispose => {
+      const [numbers, setNumbers] = createSignal({
+        a: 1,
+        b: 2,
+        c: 3
+      });
+      const { a, b, c } = destructure(numbers, { lazy: true });
+
+      const updates = {
+        a: 0,
+        b: 0,
+        c: 0
+      };
+      createComputed(() => {
+        a();
+        updates.a++;
+      });
+      createComputed(() => {
+        b();
+        updates.b++;
+      });
+      createComputed(() => {
+        c();
+        updates.c++;
+      });
+
+      expect(a()).toBe(1);
+      expect(b()).toBe(2);
+      expect(c()).toBe(3);
+
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(1);
+      expect(updates.c).toBe(1);
+
+      setNumbers({
+        a: 1,
+        b: 6,
+        c: 7
+      });
+      expect(a()).toBe(1);
+      expect(b()).toBe(6);
+      expect(c()).toBe(7);
+
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(2);
+      expect(updates.c).toBe(2);
+
+      dispose();
+    }));
+
+  test("destructure is lazy", () =>
+    createRoot(dispose => {
+      const [numbers, setNumbers] = createSignal<{ a: number; b?: number }>({
+        a: 0
+      });
+
+      const { a, b } = destructure(numbers, { lazy: true });
+
+      expect(a()).toBe(0);
+      expect(b()).toBe(undefined);
+
+      setNumbers({
+        a: 2,
+        b: 3
+      });
+
+      expect(a()).toBe(2);
+      expect(b()).toBe(3);
+
+      dispose();
+    }));
+
+  test("destructure recursively nested objects", () =>
+    createRoot(dispose => {
+      const [numbers, setNumbers] = createSignal({
+        nested: {
+          a: 1,
+          b: 2,
+          c: 3
+        }
+      });
+      const {
+        nested: { a, b, c }
+      } = destructure(numbers, { deep: true });
+
+      const updates = {
+        a: 0,
+        b: 0,
+        c: 0
+      };
+      createComputed(() => {
+        a();
+        updates.a++;
+      });
+      createComputed(() => {
+        b();
+        updates.b++;
+      });
+      createComputed(() => {
+        c();
+        updates.c++;
+      });
+
+      expect(a()).toBe(1);
+      expect(b()).toBe(2);
+      expect(c()).toBe(3);
+
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(1);
+      expect(updates.c).toBe(1);
+
+      setNumbers({
+        nested: {
+          a: 1,
+          b: 6,
+          c: 7
+        }
+      });
+      expect(a()).toBe(1);
+      expect(b()).toBe(6);
+      expect(c()).toBe(7);
+
+      expect(updates.a).toBe(1);
+      expect(updates.b).toBe(2);
+      expect(updates.c).toBe(2);
+
+      dispose();
+    }));
+});
