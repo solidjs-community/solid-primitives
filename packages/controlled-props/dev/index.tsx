01bdd5a7888eca4b653f5772ac68a88373afda89
atk <atk@users.noreply.github.com>
2022-12-08 09:17:05 +0000

    Format

--- packages/controlled-props/dev/index.tsx
+++ packages/controlled-props/dev/index.tsx
@@ -8,18 +8,20 @@ const App: Component = () => {
     bool: true,
     number: 0,
     string: "test"
-  })
+  });
 
-  return (<div class="p-24 box-border w-full min-h-screen overflow-hidden flex flex-col justify-center items-center">
-    <h2>Props</h2>
-    <ul>
-      <li>bool: {props.bool() ? 'true' : 'false'}</li>
-      <li>number: {props.number()}</li>
-      <li>string: {props.string()}</li>
-    </ul>
-    <h2>Controls</h2>
-    {controls}
-  </div>);
+  return (
+    <div class="p-24 box-border w-full min-h-screen overflow-hidden flex flex-col justify-center items-center">
+      <h2>Props</h2>
+      <ul>
+        <li>bool: {props.bool() ? "true" : "false"}</li>
+        <li>number: {props.number()}</li>
+        <li>string: {props.string()}</li>
+      </ul>
+      <h2>Controls</h2>
+      {controls}
+    </div>
+  );
 };
 
 render(() => <App />, document.getElementById("root")!);
