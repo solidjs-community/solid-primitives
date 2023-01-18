dd2d7d1c9af1c58b33dd3886c26bb62958b3bd4e
Damian Tarnawski <gthetarnav@gmail.com>
2023-01-18 19:01:21 +0100

    Add tsup-preset-solid to build packages

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,5 @@
-import fs from "fs";
-import fsp from "fs/promises";
-import path from "path";
-import { Options, build } from "tsup";
-import type { PackageJson } from "type-fest";
+import { build } from "tsup";
+import { defineConfig } from "tsup-preset-solid";
 
 /*
 
@@ -14,147 +11,40 @@ What goes in and out are decided by use of `process.env.DEV` or `process.env.PRO
 `--ssr` will enable building a server-side entry (for use with SSR frameworks).
 What goes in and out are decided by use of `process.env.SSR`.
 
+`--jsx` will enable building a `solid` entry with preserved JSX.
+
 `--write` or `-w` will write the exports configuration to package.json instead of to the console.
 The exports configuration is taken from the solid-js package.json.
 
 */
 
-const cwd = process.cwd();
 const ssrEntry = process.argv.includes("--ssr");
 const devEntry = process.argv.includes("--dev");
+const jsxEntry = process.argv.includes("--jsx");
 const writeExports = process.argv.includes("--write") || process.argv.includes("-w");
-const isCI = process.env.CI === "true" || process.env.CI === "1";
-const printExports = !writeExports && !isCI;
+const printExports = !writeExports;
 
-const getEnvVars = (mode: "dev" | "prod" | "ssr"): Options => ({
-  esbuildOptions(options) {
-    options.define = {
-      ...options.define,
-      "process.env.NODE_ENV": mode === "dev" ? `"development"` : `"production"`,
-      "process.env.PROD": mode === "dev" ? '""' : '"1"',
-      "process.env.DEV": mode === "dev" ? '"1"' : '""',
-      "process.env.SSR": mode === "ssr" ? '"1"' : '""'
-    };
-  }
-});
-
-function createTsupConfig({ onSuccess }: { onSuccess?: () => void | Promise<void> } = {}) {
-  const common: Options = {
-    format: ["esm", "cjs"],
-    treeshake: true,
-    replaceNodeEnv: true,
-    onSuccess: handleSuccess
-  };
-
-  const entries: Options[] = [
+(async () => {
+  let options = defineConfig(
     {
-      ...common,
-      dts: "src/index.ts",
-      entryPoints: ["src/index.ts"],
-      ...getEnvVars("prod")
-    }
-  ];
-
-  if (ssrEntry) {
-    entries.push({
-      ...common,
-      entryPoints: { server: "src/index.ts" },
-      ...getEnvVars("ssr")
-    });
-  }
-
-  if (devEntry) {
-    entries.push({
-      ...common,
-      entryPoints: { dev: "src/index.ts" },
-      ...getEnvVars("dev")
-    });
-  }
-
-  let buildsToComplete = entries.length;
-
-  async function handleSuccess() {
-    if (--buildsToComplete > 0) return;
-    onSuccess?.();
-  }
-
-  return entries;
-}
-
-const getExports = (): PackageJson => ({
-  type: "module",
-  ...(ssrEntry
-    ? {
-        main: "./dist/server.cjs",
-        module: "./dist/server.js",
-        browser: {
-          "./dist/server.cjs": "./dist/index.cjs",
-          "./dist/server.js": "./dist/index.js"
-        }
-      }
-    : {
-        main: "./dist/index.cjs",
-        module: "./dist/index.js",
-        browser: {}
-      }),
-  types: "./dist/index.d.ts",
-  exports: {
-    ...(ssrEntry && {
-      worker: {
-        import: "./dist/server.js",
-        require: "./dist/server.cjs"
-      }
-    }),
-    browser: {
-      ...(devEntry && {
-        development: {
-          import: "./dist/dev.js",
-          require: "./dist/dev.cjs"
-        }
-      }),
-      import: "./dist/index.js",
-      require: "./dist/index.cjs"
+      entry: `src/index.${jsxEntry ? "tsx" : "ts"}`,
+      devEntry: devEntry,
+      serverEntry: ssrEntry
     },
-    ...(ssrEntry && {
-      deno: {
-        import: "./dist/server.js",
-        require: "./dist/server.cjs"
-      },
-      node: {
-        import: "./dist/server.js",
-        require: "./dist/server.cjs"
-      }
-    }),
-    ...(devEntry && {
-      development: {
-        import: "./dist/dev.js",
-        require: "./dist/dev.cjs"
-      }
-    }),
-    import: "./dist/index.js",
-    require: "./dist/index.cjs"
-  }
-});
-
-function printInstrunctions() {
-  console.log(
-    `\npackage.json exports should look like this:\n\n${JSON.stringify(getExports(), null, 2)}\n`
+    {
+      cjs: true,
+      writePackageJson: writeExports,
+      printInstructions: printExports
+    }
   );
-}
 
-async function writeExportsFile() {
-  const pkgJsonPath = path.resolve(cwd, "package.json");
-  const pkgJson = JSON.parse(await fsp.readFile(pkgJsonPath, "utf8"));
-  const newPkjJson = { ...pkgJson, ...getExports() };
-  await fsp.writeFile(pkgJsonPath, JSON.stringify(newPkjJson, null, 2));
-}
-
-// clear dist
-fs.rmSync(path.resolve(cwd, `dist`), { recursive: true, force: true });
-
-createTsupConfig({
-  onSuccess() {
-    if (printExports) printInstrunctions();
-    if (writeExports) writeExportsFile();
+  if (typeof options === "function") {
+    options = await options({});
   }
-}).forEach(config => build(config));
+
+  if (!Array.isArray(options)) {
+    options = [options];
+  }
+
+  options.forEach(build);
+})();
