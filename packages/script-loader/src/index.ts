0818c446abe783a886bc7649d75d1a1dea377453
Alex Lohr <alex.lohr@goto.com>
2023-01-30 18:48:07 +0100

    script: allow full script props (src can still be a signal)

--- packages/script-loader/src/index.ts
+++ packages/script-loader/src/index.ts
@@ -1,4 +1,11 @@
-import { Accessor, createEffect, onCleanup } from "solid-js";
+import { Accessor, createEffect, onCleanup, splitProps, type ComponentProps } from "solid-js";
+import { spread, template } from "solid-js/web";
+
+export type ScriptProps = Omit<ComponentProps<"script">, 'src'> & {
+  src: string | Accessor<string>;
+};
+
+const scriptTag = template('<script></script>', 1);
 
 /**
  * Creates a convenient script loader utility
@@ -9,12 +16,10 @@ import { Accessor, createEffect, onCleanup } from "solid-js";
  * @param function callback on error.
  * @returns
  */
-export const createScriptLoader = (opts: {
-  src: string | Accessor<string>;
-  type?: string;
-  onload?: () => void;
-  onerror?: () => void;
-}): [script: HTMLScriptElement | undefined, remove: () => void] => {
+export const createScriptLoader = (props: ScriptProps): [
+  script: HTMLScriptElement | undefined,
+  remove: () => void
+] => {
   if (process.env.SSR) {
     return [
       undefined,
@@ -23,13 +28,12 @@ export const createScriptLoader = (opts: {
       }
     ];
   }
-  const script = document.createElement("script");
-  opts.type && (script.type = opts.type);
-  opts.onload && script.addEventListener("load", opts.onload);
-  opts.onerror && script.addEventListener("error", opts.onerror);
+  const script = scriptTag.cloneNode(false) as HTMLScriptElement;
+  const [local, scriptProps] = splitProps(props, ['src']);
+  spread(script, scriptProps, false, true);
   const remove = () => document.head.contains(script) && document.head.removeChild(script);
   const load = () => {
-    const src = typeof opts.src === "string" ? opts.src : opts.src();
+    const src = typeof local.src === "string" ? local.src : local.src();
     const prop = /^(https?:|\w[\.\w-_%]+|)\//.test(src) ? "src" : "textContent";
     if (script[prop] !== src) {
       script[prop] = src;
@@ -37,7 +41,7 @@ export const createScriptLoader = (opts: {
     }
   };
   load();
-  createEffect(load, false);
+  createEffect(load);
   onCleanup(remove);
   return [script, remove];
 };
