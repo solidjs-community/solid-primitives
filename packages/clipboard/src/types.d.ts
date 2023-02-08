8adcc6cfba589e1aff69cf72fd57ce03949d12da
Damian Tarnawski <gthetarnav@gmail.com>
2023-02-08 19:48:22 +0100

    clipboard: Add some functions, changes to createClipboard API, fixes #256

--- packages/clipboard/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare type ClipboardSetter = (data: string | ClipboardItem[]) => Promise<void>;
-declare type NewClipboardItem = (type: string, data: ClipboardItemData) => ClipboardItem;
-declare type HighlightModifier = (el: any) => void;
-declare type Highlighter = (start?: number, end?: number) => HighlightModifier;
-declare type CopyToClipboardOptions = {
-  value?: any;
-  setter?: ClipboardSetter;
-  highlight?: HighlightModifier;
-};
