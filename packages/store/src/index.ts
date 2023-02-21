1c0ca8cea36d2ed159b611f30904e3537eb664dc
Zalexios <83531993+Zalexios@users.noreply.github.com>
2023-02-20 21:39:56 -0500

    store: createStoreFactory -> createSharedStore

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,76 +1,84 @@
 import type { JSX, FlowComponent } from "solid-js";
 import { createContext, useContext } from "solid-js";
 import type { SetStoreFunction } from "solid-js/store";
-import { createStore, produce as _produce, unwrap as _unwrap } from "solid-js/store";
+import { createStore } from "solid-js/store";
 
 /**
- * An interface for a set of functions for a store factory
+ * An object containing functions to create getters and/or actions.
  *
- * @interface StoreFactoryFunctions
- * @template <T: state type of the store, A: store access/mutation functions>
- * @property {() => [state: T, actions: A]} useStore - A function to retrieve the current state and access/mutation functions
- * @property {(fn: (state: T) => void) => void} produce - A function to execute a callback with the current state
- * @property {() => T} unwrapped - A function to retrieve the current state
- * @property {[get: T, set: SetStoreFunction<T>]} store - A tuple with the current state and a setter for the state
- * @property {T} state - The current state of the store
- * @property {SetStoreFunction<T>} setState - A function to set the current state of the store
+ * @interface SharedStoreMethods
+ * @template <T: state type of the store, G: store access methods, A: store mutation methods>
+ * @property {(state: State<T>) => G} getters - A function that returns an object containing functions to access the state.
+ * @property {(setState: SetStoreFunction<T>) => A} actions - A function that returns an object containing functions that mutate the state.
  */
-export interface StoreFactoryFunctions<T extends object, A extends object | undefined> {
-  useStore: () => [state: T, actions: A];
-  produce: (fn: (state: T) => void) => void;
-  unwrapped: () => T;
-  store: [get: T, set: SetStoreFunction<T>];
-  state: T;
+export interface SharedStoreMethods<
+  T extends object,
+  G extends object | undefined,
+  A extends object | undefined
+> {
+  getters?: (state: T) => G;
+  actions?: (setState: SetStoreFunction<T>) => A;
+}
+
+/**
+ * An interface representing the returned shared state functionality.
+ * It consists of the state, any predefined methods to access and mutate the state and the setState function.
+ *
+ * @interface SharedStore
+ * @template <T: state type of the store, G: store access methods, A: store mutation methods>
+ * @property {() => [state: T, methods: { getters: G; actions: A; }]} useStore - A function to retrieve the current state and access/mutation functions.
+ * @property {SetStoreFunction<T>} setState - A function to manually mutate the current state of the store.
+ */
+export interface SharedStore<
+  T extends object,
+  G extends object | undefined,
+  A extends object | undefined
+> {
+  useStore: () => [
+    state: T,
+    methods: {
+      getters: G;
+      actions: A;
+    }
+  ];
   setState: SetStoreFunction<T>;
 }
 
 /**
- * A tuple for a store factory
+ * A tuple for a Shared Store
  *
- * @typedef StoreFactory
- * @template <T: state type of the store, A: store access/mutation functions>
- * @property {FlowComponent<Partial<T>>} StoreFactoryProvider - A Flow component that provides partial state to the store
- * @property {StoreFactoryFunctions<T, A>} StoreFactoryFunctions - A set of functions for the store factory
+ * @typedef SharedStoreReturn
+ * @template <T: state type of the store, G: store access methods, A: store mutation methods>
+ * @property {FlowComponent<Partial<T>>} SharedStoreProvider - A Flow component that provides partial state to the store
+ * @property {SharedStore<T, G, A>} SharedStore - The state, any predefined methods to access and mutate the state and the setState function.
  */
-export type StoreFactory<T extends object, A extends object | undefined> = [
-  StoreFactoryProvider: FlowComponent<Partial<T>>,
-  StoreFactoryFunctions: StoreFactoryFunctions<T, A>
-];
+export type SharedStoreReturn<
+  T extends object,
+  G extends object | undefined,
+  A extends object | undefined
+> = [SharedStoreProvider: FlowComponent<Partial<T>>, SharedStore: SharedStore<T, G, A>];
 
 /**
- * A function to create actions for a store
- *
- * @typedef CreateActions
- * @template <T: state type of the store, A: store access/mutation functions>
- * @param {T} state - The current state of the store
- * @param {SetStoreFunction<T>} setState - A function to set the current state of the store
- * @returns {A} A set of actions for the store
- */
-export type CreateActions<T extends object, A extends object | undefined> = (
-  state: T,
-  setState: SetStoreFunction<T>
-) => A;
-
-/**
- * Factory takes an `initialState` and optional access/mutation functions, wraps it in a Store, creates
- * a Context Provider, implements useContext, and returns a tuple of the Provider and the Store.
- * @template <T: state type of the store, A: store access/mutation functions (optional)>
+ * createSharedStore takes an `initialState` and an optional object containing functions to create getters and/or actions.
+ * It creates a Context Provider, implements useContext, and returns a tuple of the Provider and the useStore & setState.
+ * @template <T: state type of the store, G: store access methods, A: store mutation methods>
  * @param initialState object to be wrapped in a Store
- * @param createActions optional function that returns an object containing functions that access or
- * modify the state of the store
- * @returns a tuple of `[StoreFactoryProvider, StoreFactoryFunctions]`
+ * @param createMethods optional object containing functions to create getters and/or actions.
+ * @returns a tuple of `[SharedStoreProvider, SharedStore]`
  * @example
  * ```tsx
  * // `counter-store.ts`
- * const [CounterProvider, {useStore: useCounterStore}] = createStoreFactory({
+ * const [CounterProvider, {useStore: useCounterStore}] = createSharedStore({
  *   value: 5,
- * },
- *   (state, setState) => ({
- *     count: () => state.value,
- *     increment: () => setState(val => ({ value: val.value + 1 })),
- *     reset: () => setState({ value: 0 }),
- *   })
- * );
+ * }, {
+ *  getters: state => ({
+ *   count: () => state.value,
+ *  }),
+ *  actions: setState => ({
+ *   increment: () => setState(val => ({ ...val, value: val.value + 1 })),
+ *   reset: () => setState("value", 0),
+ *  })
+ * });
  * export { CounterProvider, useCounterStore };
  *
  * // `App.tsx`
@@ -84,54 +92,60 @@ export type CreateActions<T extends object, A extends object | undefined> = (
  * // `Example.tsx`
  * import { useCounterStore } from './counter-store.ts';
  *
- * const [counterState, {count, increment, reset}] = useCounterStore();
+ * const [counterState, {getters: {count}, actions: {increment, reset}}] = useCounterStore();
  * count() // => 5
  * increment()
  * count() // => 6
  * reset() // => 0
  * ```
  */
-export function createStoreFactory<T extends object>(initialState: T): StoreFactory<T, undefined>;
-export function createStoreFactory<T extends object, A extends object>(
+export function createSharedStore<T extends object>(
+  initialState: T
+): SharedStoreReturn<T, undefined, undefined>;
+export function createSharedStore<
+  T extends object,
+  G extends object | undefined,
+  A extends object | undefined
+>(
   initialState: T,
-  createActions: CreateActions<T, A>
-): StoreFactory<T, A>;
-export function createStoreFactory<T extends object>(
+  createMethods: SharedStoreMethods<T, G, A>
+): SharedStoreReturn<T, undefined extends G ? undefined : G, undefined extends A ? undefined : A>;
+export function createSharedStore<
+  T extends object,
+  G extends object | undefined,
+  A extends object | undefined
+>(
   initialState: T,
-  createActions: CreateActions<T, any>
-): StoreFactory<T, ReturnType<typeof createActions>>;
-export function createStoreFactory<T extends object, A extends object | undefined>(
-  initialState: T,
-  createActions?: CreateActions<T, A | undefined>
-): StoreFactory<T, A | undefined> {
+  createMethods?: SharedStoreMethods<T, G | undefined, A | undefined>
+): SharedStoreReturn<T, G | undefined, A | undefined> {
   const store = createStore<T>(initialState);
   const [state, setState] = store;
-  const actions = createActions?.(state, setState);
+  const getters = createMethods?.getters?.(state);
+  const actions = createMethods?.actions?.(setState);
+  const methods = {
+    getters,
+    actions
+  };
+  type Getters = typeof getters;
   type Actions = typeof actions;
+  type Methods = typeof methods;
 
-  const produce = (fn: (state: T) => void) => setState(_produce<T>(fn));
-  const unwrapped = () => _unwrap<T>(state);
-
-  const FactoryContext = createContext<[state: T, actions: Actions]>([initialState, actions]);
-  const useStore = () => useContext(FactoryContext);
-  const StoreFactoryProvider: FlowComponent<Partial<T>> = props =>
-    FactoryContext.Provider({
+  const SharedStoreContext = createContext<[state: T, methods: Methods]>([state, methods]);
+  const useStore = () => useContext(SharedStoreContext);
+  const SharedStoreProvider: FlowComponent<Partial<T>> = props =>
+    SharedStoreContext.Provider({
       ...props,
       value: useStore(),
       children: props.children
     });
 
   return [
-    StoreFactoryProvider,
+    SharedStoreProvider,
     {
       useStore,
-      produce,
-      unwrapped,
-      store,
-      state,
       setState
     }
-  ] as StoreFactory<T, Actions>;
+  ] as SharedStoreReturn<T, Getters, Actions>;
 }
 
 // This ensures the `JSX` import won't fall victim to tree shaking before
