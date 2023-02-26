95d326c44b0dda55a68019d3fd801c8174162353
Damian Tarnawski <gthetarnav@gmail.com>
2023-02-26 21:18:13 +0100

    Add `createListTransition` to transition-group

--- packages/transition-group/dev/list-page.tsx
+++ packages/transition-group/dev/list-page.tsx
@@ -1,28 +1,13 @@
-import { children, ParentComponent } from "solid-js";
-import { elements, mapRemoved } from "@solid-primitives/refs";
+import { children, onCleanup, untrack } from "solid-js";
+import { elements } from "@solid-primitives/refs";
 import { Component, createSignal, For, Show } from "solid-js";
-import { createCallbackStack } from "@solid-primitives/utils";
+import { createListTransition } from "../src";
 
-const Keep: ParentComponent<{
-  getClear?: (fn: VoidFunction) => void;
-}> = props => {
-  const resolved = children(() => props.children);
-  const refs = elements(resolved, HTMLElement);
-  const stack = createCallbackStack();
-  props.getClear?.(stack.execute);
-  const combined = mapRemoved<HTMLElement | undefined>(refs, (ref, i) => {
-    const [el, setEl] = createSignal<HTMLElement | undefined>(ref);
-    console.log("REMOVED", i());
-    stack.push(() => setEl(undefined));
-    ref!.style.filter = "grayscale(100%)";
-    ref!.style.position = "relative";
-    ref!.appendChild((<div class="absolute bg-black">{i()}</div>) as Element);
-
-    ref!.addEventListener("click", () => setEl(undefined));
-
-    return el;
+const grayOutOnDispose = (el: HTMLElement) => {
+  onCleanup(() => {
+    el.style.filter = "grayscale(60%)";
+    el.style.zIndex = "0";
   });
-  return combined;
 };
 
 const ListPage: Component = () => {
@@ -30,17 +15,22 @@ const ListPage: Component = () => {
   const [show1, setShow1] = createSignal(false);
   const [show2, setShow2] = createSignal(true);
   const [showWrapper, setShowWrapper] = createSignal(true);
-  const [count, setCount] = createSignal(5);
 
-  let clear!: VoidFunction;
+  const [list, setList] = createSignal([{ n: 1 }, { n: 2 }, { n: 3 }, { n: 4 }, { n: 5 }]);
 
   return (
     <>
       <div class="wrapper-h flex-wrap">
-        <button class="btn" onclick={() => setCount(p => ++p)}>
+        <button
+          class="btn"
+          onclick={() => setList(p => [...p, { n: (p[p.length - 1]?.n ?? -1) + 1 }])}
+        >
           + 1
         </button>
-        <button class="btn" onclick={() => setCount(p => --p)}>
+        <button
+          class="btn"
+          onclick={() => setList(p => (p.length > 0 ? p.slice(0, p.length - 1) : p))}
+        >
           - 1
         </button>
         <button class="btn" onclick={() => setShow(p => !p)}>
@@ -55,33 +45,110 @@ const ListPage: Component = () => {
         <button class="btn" onclick={() => setShowWrapper(p => !p)}>
           toggle wrapper
         </button>
-        <button class="btn" onclick={() => clear()}>
-          clear
-        </button>
       </div>
-      <div class="wrapper-h flex-wrap">
-        <Keep getClear={fn => (clear = fn)}>
-          <Show when={showWrapper()}>
-            <p>Hello</p>
-            World
-            {show() && <div class="node">ID 0</div>}
-            {/* <Ref<HTMLDivElement>
-                ref={el => console.log(el)}
-                onMount={el => console.log("Mounted", el)}
-              > */}
-            <Show when={show1()}>
-              <div class="node">ID 1</div>
+      <div class="wrapper-h flex-wrap space-x-0 gap-4">
+        {untrack(() => {
+          const resolved = children(() => (
+            <Show when={showWrapper()}>
+              <p>Hello</p>
+              World
+              {show() && (
+                <div class="node" ref={grayOutOnDispose}>
+                  ID 0
+                </div>
+              )}
+              <Show when={show1()}>
+                <div class="node" ref={grayOutOnDispose}>
+                  ID 1
+                </div>
+              </Show>
+              <Show when={show2()}>
+                <div class="node" ref={grayOutOnDispose}>
+                  ID 2
+                </div>
+                <div class="node" ref={grayOutOnDispose}>
+                  ID 3
+                </div>
+              </Show>
+              <For each={list()}>
+                {({ n }, i) => (
+                  <div
+                    class="node bg-yellow-600 cursor-pointer"
+                    onClick={() =>
+                      setList(p => {
+                        const copy = p.slice();
+                        copy.splice(i(), 1);
+                        return copy;
+                      })
+                    }
+                    ref={grayOutOnDispose}
+                  >
+                    {n + 1}.
+                  </div>
+                )}
+              </For>
             </Show>
-            {/* </Ref> */}
-            <Show when={show2()}>
-              <div class="node">ID 2</div>
-              <div class="node">ID 3</div>
-            </Show>
-            <For each={Array.from({ length: count() }, (_, i) => i)}>
-              {i => <div class="node bg-yellow-600">{i + 1}.</div>}
-            </For>
-          </Show>
-        </Keep>
+          ));
+          const refs = elements(resolved, HTMLElement);
+
+          const options = { duration: 600, easing: "cubic-bezier(0.4, 0, 0.2, 1)" };
+
+          return createListTransition(refs, {
+            appear: true,
+            onChange({ added, finishRemoved, moved, removed }) {
+              added.forEach(el => {
+                el.style.opacity = "0";
+                el.style.transform = "translateY(10px)";
+                requestAnimationFrame(() => {
+                  el.animate(
+                    [
+                      { opacity: 0, transform: "translateY(-36px)" },
+                      { opacity: 1, transform: "translateY(0)" },
+                    ],
+                    { ...options, fill: "both" },
+                  );
+                });
+              });
+
+              moved.forEach(el => {
+                const { left: left1, top: top1 } = el.getBoundingClientRect();
+                requestAnimationFrame(() => {
+                  const { left: left2, top: top2 } = el.getBoundingClientRect();
+                  el.animate(
+                    [
+                      { transform: `translate(${left1 - left2}px, ${top1 - top2}px)` },
+                      { transform: "none" },
+                    ],
+                    options,
+                  );
+                });
+              });
+
+              const removedRects = removed.map(el => el.getBoundingClientRect());
+              removed.forEach(el => {
+                el.style.transform = "none";
+                el.style.position = "absolute";
+              });
+              removed.forEach((el, i) => {
+                const { left: left1, top: top1 } = removedRects[i]!;
+                const { left: left2, top: top2 } = el.getBoundingClientRect();
+
+                const a = el.animate(
+                  [
+                    { transform: `translate(${left1 - left2}px, ${top1 - top2}px)` },
+                    {
+                      opacity: 0,
+                      transform: `translate(${left1 - left2}px, ${top1 - top2 + 36}px)`,
+                    },
+                  ],
+                  options,
+                );
+
+                i === removed.length - 1 && a.finished.then(() => finishRemoved(removed));
+              });
+            },
+          });
+        })}
       </div>
     </>
   );
--- packages/transition-group/src/index.ts
+++ packages/transition-group/src/index.ts
@@ -1,21 +1,23 @@
-import { FalsyValue, noop } from "@solid-primitives/utils";
-import { Accessor, batch, createComputed, createSignal, untrack } from "solid-js";
+import { Accessor, batch, createComputed, createSignal, untrack, $TRACK } from "solid-js";
+
+const noop = () => {};
+const noopTransition = (el: any, done: () => void) => done();
 
 export type TransitionMode = "out-in" | "in-out" | "parallel";
 
-const logEl = (el: any) => (el ? el.textContent : null);
+export type OnTransition<T> = (el: NonNullable<T>, done: () => void) => void;
 
-const noopTransition = (el: any, done: () => void) => done();
+export type SwitchTransitionOptions<T> = {
+  onEnter?: OnTransition<T>;
+  onExit?: OnTransition<T>;
+  mode?: TransitionMode;
+  appear?: boolean;
+};
 
-export function createSwitchTransition<T extends object>(
-  source: Accessor<T | FalsyValue>,
-  props: {
-    onEnter?: (el: T, done: () => void) => void;
-    onExit?: (el: T, done: () => void) => void;
-    mode?: TransitionMode;
-    appear?: boolean;
-  },
-): Accessor<T[]> {
+export function createSwitchTransition<T>(
+  source: Accessor<T>,
+  options: SwitchTransitionOptions<T>,
+): Accessor<NonNullable<T>[]> {
   if (process.env.SSR) {
     let el: any = source();
     el = el ? [el] : [];
@@ -23,11 +25,11 @@ export function createSwitchTransition<T extends object>(
   }
 
   // eslint-disable-next-line prefer-const
-  let { onEnter = noopTransition, onExit = noopTransition } = props;
+  let { onEnter = noopTransition, onExit = noopTransition } = options;
 
-  // prevent the first enter transition if appear is disabled
+  // skip the first enter transition if appear is disabled
   let init = true;
-  if (!props.appear) {
+  if (!options.appear) {
     const _onEnter = onEnter;
     onEnter = (el, done) => {
       onEnter = _onEnter;
@@ -35,36 +37,36 @@ export function createSwitchTransition<T extends object>(
     };
   }
 
-  const [returned, setReturned] = createSignal<T[]>([]);
+  const [returned, setReturned] = createSignal<NonNullable<T>[]>([]);
 
   let next: T | undefined;
   let isExiting = false;
 
   function exitTransition(el: T | undefined, after?: () => void) {
-    if (!el) return after?.();
+    if (!el) return after && after();
     isExiting = true;
     onExit(el, () => {
       batch(() => {
         isExiting = false;
         setReturned(p => p.filter(e => e !== el));
-        after?.();
+        after && after();
       });
     });
   }
 
   function enterTransition(after?: () => void) {
     const el = next;
-    if (!el) return after?.();
+    if (!el) return after && after();
     next = undefined;
     setReturned(p => [el, ...p]);
     onEnter(el, after ?? noop);
   }
 
   const transition: (prev: T | undefined) => void =
-    props.mode === "out-in"
+    options.mode === "out-in"
       ? // exit -> enter
         prev => isExiting || exitTransition(prev, enterTransition)
-      : props.mode === "in-out"
+      : options.mode === "in-out"
       ? // enter -> exit
         prev => enterTransition(() => exitTransition(prev))
       : // exit & enter
@@ -74,12 +76,97 @@ export function createSwitchTransition<T extends object>(
         };
 
   createComputed((prev: T | undefined) => {
-    const el = source() || undefined;
+    const el = source();
     if (el !== prev) {
       next = el;
       untrack(() => batch(() => transition(prev)));
     }
     return el;
+  });
+
+  init = false;
+  return returned;
+}
+
+export type OnListChange<T> = (payload: {
+  added: T[];
+  removed: T[];
+  moved: T[];
+  finishRemoved: (els: T[]) => void;
+}) => void;
+
+export type ListTransitionOptions<T> = {
+  onChange: OnListChange<T>;
+  appear?: boolean;
+};
+
+export function createListTransition<T>(
+  source: Accessor<readonly NonNullable<T>[]>,
+  options: ListTransitionOptions<NonNullable<T>>,
+): Accessor<NonNullable<T>[]> {
+  if (process.env.SSR) {
+    const copy = source().slice();
+    return () => copy;
+  }
+
+  type V = NonNullable<T>;
+
+  let { onChange } = options;
+
+  // skip the first enter transition if appear is disabled
+  let init = true;
+  if (!options.appear) {
+    const _onChange = onChange;
+    onChange = data => {
+      onChange = _onChange;
+      init || onChange(data);
+    };
+  }
+
+  const [returned, setReturned] = createSignal<V[]>([]);
+
+  let prevSet: ReadonlySet<V> = new Set<V>();
+  const exiting = new Set<V>();
+
+  function finishRemoved(els: V[]): void {
+    setReturned(p => p.filter(e => !els.includes(e)));
+    for (const el of els) exiting.delete(el);
+  }
+
+  createComputed(() => {
+    const list = source();
+    (list as any)[$TRACK]; // top level store tracking
+
+    untrack(() => {
+      setReturned(prev => {
+        const nextSet: ReadonlySet<V> = new Set(list);
+        const next: V[] = list.slice();
+
+        const added: V[] = [];
+        const removed: V[] = [];
+        const moved: V[] = [];
+
+        for (const el of list) {
+          (prevSet.has(el) ? moved : added).push(el);
+        }
+
+        for (let i = 0; i < prev.length; i++) {
+          const el = prev[i]!;
+          if (!nextSet.has(el)) {
+            if (!exiting.has(el)) {
+              removed.push(el);
+              exiting.add(el);
+            }
+            next.splice(i, 0, el);
+          }
+        }
+
+        onChange({ added, removed, moved, finishRemoved });
+
+        prevSet = nextSet;
+        return next;
+      });
+    });
   });
 
   init = false;
