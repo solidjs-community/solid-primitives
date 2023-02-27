96eb2b23d6fdaa45aa7063e3bab69f527ee552e3
Caleb Taylor <aquaductape@gmail.com>
2023-02-26 22:06:26 -0800

    update

--- scripts/get-size.ts
+++ scripts/get-size.ts
@@ -1,24 +1,43 @@
 import { build } from "esbuild";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync, rmSync } from "fs";
 import { gzipSizeSync } from "gzip-size";
 import { formatBytes } from "./utils";
 
-const run = async () => {
+const run = async ({
+  packageName,
+  primitiveName
+}: {
+  packageName: string;
+  primitiveName: string;
+}) => {
+  const file = `
+export { ${primitiveName} } from "./index"
+`;
+  const fileName = "_temp_check_primitive.ts";
+  const outDir = "./_temp_output_primitive_dir";
+  const outFile = `${outDir}/main.js`;
+  const packagePath = `./packages/${packageName}/src/`;
+  const packageExportFilePath = `${packagePath}${fileName}`;
+  writeFileSync(packageExportFilePath, file);
+
   await build({
-    entryPoints: ["./packages/stream/src/index.ts"],
-    outfile: "./dist/main.js",
+    entryPoints: [packageExportFilePath],
+    outfile: outFile,
     target: ["esnext"],
     format: "esm",
     bundle: true,
     minify: true,
     treeShaking: true,
     platform: "browser",
-    external: ["solid-js", "@solid-primitives/utils"]
+    external: ["solid-js"]
   });
-  const buffer = readFileSync("./dist/main.js");
+  const buffer = readFileSync(outFile);
   const minifiedSize = buffer.toString().length;
   const gzippedSize = gzipSizeSync(buffer);
+  rmSync(packageExportFilePath);
+  rmSync(outDir, { recursive: true });
 
   console.log(formatBytes(minifiedSize), formatBytes(gzippedSize));
 };
-run();
+
+run({ packageName: "stream", primitiveName: "createAmplitudeStream" });
