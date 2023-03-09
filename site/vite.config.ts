480c7381023e6c36f9c952267d0f97ba770631b8
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-09 20:35:00 +0100

    Add devtools

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,5 +1,6 @@
-import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import devtools from "solid-devtools/vite";
+import solid from "solid-start/vite";
 // @ts-ignore
 import staticAdapter from "solid-start-static";
 import dotenv from "dotenv";
@@ -8,6 +9,13 @@ export default defineConfig(() => {
   dotenv.config();
   return {
     plugins: [
+      devtools({
+        autoname: true,
+        locator: {
+          componentLocation: true,
+          targetIDE: "vscode",
+        },
+      }),
       solid({
         hot: false,
         adapter: staticAdapter(),
