583333f1ae936f409086133618b4c850a99b4951
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-14 22:57:16 +0100

    Remove `composites` package

--- packages/composites/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./createCompositeEffect";
-export * from "./createCompositeComputed";
-export * from "./createCompositeMemo";
-export * from "./createCompositeRenderEffect";
-export * from "./createModifier";
-export * from "./modifiers";
-export * from "./types";
