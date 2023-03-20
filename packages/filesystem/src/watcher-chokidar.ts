840c2caae68f864b5717128391437c9c7afbe0b5
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-20 21:08:26 +0100

    Fix no-console comment

--- packages/filesystem/src/watcher-chokidar.ts
+++ packages/filesystem/src/watcher-chokidar.ts
@@ -4,17 +4,16 @@ export const makeChokidarWatcher = (basePath: string = "/"): Watcher => {
   let subscriber: ((operation: Operation, path: string) => void) | undefined;
 
   import("chokidar")
-    .then(
-      chokidar =>
-        chokidar
-          .watch(`${basePath}${basePath.endsWith("/") ? "" : "/"}**/*`, { persistent: true })
-          .on("change", (path: string) => subscriber?.("writeFile", path))
-          .on("add", (path: string) => subscriber?.("writeFile", path))
-          .on("addDir", (path: string) => subscriber?.("mkdir", path))
-          .on("unlink", (path: string) => subscriber?.("rm", path))
-          .on("unlinkDir", (path: string) => subscriber?.("rm", path)),
-      // eslint-disable-next-line no-console
+    .then(chokidar =>
+      chokidar
+        .watch(`${basePath}${basePath.endsWith("/") ? "" : "/"}**/*`, { persistent: true })
+        .on("change", path => subscriber?.("writeFile", path))
+        .on("add", path => subscriber?.("writeFile", path))
+        .on("addDir", path => subscriber?.("mkdir", path))
+        .on("unlink", path => subscriber?.("rm", path))
+        .on("unlinkDir", path => subscriber?.("rm", path)),
     )
+    // eslint-disable-next-line no-console
     .catch(e => console.warn(e));
 
   return fn => {
