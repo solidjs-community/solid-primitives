3b590c37f14b0405c7f839550f8adf37a40a63f6
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-23 18:48:47 +0100

    Move staric markup to Heading

--- site/src/routes/package/[name].tsx
+++ site/src/routes/package/[name].tsx
@@ -1,4 +1,3 @@
-import { FaBrandsGithub } from "solid-icons/fa";
 import { Component, createResource } from "solid-js";
 import { Title, useParams, useRouteData } from "solid-start";
 import { PRIMITIVE_PAGE_PADDING_TOP } from "~/components/Header/Header";
@@ -7,7 +6,7 @@ import InfoBar from "~/components/Primitives/InfoBar";
 import { pageWidthClass } from "~/constants";
 import createTooltipOnCodePrimitives from "~/hooks/createTooltipOnCodePrimitives";
 import onPreMount from "~/hooks/onPreMount";
-import { GITHUB_REPO, PackageData } from "~/types";
+import { PackageData } from "~/types";
 
 type Params = {
   name: string;
@@ -37,7 +36,9 @@ const Page: Component = () => {
       {data() && <Title>{data()!.name}</Title>}
       <script>document.documentElement.classList.add("primitives-page-main")</script>
       <div
-        class="-z-1 absolute top-0 left-0 right-0 h-[95vh] bg-[linear-gradient(to_bottom,#fff_var(--primitive-padding-top-gr),transparent)] dark:bg-[linear-gradient(to_bottom,#293843_var(--primitive-padding-top-gr),transparent)]"
+        class="-z-1 absolute top-0 left-0 right-0 h-[95vh]
+        bg-[linear-gradient(to_bottom,#fff_var(--primitive-padding-top-gr),transparent)]
+        dark:bg-[linear-gradient(to_bottom,#293843_var(--primitive-padding-top-gr),transparent)]"
         style={{
           "--primitive-padding-top-gr": `${PRIMITIVE_PAGE_PADDING_TOP + 300}px`,
         }}
@@ -48,38 +49,7 @@ const Page: Component = () => {
       >
         <div class="bg-page-main-bg rounded-3xl p-3 sm:p-8">
           <div class="mb-[90px] flex items-center justify-between gap-[30px] text-[#232324] dark:text-white sm:gap-[100px]">
-            {/* <Heading>
-              Has This Type Pattern Tried To Sneak In Some Generic Or Parameterized Type Pattern
-              Matching Stuff Anywhere Visitor .java
-            </Heading> */}
-
-            <Heading>{data()?.name.replace("-", " ")}</Heading>
-            <div class="relative">
-              <svg
-                class="absolute"
-                viewBox="0 0 88.975 79.46"
-                // @ts-ignore
-                xml:space="preserve"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                xmlns="http://www.w3.org/2000/svg"
-                style="top: 13px; left: -30px; width: 247px; pointer-events: none;"
-              >
-                <use href="#solid-blocks-header-cluster-e" transform="translate(-44 -38)" />
-                <use href="#solid-blocks-header-cluster-e" transform="translate(-44 -38)" />
-                <use href="#solid-blocks-header-cluster-e" transform="translate(-29 -29)" />
-              </svg>
-              <a
-                class="relative inline-block scale-90 transition-opacity hover:opacity-70 sm:scale-100"
-                href={`${GITHUB_REPO}/tree/main/packages/${data()?.name}`}
-                target="_blank"
-                rel="noopener"
-              >
-                <FaBrandsGithub size={28} />
-                <div class="mask-image-[linear-gradient(to_bottom,transparent_12px,#000)] absolute top-[32px] left-0 -scale-y-100 opacity-20 blur-[2px]">
-                  <FaBrandsGithub size={28} />
-                </div>
-              </a>
-            </div>
+            <Heading name={data()?.name} />
           </div>
 
           <div class="my-8">
