e9a516d58ecca4a4f7895b97f3c622cf1c46002f
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-26 23:53:36 +0200

    Add jsdoc to static-store

--- packages/static-store/src/index.ts
+++ packages/static-store/src/index.ts
@@ -1,19 +1,23 @@
+import { accessWith, isObject, SetterParam } from "@solid-primitives/utils";
 import {
   Accessor,
   batch,
   createMemo,
   createSignal,
+  EffectFunction,
   getListener,
   getOwner,
+  MemoOptions,
+  NoInfer,
   onMount,
   runWithOwner,
   sharedConfig,
   Signal,
   untrack,
 } from "solid-js";
-import { accessWith, AnyStatic, isObject, isServer, SetterParam } from "@solid-primitives/utils";
+import { isServer } from "solid-js/web";
 
-export type StaticStoreSetter<T extends Readonly<AnyStatic>> = {
+export type StaticStoreSetter<T extends object> = {
   (setter: (prev: T) => Partial<T>): T;
   (state: Partial<T>): T;
   <K extends keyof T>(key: K, state: SetterParam<T[K]>): T;
@@ -26,8 +30,20 @@ export type StaticStoreSetter<T extends Readonly<AnyStatic>> = {
  * ```ts
  * [access: Readonly<T>, write: StaticStoreSetter<T>]
  * ```
+ * @example
+ * ```ts
+ * const [size, setSize] = createStaticStore({ width: 0, height: 0 });
+ *
+ * el.addEventListener("resize", () => {
+ *  setSize({ width: el.offsetWidth, height: el.offsetHeight });
+ * });
+ *
+ * createEffect(() => {
+ *   console.log(size.width, size.height);
+ * })
+ * ```
  */
-export function createStaticStore<T extends Readonly<AnyStatic>>(
+export function createStaticStore<T extends object>(
   init: T,
 ): [access: T, write: StaticStoreSetter<T>] {
   const copy = { ...init };
@@ -80,26 +96,59 @@ export function createStaticStore<T extends Readonly<AnyStatic>>(
  * [access: Readonly<T>, write: StaticStoreSetter<T>]
  * ```
  */
-export function createHydratableStaticStore<T extends Readonly<AnyStatic>>(
+export function createHydratableStaticStore<T extends object>(
   serverValue: T,
   update: () => T,
 ): ReturnType<typeof createStaticStore<T>> {
   if (isServer) return createStaticStore(serverValue);
+
   if (sharedConfig.context) {
     const [state, setState] = createStaticStore(serverValue);
     onMount(() => setState(update()));
     return [state, setState];
   }
+
   return createStaticStore(update());
 }
 
-export function createDerivedStaticStore<T extends object>(fn: () => T): T {
-  const cache = new Map<keyof T, Accessor<any>>(),
+/**
+ * A derived version of the {@link createStaticStore}. It will use the update function to derive the value of the store. It will only update when the dependencies of the update function change.
+ * @param fn a reactive function to derive the value of the store
+ * @returns a shallow, reactive, static store object
+ * @example
+ * ```ts
+ * const [size, setSize] = createSignal({ width: 0, height: 0 });
+ *
+ * el.addEventListener("resize", () => {
+ *  setSize({ width: el.offsetWidth, height: el.offsetHeight });
+ * });
+ *
+ * const store = createDerivedStaticStore(size);
+ *
+ * createEffect(() => {
+ *   console.log(store.width, store.height);
+ * })
+ * ```
+ */
+export function createDerivedStaticStore<Next extends Prev & object, Prev = Next>(
+  fn: EffectFunction<undefined | NoInfer<Prev>, Next>,
+): Next;
+export function createDerivedStaticStore<Next extends Prev & object, Init = Next, Prev = Next>(
+  fn: EffectFunction<Init | Prev, Next>,
+  value: Init,
+  options?: MemoOptions<Next>,
+): Next;
+export function createDerivedStaticStore<T extends object>(
+  fn: EffectFunction<T | undefined, T>,
+  value?: T,
+  options?: MemoOptions<T>,
+): T {
+  const cache = new Map<keyof T, Accessor<T[keyof T]>>(),
     o = getOwner(),
-    fnMemo = createMemo(fn),
+    fnMemo = createMemo(fn, value, options),
     store = { ...untrack(fnMemo) };
 
-  for (const key in store) {
+  for (const key in store)
     Object.defineProperty(store, key, {
       get() {
         let propMemo = cache.get(key);
@@ -111,7 +160,6 @@ export function createDerivedStaticStore<T extends object>(fn: () => T): T {
       },
       enumerable: true,
     });
-  }
 
   return store;
 }
