fd630ef14fcda2c1cb4eb2af9c369d2e3f11ff3b
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-26 23:17:12 +0200

    Add static-store pkg & add `createDerivedStaticStore`

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,11 +4,9 @@ import {
   createSignal,
   Accessor,
   untrack,
-  batch,
   AccessorArray,
   EffectFunction,
   NoInfer,
-  Signal,
   SignalOptions,
   sharedConfig,
   onMount,
@@ -22,8 +20,6 @@ import type {
   Noop,
   AnyObject,
   AnyFunction,
-  SetterParam,
-  AnyStatic,
 } from "./types";
 
 export * from "./types";
@@ -256,86 +252,6 @@ export function createHydratableSignal<T>(
 
 /** @deprecated use {@link createHydratableSignal} instead */
 export const createHydrateSignal = createHydratableSignal;
-
-export type StaticStoreSetter<T extends Readonly<AnyStatic>> = {
-  (setter: (prev: T) => Partial<T>): T;
-  (state: Partial<T>): T;
-  <K extends keyof T>(key: K, state: SetterParam<T[K]>): T;
-};
-
-/**
- * A shallowly wrapped reactive store object. It behaves similarly to the createStore, but with limited features to keep it simple. Designed to be used for reactive objects with static keys, but dynamic values, like reactive Event State, location, etc.
- * @param init initial value of the store
- * @returns
- * ```ts
- * [access: Readonly<T>, write: StaticStoreSetter<T>]
- * ```
- */
-export function createStaticStore<T extends Readonly<AnyStatic>>(
-  init: T,
-): [access: T, write: StaticStoreSetter<T>] {
-  const copy = { ...init };
-  const store = { ...init };
-  const cache = new Map<PropertyKey, Signal<any>>();
-
-  const getValue = <K extends keyof T>(key: K): T[K] => {
-    const saved = cache.get(key);
-    if (saved) return saved[0]();
-    const signal = createSignal<any>(copy[key], {
-      internal: true,
-    });
-    cache.set(key, signal);
-    delete copy[key];
-    return signal[0]();
-  };
-
-  const setValue = <K extends keyof T>(key: K, value: SetterParam<any>): void => {
-    const saved = cache.get(key);
-    if (saved) return saved[1](value);
-    if (key in copy) copy[key] = accessWith(value, [copy[key]]);
-  };
-
-  for (const key in init) {
-    Object.defineProperty(store, key, { get: getValue.bind(void 0, key) });
-  }
-
-  const setter = (a: ((prev: T) => Partial<T>) | Partial<T> | keyof T, b?: SetterParam<any>) => {
-    if (isObject(a)) {
-      const entries = untrack(
-        () => Object.entries(accessWith(a, store) as Partial<T>) as [any, any][],
-      );
-      batch(() => {
-        for (const [key, value] of entries) setValue(key, () => value);
-      });
-    } else setValue(a, b);
-    return store;
-  };
-
-  return [store, setter];
-}
-
-/**
- * A hydratable version of the {@link createStaticStore}. It will use the serverValue on the server and the update function on the client. If initialized during hydration it will use serverValue as the initial value and update it once hydration is complete.
- *
- * @param serverValue initial value of the state on the server
- * @param update called once on the client or on hydration to initialize the value
- * @returns
- * ```ts
- * [access: Readonly<T>, write: StaticStoreSetter<T>]
- * ```
- */
-export function createHydratableStaticStore<T extends Readonly<AnyStatic>>(
-  serverValue: T,
-  update: () => T,
-): ReturnType<typeof createStaticStore<T>> {
-  if (isServer) return createStaticStore(serverValue);
-  if (sharedConfig.context) {
-    const [state, setState] = createStaticStore(serverValue);
-    onMount(() => setState(update()));
-    return [state, setState];
-  }
-  return createStaticStore(update());
-}
 
 /**
  * Handle items removed and added to the array by diffing it by refference.
