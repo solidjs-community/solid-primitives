573a11a4d4656432b88fbfb1fbbc9082015b3e63
Damian Tarnawski <gthetarnav@gmail.com>
2023-03-29 17:52:16 +0200

    Cleanup lifecycle sandbox

--- packages/lifecycle/dev/index.tsx
+++ packages/lifecycle/dev/index.tsx
@@ -3,23 +3,6 @@ import { render } from "solid-js/web";
 import "uno.css";
 import { onElementConnect } from "../src";
 
-// const App: Component = () => {
-//   const [count, setCount] = createSignal(0);
-//   const increment = () => setCount(count() + 1);
-
-//   return (
-//     <div class="box-border flex min-h-screen w-full flex-col items-center justify-center space-y-4 bg-gray-800 p-24 text-white">
-//       <div class="wrapper-v">
-//         <h4>Counter component</h4>
-//         <p class="caption">it's very important...</p>
-//         <button class="btn" onClick={increment}>
-//           {count()}
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
 function App() {
   const [count, setCount] = createSignal(1);
   const increment = () => setCount(count() + 1);
