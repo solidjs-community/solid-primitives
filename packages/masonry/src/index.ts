b6121562c8a2c98aa52107433b90c25d434deac1
Damian Tarnawski <gthetarnav@gmail.com>
2023-04-15 14:30:28 +0200

    Add masonry jsdoc

--- packages/masonry/src/index.ts
+++ packages/masonry/src/index.ts
@@ -1,33 +1,6 @@
 import { Accessor, createMemo, createSignal, mapArray } from "solid-js";
 import { MaybeAccessor, asAccessor } from "@solid-primitives/utils";
 
-export type MasonryItemData<T> = {
-  source: T;
-  order: Accessor<number>;
-  margin: Accessor<number>;
-  height: Accessor<number>;
-  column: Accessor<number>;
-};
-
-export type MapElement<TSource, TElement> = (
-  data: MasonryItemData<TSource>,
-  index: Accessor<number>,
-) => TElement;
-
-export type MasonryOptions<TSource> = {
-  source: Accessor<readonly TSource[] | false | null | undefined>;
-  mapHeight: (item: TSource) => MaybeAccessor<number>;
-  mapElement?: undefined;
-  columns: MaybeAccessor<number>;
-};
-
-export type MasonryOptionsWithElement<TSource, TElement> = Omit<
-  MasonryOptions<TSource>,
-  "mapElement"
-> & {
-  mapElement: MapElement<TSource, TElement>;
-};
-
 function getShortestColumn(heights: number[]): number {
   let min = 0;
   for (let i = 0, record = Infinity; i < heights.length; i++)
@@ -35,15 +8,75 @@ function getShortestColumn(heights: number[]): number {
   return min;
 }
 
+const getNColumns = (n: number): any => Array.from({ length: n }, () => []);
+
 const $SET_STYLE = Symbol("set-style");
 
 const noopIndex = () => 0;
+
+export type MasonryItemData<T> = {
+  /**
+   * Reference to the source item.
+   */
+  source: T;
+  /**
+   * The calculated flex order of the item.
+   */
+  order: Accessor<number>;
+  /**
+   * Calculated margin-bottom size to prevent the next item from switching columns.
+   */
+  margin: Accessor<number>;
+  /**
+   * Height of the item provided by {@link MasonryOptions.mapHeight}.
+   */
+  height: Accessor<number>;
+  /**
+   * The column the item falls into. The first column is 0.
+   */
+  column: Accessor<number>;
+};
+
+/**
+ * The options for the masonry {@link createMasonry}
+ */
+export type MasonryOptions<TSource, TElement> = {
+  /**
+   * The source array of items to be mapped.
+   * When updating the array, the masonry will be recalculated.
+   * The items are compared by reference.
+   */
+  source: Accessor<readonly TSource[] | false | null | undefined>;
+  /**
+   * A function that maps the source item to a height.
+   * This function is not reactive, it will be called only once for each item.
+   * To provede a reactice height, return an accessor.
+   */
+  mapHeight: (item: TSource) => MaybeAccessor<number>;
+  /**
+   * A function that maps the source item to an element to render.
+   * This function is not reactive, it will be called only once for each item.
+   */
+  mapElement: (data: MasonryItemData<TSource>, index: Accessor<number>) => TElement;
+  /**
+   * The number of columns to split the items into.
+   * This can be an accessor to provide a reactive number of columns.
+   */
+  columns: MaybeAccessor<number>;
+};
+
+/**
+ * The options for the masonry {@link createMasonry} without the {@link MasonryOptions.mapElement} parameter.
+ */
+export type MasonryOptionsNoElements<TSource> = Omit<MasonryOptions<TSource, any>, "mapElement"> & {
+  mapElement?: undefined;
+};
 
 const mapData = <TSource, TElement>(
   source: TSource,
   track: VoidFunction,
   mapHeight: (item: TSource) => MaybeAccessor<number>,
-  mapElement: MapElement<TSource, TElement> | undefined,
+  mapElement: MasonryOptions<TSource, TElement>["mapElement"] | undefined,
   index: Accessor<number>,
 ) => {
   let orderValue = 0,
@@ -72,20 +105,65 @@ const mapData = <TSource, TElement>(
   return data;
 };
 
+/**
+ * Creates a masonry layout calculation from a reactive source array.
+ *
+ * It splits the items into columns and calculates the order based on the height of each item.
+ *
+ * The masonary is expected to be rendreded in a flex container with `flex-direction: column`, `flex-wrap: wrap` and limited height
+ * to force the items to wrap.
+ *
+ * @param options The options for the masonry {@link MasonryOptions}
+ *
+ * @returns An array accessor of {@link MasonryItemData} or elements returned by {@link MasonryOptions.mapElement} function with the calculated container height.
+ * ```ts
+ * // if mapElement is not provided
+ * Accessor<MasonryItemData<TSource>[]> & { height: Accessor<number> }
+ * // if mapElement is provided
+ * Accessor<TElement[]> & { height: Accessor<number> }
+ * ```
+ *
+ * @example
+ * ```tsx
+ * const [source, setSource] = createSignal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+ *
+ * const masonry = createMasonry({
+ *   source,
+ *   mapHeight: i => i * 100,
+ *   columns: 3,
+ *   mapElement: (data, index) => <div style={{
+ *     height: `${data.source * 100}px`,
+ *     order: data.order(),
+ *     "margin-bottom": `${data.margin()}px`,
+ *   }}>
+ *     Item {index()}
+ *   </div>,
+ * })
+ *
+ * <div style={{
+ *   display: "flex",
+ *   "flex-direction": "column",
+ *   "flex-wrap": "wrap",
+ *   height: masonry.height()
+ * }}>
+ *   {masonry()}
+ * </div>
+ * ```
+ */
+
 export function createMasonry<TSource, TElement>(
-  options: MasonryOptionsWithElement<TSource, TElement>,
+  options: MasonryOptions<TSource, TElement>,
 ): Accessor<TElement[]> & { height: Accessor<number> };
 
 export function createMasonry<TSource>(
-  options: MasonryOptions<TSource>,
+  options: MasonryOptionsNoElements<TSource>,
 ): Accessor<MasonryItemData<TSource>[]> & { height: Accessor<number> };
 
 export function createMasonry<T>(
-  options: MasonryOptions<T> | MasonryOptionsWithElement<T, any>,
+  options: MasonryOptionsNoElements<T> | MasonryOptions<T, any>,
 ): Accessor<any[]> & { height: Accessor<number> } {
-  const { source, mapHeight, mapElement } = options,
+  const { source, mapHeight, mapElement, columns } = options,
     [memo, setMemo] = createSignal<VoidFunction>(),
-    getCols = asAccessor(options.columns),
     mapped = createMemo(
       mapArray(
         source,
@@ -94,13 +172,17 @@ export function createMasonry<T>(
           : source => mapData(source, () => memo()?.(), mapHeight, mapElement, noopIndex),
       ),
     ),
-    getOrder = createMemo(() =>
-      Array.from({ length: getCols() }, (): ReturnType<typeof mapped> => []),
-    ),
+    getColumns: Accessor<ReturnType<typeof mapped>[]> =
+      typeof columns === "number"
+        ? (() => {
+            const cols = getNColumns(columns);
+            return () => cols;
+          })()
+        : createMemo(() => getNColumns(columns())),
     height = setMemo(() =>
       createMemo(() => {
         const items = mapped(),
-          columns = getOrder(),
+          columns = getColumns(),
           heights = new Array(columns.length).fill(0);
 
         for (let i = 0; i < items.length; i++) {
@@ -125,9 +207,8 @@ export function createMasonry<T>(
 
         return height;
       }),
-    );
-
-  const result = mapElement ? createMemo(() => mapped().map(i => i.element)) : mapped;
+    ),
+    result = mapElement ? createMemo(() => mapped().map(i => i.element)) : mapped;
 
   (result as any).height = height;
 
