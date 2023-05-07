955f9a70764faad1944bffd51eb0b7ea796abe07
Damian Tarnawski <gthetarnav@gmail.com>
2023-05-07 20:54:53 +0200

    Include playgrounds in the tailwind context array

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -3,10 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: ["w-screen", "h-screen"],
-  mode: "jit",
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "../packages/*/dev/*.{ts,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
