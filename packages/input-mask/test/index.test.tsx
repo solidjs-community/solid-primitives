281006e43781ae97008302db7f55b74023de2b50
Alex Lohr <alexthkloss@web.de>
2023-06-03 22:13:14 +0200

    input-mask: new pattern display primitive (#454)
    A new function to show a pattern how the remaining input value needs to look to make input-masks more accessible.

--- packages/input-mask/test/index.test.tsx
+++ packages/input-mask/test/index.test.tsx
@@ -1,34 +1,18 @@
 import { describe, expect, it } from "vitest";
 
-import { render } from "solid-js/web";
+import { render } from "@solidjs/testing-library";
 
-import { createInputMask } from "../src/index";
-import type { JSX } from "solid-js";
+import { createInputMask, createMaskPattern } from "../src/index";
+
+const dispatchInputEvent = (node: HTMLElement) =>
+  new Promise<void>(resolve => {
+    node.addEventListener("input", () => resolve(), { once: true });
+    node.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }));
+  });
 
 describe("createInputMask", () => {
-  const renderTest = (testCase: () => JSX.Element) => {
-    const container = document.createElement("div");
-    document.body.appendChild(container);
-    const unmount = render(testCase, container);
-    return {
-      container,
-      unmount: () => {
-        unmount();
-        document.body.removeChild(container);
-      },
-    };
-  };
-
-  const dispatchInputEvent = (node: HTMLElement) =>
-    new Promise<void>(resolve => {
-      node.addEventListener("input", () => resolve(), { once: true });
-      node.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }));
-    });
-
   it("adds placeholder (e.g. to an iso date)", async () => {
-    const { container, unmount } = renderTest(() => (
-      <input onInput={createInputMask("9999-99-99")} />
-    ));
+    const { container } = render(() => <input onInput={createInputMask("9999-99-99")} />);
     const input = container.querySelector("input") as HTMLInputElement;
     input.value = "11111";
     await dispatchInputEvent(input);
@@ -45,13 +29,10 @@ describe("createInputMask", () => {
     input.value = "1111-11-111";
     await dispatchInputEvent(input);
     expect(input.value).toBe("1111-11-11");
-    unmount();
   });
 
   it("removes wrongful input (e.g. from iso-date)", async () => {
-    const { container, unmount } = renderTest(() => (
-      <input onInput={createInputMask("9999-99-99")} />
-    ));
+    const { container } = render(() => <input onInput={createInputMask("9999-99-99")} />);
     const input = container.querySelector("input") as HTMLInputElement;
     input.value = "a";
     await dispatchInputEvent(input);
@@ -59,11 +40,10 @@ describe("createInputMask", () => {
     input.value = "-";
     await dispatchInputEvent(input);
     expect(input.value).toBe("");
-    unmount();
   });
 
   it("works with regex mask", async () => {
-    const { container, unmount } = renderTest(() => (
+    const { container } = render(() => (
       <input
         onInput={createInputMask([
           /[^0-9a-zäöüß\-_/]|^(https?:\/\/|)(meet\.goto\.com|gotomeet\.me|)\/?/gi,
@@ -75,6 +55,31 @@ describe("createInputMask", () => {
     input.value = "https://meet.goto.com/test";
     await dispatchInputEvent(input);
     expect(input.value).toBe("test");
-    unmount();
+  });
+});
+
+describe("createMaskPattern", () => {
+  it("adds data-mask-value and data-mask-pattern to the previous element", async () => {
+    const { container } = render(() => (
+      <div>
+        <label></label>
+        <input
+          placeholder="YYYY-MM-DD"
+          onInput={createMaskPattern(createInputMask("9999-99-99"), () => "YYYY-MM-DD")}
+        />
+      </div>
+    ));
+    const label = container.querySelector("label")!;
+    const input = container.querySelector("input")!;
+    expect(label.hasAttribute("data-mask-value")).toBe(false);
+    expect(label.hasAttribute("data-mask-pattern")).toBe(false);
+    input.value = "1";
+    await dispatchInputEvent(input);
+    expect(label.getAttribute("data-mask-value")).toBe("1");
+    expect(label.getAttribute("data-mask-pattern")).toBe("YYY-MM-DD");
+    input.value = "20771";
+    await dispatchInputEvent(input);
+    expect(label.getAttribute("data-mask-value")).toBe("2077-1");
+    expect(label.getAttribute("data-mask-pattern")).toBe("M-DD");
   });
 });
