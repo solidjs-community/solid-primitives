b4294233348739f12f3176f08c0d67e22fe9b3c6
Ryan Conceicao <ryoid@users.noreply.github.com>
2023-09-24 17:29:15 +0800

    fix: jsdoc

--- packages/selectors/src/index.ts
+++ packages/selectors/src/index.ts
@@ -3,8 +3,8 @@ import { createSelector } from "solid-js";
 /**
  * Wrapper around `createSelector` to create a selector for an array.
  *
- * @param source source array signal to create the selector from
- * @param options set computation name for debugging pourposes
+ * @param source - source array signal to create the selector from
+ * @param options - set computation name for debugging purposes
  * @returns selector for the array
  *
  * @see https://github.com/solidjs-community/solid-primitives/tree/main/packages/selectors#createArraySelector
