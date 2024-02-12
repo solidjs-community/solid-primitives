a1b62829554ad9c02a1d463722bc9c902cc4f78b
David Bieregger <david-e@outlook.de>
2024-02-12 21:06:48 +0100

    Remove unused type exports in utils
    This commit removes the ResolvedJSXElement and ResolvedChildren type exports from the types.ts file in the utils package.
    These types were previously imported from the solid-js library
    but are no longer needed
    because now they are properly exported by solid-js.

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -2,12 +2,6 @@ import type { Accessor, Setter } from "solid-js";
 
 export type { EffectOptions, OnOptions } from "solid-js";
 
-// export types that aren't exported by solid-js main module
-export type {
-  ResolvedJSXElement,
-  ResolvedChildren,
-} from "../node_modules/solid-js/types/reactive/signal.js";
-
 /**
  * Can be single or in an array
  */
