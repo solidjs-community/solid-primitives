5e0422ca9cb9cea7fd02f8da1bda53f21be20204
David Bieregger <david-e@outlook.de>
2024-02-14 21:24:47 +0100

    Add export again, but forward from solid-js

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -2,6 +2,12 @@ import type { Accessor, Setter } from "solid-js";
 
 export type { EffectOptions, OnOptions } from "solid-js";
 
+// export types can be deleted in the next major
+export type {
+  ResolvedJSXElement,
+  ResolvedChildren,
+} from "solid-js/types/reactive/signal.js";
+
 /**
  * Can be single or in an array
  */
