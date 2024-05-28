27852aaab57e3d97856795789ae79e2ce569f4b0
thetarnav <thetarnav@users.noreply.github.com>
2024-05-28 15:50:03 +0000

    Format

--- packages/list/test/index.bench.tsx
+++ packages/list/test/index.bench.tsx
@@ -3,7 +3,6 @@ import { onMount, For } from "solid-js";
 import { List } from "../src/index.js";
 import { render } from "solid-js/web";
 
-
 describe("benchmark", () => {
   const ITEMS = 1000;
   const renderedFor = new Set();
@@ -12,16 +11,25 @@ describe("benchmark", () => {
   document.body.appendChild(divFor);
   let unmountFor = () => {};
 
-  bench('For', () => new Promise((resolve) => {
-    const ItemFor = (props: { number: number }) => {
-      onMount(() => {
-        renderedFor.add(props.number);
-        if (renderedFor.size === ITEMS) { resolve(); }
-      })
-      return (<span>{props.number}</span>);
-    };
-    unmountFor = render(() => <For each={listFor}>{(item) => <ItemFor number={item} />}</For>, divFor);
-  }))
+  bench(
+    "For",
+    () =>
+      new Promise(resolve => {
+        const ItemFor = (props: { number: number }) => {
+          onMount(() => {
+            renderedFor.add(props.number);
+            if (renderedFor.size === ITEMS) {
+              resolve();
+            }
+          });
+          return <span>{props.number}</span>;
+        };
+        unmountFor = render(
+          () => <For each={listFor}>{item => <ItemFor number={item} />}</For>,
+          divFor,
+        );
+      }),
+  );
 
   const renderedList = new Set();
   const listList = Array.from({ length: ITEMS }, (_, i) => i);
@@ -29,16 +37,25 @@ describe("benchmark", () => {
   document.body.appendChild(divList);
   let unmountList = () => {};
 
-  bench('List', () => new Promise((resolve) => {
-    const ItemList = (props: { number: number }) => {
-      onMount(() => {
-        renderedList.add(props.number);
-        if (renderedList.size === ITEMS) { resolve(); }
-      });
-      return (<span>{props.number}</span>);
-    }
-    unmountList = render(() => <List each={listList}>{(item) => <ItemList number={item()} />}</List>, divList);
-  }));
+  bench(
+    "List",
+    () =>
+      new Promise(resolve => {
+        const ItemList = (props: { number: number }) => {
+          onMount(() => {
+            renderedList.add(props.number);
+            if (renderedList.size === ITEMS) {
+              resolve();
+            }
+          });
+          return <span>{props.number}</span>;
+        };
+        unmountList = render(
+          () => <List each={listList}>{item => <ItemList number={item()} />}</List>,
+          divList,
+        );
+      }),
+  );
 
   afterAll(() => {
     unmountFor();
