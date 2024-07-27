8fe0619ae6eff18c424e9ff13bb205aa4b7dcd47
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2024-07-27 14:03:52 -0400

    add controls to demo

--- packages/virtualized-list/dev/index.tsx
+++ packages/virtualized-list/dev/index.tsx
@@ -1,3 +1,4 @@
+import { createSignal } from "solid-js";
 import type { Component } from "solid-js";
 import { VirtualList } from "../src/index.jsx";
 
@@ -5,30 +6,114 @@ const intl = new Intl.NumberFormat();
 
 const items = new Array(100_000).fill(0).map((_, i) => i);
 
+const clampRange = (min: number, max: number, v: number) => (v < min ? min : v > max ? max : v);
+
 const App: Component = () => {
+  const [listLength, setListLength] = createSignal(100_000);
+  const [overscanCount, setOverscanCount] = createSignal(5);
+  const [rootHeight, setRootHeight] = createSignal(240);
+  const [rowHeight, setRowHeight] = createSignal(24);
+
   return (
     <div class="box-border flex min-h-screen w-full flex-col space-y-4 bg-gray-800 p-24 text-white">
+      <div class="grid w-full grid-cols-4 bg-white p-4 text-gray-800 shadow-md">
+        <div>
+          <DemoControl
+            label="Number of rows"
+            max={100_000}
+            min={1}
+            name="rowCount"
+            setValue={setListLength}
+            value={listLength()}
+          />
+        </div>
+        <div>
+          <DemoControl
+            label="overscanCount"
+            max={100}
+            min={1}
+            name="overscanCount"
+            setValue={setOverscanCount}
+            value={overscanCount()}
+          />
+        </div>
+        <div>
+          <DemoControl
+            label="rootHeight"
+            max={1_000}
+            min={100}
+            name="rootHeight"
+            setValue={setRootHeight}
+            value={rootHeight()}
+          />
+        </div>
+        <div>
+          <DemoControl
+            label="rowHeight"
+            max={100}
+            min={10}
+            name="rowHeight"
+            setValue={setRowHeight}
+            value={rowHeight()}
+          />
+        </div>
+      </div>
+
       <VirtualList
-        each={items}
-        overscanCount={5}
-        rootHeight={720}
-        rowHeight={24}
-        class="bg-white text-black"
+        each={items.slice(0, listLength())}
+        overscanCount={overscanCount()}
+        rootHeight={rootHeight()}
+        rowHeight={rowHeight()}
+        class="bg-white text-gray-800"
       >
-        {item => <VirtualListItem item={item} />}
+        {item => <VirtualListItem item={item} height={rowHeight()} />}
       </VirtualList>
     </div>
   );
 };
 
-type VirtualListItemProps = {
-  item: number;
+type DemoControlProps = {
+  label: string;
+  max: number;
+  min: number;
+  name: string;
+  setValue: (v: number) => void;
+  value: number;
 };
 
-const VirtualListItem: Component<VirtualListItemProps> = (props: { item: number }) => {
+const DemoControl: Component<DemoControlProps> = props => (
+  <label>
+    <div class="text-s">{props.label}:</div>
+    <input
+      type="number"
+      name={props.name}
+      value={props.value}
+      min={props.min}
+      max={props.max}
+      class="mx-2 w-32"
+      onInput={e => {
+        props.setValue(clampRange(props.min, props.max, Number(e.target.value)));
+      }}
+    />
+    <div class="text-xs">
+      ({intl.format(props.min)} min, {intl.format(props.max)} max)
+    </div>
+  </label>
+);
+
+type VirtualListItemProps = {
+  item: number;
+  height: number;
+};
+
+const VirtualListItem: Component<VirtualListItemProps> = props => {
   console.log("ran", props.item);
 
-  return <div>{intl.format(props.item)}</div>;
+  return (
+    <div style={{ height: `${props.height}px` }} class="align-center flex">
+      {intl.format(props.item)}
+    </div>
+  );
 };
 
 export default App;
