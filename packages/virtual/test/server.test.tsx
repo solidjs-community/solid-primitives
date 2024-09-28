4e3925f1e509b29ee3c661af74b6ace8087603c3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2024-09-28 14:32:14 -0400

    make row heights in tests more realistic

--- packages/virtual/test/server.test.tsx
+++ packages/virtual/test/server.test.tsx
@@ -14,7 +14,7 @@ describe("VirtualList", () => {
         rowHeight={10}
         class="classString"
       >
-        {item => <div style={{ height: "100px" }}>{item}</div>}
+        {item => <div style={{ height: "10px" }}>{item}</div>}
       </VirtualList>
     ));
 
@@ -23,9 +23,9 @@ describe("VirtualList", () => {
         '<div style="overflow:auto;height:20px" class="classString">',
         '  <div style="position:relative;width:100%;height:10000px">',
         '    <div style="position:absolute;top:0px">',
-        '      <div style="height:100px">0</div>',
-        '      <div style="height:100px">1</div>',
-        '      <div style="height:100px">2</div>',
+        '      <div style="height:10px">0</div>',
+        '      <div style="height:10px">1</div>',
+        '      <div style="height:10px">2</div>',
         "    </div>",
         "  </div>",
         "</div>",
