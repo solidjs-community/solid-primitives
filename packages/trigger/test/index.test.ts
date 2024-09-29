489793e5d7fccf7ffc2811203464f917646f9bd8
thetarnav <thetarnav@users.noreply.github.com>
2024-09-29 11:34:20 +0000

    Format

--- packages/trigger/test/index.test.ts
+++ packages/trigger/test/index.test.ts
@@ -22,20 +22,20 @@ describe("createTriggerCache", () => {
       dispose();
     }));
 
-    test("dirtyAll", () =>
-      createRoot(dispose => {
-        const [track, , dirtyAll] = createTriggerCache(Map);
-        let runs = -1;
-        createComputed(() => {
-          track(1);
-          runs++;
-        });
-        expect(runs).toBe(0);
-        dirtyAll();
-        expect(runs).toBe(1);
+  test("dirtyAll", () =>
+    createRoot(dispose => {
+      const [track, , dirtyAll] = createTriggerCache(Map);
+      let runs = -1;
+      createComputed(() => {
+        track(1);
+        runs++;
+      });
+      expect(runs).toBe(0);
+      dirtyAll();
+      expect(runs).toBe(1);
 
-        dispose();
-      }));
+      dispose();
+    }));
 
   test("weak trigger cache", () =>
     createRoot(dispose => {
