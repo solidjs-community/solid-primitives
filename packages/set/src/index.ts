f39dd15fc00d36db81b536cf9259f40ba742be01
Maciej Kwaśniak <contact@exelord.com>
2024-10-04 11:14:26 +0200

    feat: Optimize Set initialization

--- packages/set/src/index.ts
+++ packages/set/src/index.ts
@@ -20,7 +20,12 @@ export class ReactiveSet<T> extends Set<T> {
 
   constructor(values?: Iterable<T> | null) {
     super();
-    if (values) for (const v of values) super.add(v);
+
+    if (values) {
+      batch(() => {
+        for (const value of values) super.add(value);
+      });
+    }
   }
 
   [Symbol.iterator](): SetIterator<T> {
@@ -113,7 +118,12 @@ export class ReactiveWeakSet<T extends object> extends WeakSet<T> {
 
   constructor(values?: Iterable<T> | null) {
     super();
-    if (values) for (const v of values) super.add(v);
+
+    if (values) {
+      batch(() => {
+        for (const value of values) super.add(value);
+      });
+    }
   }
 
   has(value: T): boolean {
