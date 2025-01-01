ab7b9ddd0323c79158ff9924056bd69a29fad0f6
Alex <git@pentive.com>
2025-01-01 14:45:35 -0600

    demo that ReactiveMap works as expected

--- packages/keyed/dev/index.tsx
+++ packages/keyed/dev/index.tsx
@@ -3,6 +3,7 @@ import { Component } from "solid-js";
 import Key from "./key.js";
 import Entries from "./entries.js";
 import MapEntries from "./mapEntries.js";
+import ReactiveMapEntries from "./reactiveMapEntries.js";
 
 const App: Component = () => {
   return (
@@ -18,6 +19,10 @@ const App: Component = () => {
       <div class="wrapper-v">
         <h4>MapEntries</h4>
         <MapEntries />
+      </div>
+      <div class="wrapper-v">
+        <h4>MapEntries with ReactiveMap</h4>
+        <ReactiveMapEntries />
       </div>
     </div>
   );
