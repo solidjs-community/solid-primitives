4bd178f20ef6d4ecad8e3e3c3d1eb1fd95007393
Alex Lohr <alexthkloss@web.de>
2026-05-27 16:59:34 +0200

    feat: fromStream - use TextEncoder feat: fromJSONStream - new primitive test: improve tests feat: makeRetrying WIP

--- packages/async/src/index.ts
+++ packages/async/src/index.ts
@@ -1,6 +1,5 @@
-import {
-  onCleanup,
-} from "solid-js";
+import { onCleanup, createMemo } from "solid-js";
+import type { Accessor, ComputeFunction } from "solid-js";
 
 const chained = new Map<() => AbortSignal, (() => void)[]>();
 
@@ -16,7 +15,7 @@ const chained = new Map<() => AbortSignal, (() => void)[]>();
  */
 export function fromStream<Args extends [] | [any, ...any[]]>(fetcher: (...args: Args) => Promise<Response | ReadableStream> | Response | ReadableStream) {
   return async function*(...args: Args) {
-    let parts = '';
+    let parts = '', decoder;
     const source = await fetcher(...args);
     const stream = source instanceof Response ? source.body : source;
     const reader = stream?.getReader();
@@ -28,12 +27,72 @@ export function fromStream<Args extends [] | [any, ...any[]]>(fetcher: (...args:
       const { done, value } = await reader.read();
       if (done) return;
       if (value) {
-        parts += typeof value === 'string' ? value : String.fromCharCode(value);
+        if (typeof value !== 'string') {
+          parts += (decoder ??= new TextDecoder()).decode(value, { stream: true });
+        } else {
+          parts += value;
+        }
       }
       yield parts;
     }
   }
 }
+
+const endMatcher = /(?:\W)(t|tru?|f|fa|fals?|n|nul?)$/;
+const endLiterals: Record<string, string> = {
+  t: "rue", tr: "ue", tru: "e", 
+  f: "alse", fa: "lse", fal: "se", fals: "e",
+  n: "ull", nu: "ll", nul: "l"
+};
+
+const closeJSONPart = (json: string) =>
+  json.replace(/[,:]\s*$/, "") + 
+    (endMatcher.test(json) && endLiterals[RegExp.$1] || "") +
+    [...json].reduce((stack: string[], char: string) => {
+      const close = ({ '"': '"', "[": "]", "{": "}" })[char];
+      if (char === stack[0]) stack.shift();
+      else if (close) stack.unshift(close);
+      return stack;
+    }, []).join("");
+
+/**
+ * **aggregates web stream chunks into a memo supporting partial JSON**
+ * ```ts
+ * // from Response:
+ * const streamed = createMemo(fromStream(() => fetch(url())));
+ *
+ * // from another ReadableStream:
+ * const streamed = createMemo(fromStream(() => getStream()));
+ * ```
+ */
+export function fromJSONStream<Args extends [] | [any, ...any[]]>(fetcher: (...args: Args) => Promise<Response | ReadableStream> | Response | ReadableStream) {
+  return async function*(...args: Args) {
+    let parts = '', decoder;
+    const source = await fetcher(...args);
+    const stream = source instanceof Response ? source.body : source;
+    const reader = stream?.getReader();
+    if (!reader) {
+      console.warn('No ReadableStream found!')
+      return;
+    }
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) return;
+      if (value) {
+        if (typeof value !== 'string') {
+          parts += (decoder ??= new TextDecoder()).decode(value, { stream: true });
+        } else {
+          parts += value;
+        }
+      }
+      try {
+        const parsed = JSON.parse(closeJSONPart(parts))
+        yield parsed;
+      } catch (e) { /* ignore erroneous states, recover later */ }
+    }
+  }
+}
+
 
 export type AbortableReturn = [
   signal: () => AbortSignal,
@@ -42,8 +101,11 @@ export type AbortableReturn = [
 ]
 
 export type AbortableOptions = {
-  noAutoAbort?: boolean;
+  /** Should abort when a new signal is requested, default is true */
+  autoAbort?: boolean;
+  /** Automatically abort after a timeout in ms if set */
   timeout?: number;
+  /** Aborts if a parent signal is aborted (e.g. first optimistic update after a second write) */
   chainTo?: () => AbortSignal;
 };
 
@@ -59,14 +121,14 @@ export type AbortableOptions = {
  *
  * Options are optional and include:
  * - `timeout`: time in Milliseconds after which the fetcher aborts automatically
- * - `noAutoAbort`: can be set to true to make a new source not automatically abort a previous request
+ * - `autoAbort`: can be set to true to make a new source not automatically abort a previous request
  * - `chainTo`: listen to another abort signal to abort this signal
  */
 export function makeAbortable(
   options: AbortableOptions = {},
 ): AbortableReturn {
   let controller: AbortController;
-  let timeout: NodeJS.Timeout | number | undefined;
+  let timeout: ReturnType<typeof setTimeout> | undefined;
   const abort = (reason?: string) => {
     timeout && clearTimeout(timeout);
     controller?.abort(reason);
@@ -75,11 +137,9 @@ export function makeAbortable(
     chained.set(options.chainTo, [...(chained.get(options.chainTo) || []), () => abort("chain abort")]);
   }
   function signal() {
-    if (!options.noAutoAbort && controller?.signal.aborted === false) {
+    if (options.autoAbort !== false && controller?.signal.aborted === false)
       abort("retry");
-    }
     controller = new AbortController();
-    if (!controller) { throw new Error('AbortController is not supported!'); }
     if (options.timeout) {
       timeout = setTimeout(() => abort("timeout"), options.timeout);
     }
@@ -113,11 +173,105 @@ export function makeAbortable(
  * - `noAutoAbort`: can be set to true to make a new source not automatically abort a previous request
  * - `chainTo`: listen to another abort signal to abort this signal
  */
-
 export function createAbortable(
   options?: AbortableOptions,
 ): [() => AbortSignal, () => void, (err: any) => void] {
   const [signal, abort, filterAbortError] = makeAbortable(options);
   onCleanup(abort);
   return [signal, abort, filterAbortError];
+}
+
+const isPromiseLike = (obj: unknown): obj is PromiseLike<unknown> => !!obj &&
+  ['object', 'function'].includes(typeof obj) && typeof (obj as PromiseLike<unknown>).then === 'function';
+
+const isIterable = (obj: unknown): obj is Iterable<unknown> => !!obj && Object.hasOwn(obj, Symbol.iterator);
+  
+const isAsyncIterable = (obj: unknown): obj is AsyncIterable<unknown> => !!obj && Object.hasOwn(obj, Symbol.asyncIterator);
+  
+export type RetryOptions = {
+  delay?: number;
+  retries?: number;
+};
+
+/**
+ * **Creates a fetcher that retries multiple times in case of errors**
+ * ```ts
+ * const data = createMemo(makeRetrying(() => fetch(url()), { retries: 5 }));
+ * ```
+ * Receives the fetcher and an optional options object and returns a wrapped fetcher that retries on error after a delay multiple times.
+ *
+ * The optional options object contains the following optional properties:
+ * - `delay` - number of Milliseconds to wait before retrying; default is 5s
+ * - `retries` - number of times a request should be repeated before giving up throwing the last error; default is 3 times
+ */
+export function makeRetrying<T, C extends ComputeFunction<undefined | NoInfer<T>, T>>(
+  fetcher: C,
+  options: RetryOptions = {},
+) {
+  const delay = options.delay ?? 5000;
+  let retries = options.retries || 3;
+  
+  const handleError = (error: unknown) => {
+    if (retries-- > 0) {
+      return (delay ? 
+        (new Promise<void>(resolve => setTimeout(resolve, delay))) 
+        : Promise.resolve()).then(() => retrying);
+    }
+    retries = options.retries || 3;
+    throw error;
+  };
+
+ async function* retrying(v?: T): AsyncGenerator<T> {
+    try {
+      const result = fetcher(v);
+      if (isPromiseLike(result)) yield result.then(out => out, handleError) as Promise<T>;
+      else if (isIterable(result)) 
+        for (const item of result) 
+          if (isPromiseLike(item)) yield item.then(out => out, handleError) as Promise<T>;
+          else yield Promise.resolve(item) as Promise<T>;
+      else yield Promise.resolve(result) as Promise<T>;
+    } catch(error) {
+      handleError(error)
+    }
+  };
+  return retrying;
+}
+
+function toArray(item: any) {
+  return Array.isArray(item) ? item : item ? [item] : [];
+}
+
+/**
+ * **Automatically aggregates resource changes**
+ * ```ts
+ * const pages = makeAggregated(currentPage, [], { id: "infinite-scroll" });
+ * ```
+ * @param res {Accessor<R>} - The accessor that should be aggregated
+ * @param initialValue {I | undefined} - an optional initial value
+ * @param memoOptions - optional options for `createMemo`
+ *
+ * Depending on the content of the initialValue or the first response, this will aggregate the incoming responses:
+ * - null will not overwrite undefined
+ * - if the previous value is an Array, incoming values will be appended
+ * - if any of the values are Objects, the current one will be shallow-merged into the previous one
+ * - if the previous value is a string, more string data will be appended
+ * - otherwise the incoming data will be put into an array
+ *
+ * Objects and Arrays are re-created on each operation, but the values will be left untouched, so `<For>` should work fine.
+ */
+export function createAggregated<R, I extends R | R[]>(res: Accessor<R>, initialValue?: I, memoOptions?: Parameters<typeof createMemo<{} | R | I | R[] | I[] | undefined>>[1]) {
+  return createMemo<I | R | {} | R[] | I[] | undefined>((previous = initialValue) => {
+    const current = res();
+    return current == null && previous == null
+      ? previous
+      : Array.isArray(previous || current)
+        ? [...toArray(previous), ...toArray(current)]
+        : typeof (previous || current) === "object"
+          ? { ...previous, ...current }
+          : typeof previous === "string" || typeof current === "string"
+            ? (previous?.toString() || "") + (current || "")
+            : previous
+              ? [previous, current]
+              : [current];
+  }, memoOptions);
 }
--- packages/async/test/index.test.ts
+++ packages/async/test/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, test, expect } from "vitest";
-import { createEffect, createMemo, createRoot } from "solid-js";
-import { fromStream, makeAbortable, createAbortable } from "../src/index.js";
+import { createEffect, createMemo, createRoot, flush } from "solid-js";
+import { fromStream, fromJSONStream, makeAbortable, createAbortable, makeRetrying } from "../src/index.js";
 
 const delay = (ms = 50) => new Promise<void>(resolve => setTimeout(resolve, ms));
 
@@ -8,10 +8,11 @@ describe("fromStream", () => {
   const createStream = (data: string) => new ReadableStream({
     start(controller) {
       const chars = data[Symbol.iterator]();
+      const encoder = new TextEncoder();
       const step = () => {
         const { value, done } = chars.next();
         if (done) return;
-        controller.enqueue(value.codePointAt(0));
+        controller.enqueue(encoder.encode(value));
         delay(15).then(step);
       }
       delay().then(step);
@@ -51,12 +52,52 @@ describe("fromStream", () => {
   })), 2000);
 });
 
+describe("fromJSONStream", () => {
+  const createStream = (data: string[]) => new ReadableStream<Uint8Array>({
+    start(controller) {
+      const parts = data[Symbol.iterator]();
+      const encoder = new TextEncoder();
+      const step = () => {
+        const { value, done } = parts.next();
+        if (done) return;
+        controller.enqueue(encoder.encode(value));
+        delay(15).then(step);
+      }
+      delay().then(step);
+    },
+    pull(_controller) {},
+    cancel: () => {},
+  });
+  
+  test("streams partial JSON from response", () => new Promise<void>(resolve => createRoot(dispose => {
+    const data = [
+      '{"test": tru',
+      'e, "data": [1, 2, ',
+      '3], "solid": "is great!"}'
+    ];
+    const expected = [
+      { test: true },
+      { test: true, data: [1, 2] },
+      { test: true, data: [1, 2, 3], "solid": "is great!" },
+    ];
+    const stream = createMemo(fromJSONStream(() => createStream(data))); 
+    createEffect(stream, (json) => {
+      expect(json).toEqual(expected.shift());
+      if (!expected.length) {
+        queueMicrotask(dispose);
+        resolve();
+      }
+    }) 
+  })));
+});
+
 describe("makeAbortable", () => {
   test("makes a fetcher abortable", () => {
     const [signal, abort] = makeAbortable();
     const signal1 = signal();
     expect(signal1.aborted, "first signal should not be initially aborted").toBeFalsy();
     const signal2 = signal();
+    flush();
     expect(signal1.aborted, "first signal should be aborted after new request").toBeTruthy();
     expect(signal2, "already aborted signal should not be re-used").not.toBe(signal1);
     expect(signal2.aborted, "second signal should not be initially aborted").toBeFalsy();
@@ -99,5 +140,23 @@ describe("makeAbortable", () => {
       .catch(filterAbortError)
       .then(() => expect.fail("filtered error that was not an AbortError"))
       .catch(err => expect(err).toBe(noAbortError));
+  });
+});
+
+describe("createAbortable", () => {
+  test("aborts on cleanup", () => {
+    const [dispose, signal] = createRoot((dispose) => [dispose, createAbortable()[0]()]);
+    expect(signal.aborted).toBeFalsy();
+    dispose();
+    expect(signal.aborted).toBeTruthy();
+  });
+});
+
+describe("makeRetrying", () => {
+  test("makes a fetcher retry in case of error", async () => {
+    const responses: Promise<unknown>[] = [Promise.reject(new Error("retry"))];
+    const fetcher = (_prev: unknown) => responses.shift() || Promise.resolve(true);
+    const wrapped = makeRetrying(fetcher, { delay: 15 });
+    expect(await wrapped()).toBe(true);
   });
 });
