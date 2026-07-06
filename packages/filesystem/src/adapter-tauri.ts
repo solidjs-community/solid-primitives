9b0179173bedbaa816aafaa0f377ff29e8c7007f
Okiki Ojo <hey@okikio.dev>
2026-07-06 00:25:26 -0400

    fix(filesystem): rename tauri fs readdir to proper readDir format & add explicit return types to makeTauriFileSystem
    Signed-off-by: Okiki Ojo <hey@okikio.dev>

--- packages/filesystem/src/adapter-tauri.ts
+++ packages/filesystem/src/adapter-tauri.ts
@@ -1,7 +1,9 @@
 import type { BaseDirectory, FileEntry, FsDirOptions } from "@tauri-apps/api/fs";
+import type { AsyncFileSystemAdapter } from "./types.ts";
+import type * as TauriFS from "@tauri-apps/api/fs";
 
-export const makeTauriFileSystem = (options: FsDirOptions = { dir: 22 as BaseDirectory.AppData }) =>
-  (taurifs =>
+export const makeTauriFileSystem = (options: FsDirOptions = { dir: 22 as BaseDirectory.AppData }): AsyncFileSystemAdapter | null =>
+  ((taurifs: typeof TauriFS | undefined | null) =>
     taurifs
       ? {
           async: true as const,
@@ -9,14 +11,14 @@ export const makeTauriFileSystem = (options: FsDirOptions = { dir: 22 as BaseDir
             taurifs.exists(path, options).then((present: boolean) =>
               present
                 ? taurifs
-                    .readdir(path)
+                    .readDir(path)
                     .then(() => "dir" as const)
                     .catch(() => "file" as const)
                 : null,
             ),
           mkdir: (path: string) => taurifs.createDir(path, { ...options, recursive: true }),
           readdir: (path: string) =>
-            taurifs.readdir(path, options).then((entries: FileEntry[]) =>
+            taurifs.readDir(path, options).then((entries: FileEntry[]) =>
               entries.reduce((list, entry) => {
                 entry.name && list.push(entry.name);
                 return list;
@@ -26,7 +28,7 @@ export const makeTauriFileSystem = (options: FsDirOptions = { dir: 22 as BaseDir
           rename: (previous: string, next: string) => taurifs.renameFile(previous, next),
           rm: (path: string) =>
             taurifs
-              .readdir(path)
+              .readDir(path)
               .then(() => taurifs.removeDir(path, { ...options, recursive: true }))
               .catch(() => taurifs.removeFile(path, options)),
           writeFile: (path: string, data: string) => taurifs.writeTextFile(path, data),
